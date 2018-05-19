4bf2cf4da8456a76cfdee9926d2ab83453b3114c
Melvin Kooi <l3t1t5pam@gmail.com>
2018-05-19 11:40:00 +0200

    Changed the structure to hopefully update the packages.

---
+++ automatic/naps2.install/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$downloadUrl  = 'https://github.com/cyanfish/naps2/releases/download/v5.3.0/naps2-5.3.0-setup.msi'
+$checksum     = '7BB083199D165C50E06A604C4CD86FE939CF02CA99364D832A497BCDBEF5A53B'
+
+$packageArgs = @{
+  packageName    = "naps2.install"
+  softwareName   = "NAPS2 (Not Another PDF Scanner 2)"
+  url            = $downloadUrl
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  fileType       = 'msi'
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/naps2.install/update.ps1
@@ -0,0 +1,34 @@
+﻿import-module au
+
+$releases = 'https://api.github.com/repos/cyanfish/naps2/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]downloadUrl\s*=\s*)('.*')" = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $json = $(Invoke-WebRequest -Uri $releases | ConvertFrom-Json)
+    $msi_release = $json.assets | Where-Object { $_.name.EndsWith(".msi") }
+
+    # Get Version
+    $version = $($json | Select-Object -ExpandProperty "name")
+
+    # Get URL
+    $url = $($msi_release | Select-Object -ExpandProperty "browser_download_url")
+
+    # Get Checksum
+    $filename = $($msi_release | Select-Object -ExpandProperty "name")
+    $temp_file = $env:TEMP + '\' + $filename
+    Invoke-WebRequest $url -OutFile $temp_file
+    $checksum = $(Get-FileHash $temp_file -Algorithm SHA256 | Select-Object -ExpandProperty Hash)
+
+    $Latest = @{ URL = $url; Checksum = $checksum; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
