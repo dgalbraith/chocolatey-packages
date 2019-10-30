a5f1930e4b0cae94c979870cd78da28f0f2f7134
dgalbraith <dg@lbraith.io>
2019-10-30 09:28:09 +0000

    Updated version and documentation handling
    Version handling needed to be updated to remove the explicit replacement
    of the version in the nuspec as this is handled already by the au
    updater and to include an update for the version in the dependency
    on VS Code.
    
    Documentation handing was modified to address an apparent issue in the
    AU updater.  Prior nuspec changes were being dropped when the
    description was being updated through the Set-DescriptionFromReadme
    function - search and replace results were being reverted when the
    nuspec was written following on frrom the description update from the
    readme.
    
    As an interim measure the use of the automated update has been disabled
    but the correct action is to address the bug upstream in the AU updater.

--- automatic/vscode-python/update.ps1
+++ automatic/vscode-python/update.ps1
@@ -7,8 +7,8 @@ $publisher = 'ms-python'
 function global:au_SearchReplace {
   @{
     "$($Latest.PackageName).nuspec" = @{
-      "(copyright>)(.*)(<\/copyright>)"     = "`${1}$($Latest.Copyright)`${3}"
-      "(<version>)([0-9]+\.[0-9]+\.[0-9]+)" = "`${1}$($Latest.Version)"
+      "(copyright>)(.*)(<\/copyright>)"                     = "`${1}$($Latest.Copyright)`${3}"
+      "(id=`"vscode`"\sversion=`")([0-9]+\.[0-9]+\.[0-9]+)" = "`${1}$($Latest.VSCodeVersion)"
     }
 
     ".\tools\chocolateyinstall.ps1" = @{
@@ -25,20 +25,18 @@ function global:au_BeforeUpdate {
   Get-RemoteFiles -Purge -NoSuffix $($Latest.FileNameBase)
 }
 
-function global:au_AfterUpdate ($Package) {
-  Set-DescriptionFromReadme $Package -SkipFirst 0 -SkipLast 1
-}
 function global:au_GetLatest {
   Write-Host('Running GetLatest')
   $extensionInfo = Get-VSMarketplaceExtensionDetails -Extension $extension -Publisher $publisher
 
   @{
-    Copyright    = $extensionInfo.Copyright
-    Version      = $extensionInfo.Version
-    URL32        = $extensionInfo.DownloadUrl
-    FileNameBase = $extensionInfo.Filename -match '(?<Filename>(.*))(.vsix)' | ForEach-Object { $Matches['Filename'] }
-    FileType     = 'vsix'
+    Copyright     = $extensionInfo.Copyright
+    Version       = $extensionInfo.Version
+    VSCodeVersion = $extensionInfo.VSCodeVersion
+    URL32         = $extensionInfo.DownloadUrl
+    FileNameBase  = $extensionInfo.Filename -match '(?<Filename>(.*))(.vsix)' | ForEach-Object { $Matches['Filename'] }
+    FileType      = 'vsix'
   }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none -NoCheckUrl -NoReadme
