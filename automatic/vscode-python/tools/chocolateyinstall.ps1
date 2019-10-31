ea52c51d23fee39fb27a894b8e44bd5faf775f9c
dgalbraith <dg@lbraith.io>
2019-10-31 15:44:24 +0000

    Use fully qualified path for extension install
    The extension was not being picked up from the tools directory on
    execution.  Updated the install script to determine the   tools
    directory and fully qualify the extension reference with it.

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -1,3 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-python.python-2019.10.44104.vsix'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2019.10.44104.vsix"
