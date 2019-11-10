3abfa1d980b1656e07a124c98c6246fd014cf904
dgalbraith <dg@lbraith.io>
2019-11-10 09:50:51 +0000

    Updated versions for the 0.21.0 extension

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.20.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.21.0.vsix"
