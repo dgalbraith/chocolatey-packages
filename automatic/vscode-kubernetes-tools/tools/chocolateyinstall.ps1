eeae9e23e4507845880274e8575e467693430a44
dgalbraith <dg@lbraith.io>
2019-11-19 18:19:26 +1100

    Added VSCode Kubernetes Tools extension automatic package

---
+++ automatic/vscode-kubernetes-tools/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\ms-kubernetes-tools.vscode-kubernetes-tools-1.0.7.vsix"
---
+++ automatic/vscode-kubernetes-tools/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Uninstall-VsCodeExtension -extensionId 'ms-kubernetes-tools.vscode-kubernetes-tools'
