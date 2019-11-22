14cbe0a94f8d28382c590d19a761950465abd7c4
dgalbraith <dg@lbraith.io>
2019-11-23 10:30:42 +1100

    gh-2 Remove binary distribution
    The license for the VSCode Intellicode extension does not permit binary
    distribution. The package was updated to remove the binary and instead
    retrieve at runtime.

--- automatic/vscode-intellicode/tools/chocolateyinstall.ps1
+++ automatic/vscode-intellicode/tools/chocolateyinstall.ps1
@@ -1,5 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-VsCodeExtension -extensionId "$toolsDir\VisualStudioExptTeam.vscodeintellicode-1.2.0.vsix"
+Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.0'
