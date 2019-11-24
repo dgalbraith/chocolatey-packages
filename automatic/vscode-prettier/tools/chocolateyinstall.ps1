34fb7aade8f136a4c9a61ac57c920c6f78780ae0
dgalbraith <dg@lbraith.io>
2019-11-24 04:15:51 +0000

    AU: 1 updated - vscode-prettier [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3f362ac58c463814860d63af788ce821a0a32594

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-3.7.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-3.9.0.vsix"
