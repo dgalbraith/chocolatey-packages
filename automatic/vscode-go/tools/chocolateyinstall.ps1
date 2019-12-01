c1a3e61d915dc930ad7fcd70f3165b45207c2540
dgalbraith <dg@lbraith.io>
2019-12-01 22:59:35 +1100

    (gh-16) Added VSCode Go extension automatic package

---
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\ms-vscode.Go-0.11.9.vsix"
