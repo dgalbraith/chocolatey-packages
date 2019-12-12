b198179cfe948792847884716873e01dd2425d4c
dgalbraith <dg@lbraith.io>
2019-12-12 04:15:41 +0000

    AU: 1 updated - vscode-edge-debug [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7e5fe927dd3e1c96a4178d8299f8300225a2eb26

--- automatic/vscode-edge-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-edge-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-edge-1.0.12.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-edge-1.0.13.vsix"
