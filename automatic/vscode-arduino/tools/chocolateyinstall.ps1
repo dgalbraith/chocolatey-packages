4e4f1a84be31b0254fc0fdadc453730b48fbce42
dgalbraith <dg@lbraith.io>
2020-01-02 12:28:14 +0000

    AU: 1 updated - vscode-arduino [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/905f9433e9f5a56dc3b22ee407072d5505f03c23

--- automatic/vscode-arduino/tools/chocolateyinstall.ps1
+++ automatic/vscode-arduino/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vsciot-vscode.vscode-arduino-0.2.28.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vsciot-vscode.vscode-arduino-0.2.29.vsix"
