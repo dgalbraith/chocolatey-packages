2ac84fe2b5157c6df413c9efd595ed5d30328181
dgalbraith <dg@lbraith.io>
2020-01-08 16:46:29 +1100

    (gh-45) Added SQL Server Reporting Services 2019 automatic package

---
+++ automatic/ssrs-2019/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$silentArgs = '/quiet /norestart /IAcceptLicenseTerms'
+
+$pp = Get-PackageParameters
+$silentArgs += ($pp.GetEnumerator() | ForEach-Object { " /$($_.name)=`"$($_.value)`"" })
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Microsoft SQL Server Reporting Services'
+  url64bit       = 'https://download.microsoft.com/download/1/a/a/1aaa9177-3578-4931-b8f3-373b24f63342/SQLServerReportingServices.exe'
+  checksum64     = 'a03b2375c122599e47c179976abd877fdd34bb9017ae279b49e6d71b6782f5bc'
+  checksumType64 = 'sha256'
+  silentArgs     = $silentArgs
+  validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/ssrs-2019/update.ps1
@@ -0,0 +1,54 @@
+import-module au
+
+$base      = 'https://www.microsoft.com/download/'
+$productId = '100122'
+
+$detail       = "${base}details.aspx?id=${productId}"
+$download     = "${base}confirmation.aspx?id=${productId}"
+
+$reversion    = '(?<Version>([\d]+\.[\d]+\.[\d]+\.[\d]+))'
+$reexecutable = 'p>(?<Executable>(.*\.exe))<'
+
+function global:au_SearchReplace {
+  @{
+    "$($Latest.PackageName).nuspec" = @{
+      #"(>)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(<\/)" = "`${1}$($Latest.Version)`${3}"
+      "(>)$($reversion)(<\/)" = "`${1}$($Latest.Version)`${3}"
+    }
+
+    ".\README.md" = @{
+      "(-)$($reversion)(-)" = "`${1}$($Latest.Version)`${3}"
+    }
+
+    'tools\ChocolateyInstall.ps1' = @{
+      "(\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.URL64Bit)'"
+      "(\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $detail_page   = Invoke-WebRequest -UseBasicParsing -Uri $detail
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $download
+
+  $detail_page.Content -match $reversion
+  $version = $Matches.Version
+
+  $detail_page.Content -match $reexecutable
+  $filename = $Matches.Executable
+
+  $url = $download_page.Links | where-object { $_ -Match $filename } | Select-Object -ExpandProperty href | Select-Object -First 1
+
+  $checksum = Get-RemoteChecksum $url
+
+  @{
+    Version        = $version
+    URL64Bit       = $url
+    Filename64     = $filename
+    Checksum64     = $checksum
+    ChecksumType64 = "sha256"
+  }
+}
+
+update -ChecksumFor none -NoReadme
