3dc5e8da9a915d9b827fea9b97c73f0534395bc4
dgalbraith <dg@lbraith.io>
2020-01-23 04:16:51 +0000

    AU: 1 updated - vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/18bb54a2d8e124652b1aa84de821e55167c1b643

--- automatic/vscode-todo-tree/tools/chocolateyinstall.ps1
+++ automatic/vscode-todo-tree/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\Gruntfuggly.todo-tree-0.0.165.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\Gruntfuggly.todo-tree-0.0.166.vsix"
