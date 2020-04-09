1f61c04d8d9deeb491c23d26c2227b3a8168aa83
dgalbraith <dg@lbraith.io>
2020-04-09 04:16:07 +0000

    AU: 1 updated - vscode-azure-deploy [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/310ef1b303ae9fcfdd11211d2da2e2e89adbca49

--- automatic/vscode-azure-deploy/tools/chocolateyinstall.ps1
+++ automatic/vscode-azure-deploy/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-vscode-deploy-azure.azure-deploy-1.0.3.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-vscode-deploy-azure.azure-deploy-1.0.5.vsix"
