869a868163644ccb16c7e7ffe859e3a642fafdb1
dgalbraith <dg@lbraith.io>
2020-05-08 10:10:21 +0100

    (gh-22) Added VSCode Spring Boot Dashboard extension automatic package

---
+++ automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.1.8.vsix"
