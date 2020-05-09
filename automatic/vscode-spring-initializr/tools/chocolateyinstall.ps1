695ea96b7964ebdaec11f8a1f83f5c82181ebd0a
dgalbraith <dg@lbraith.io>
2020-05-09 04:16:57 +0000

    AU: 3 updated - vscode-cloud-code vscode-firefox-debug vscode-spring-initializr [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/cf3fceaea41376cff9256c9ae09495d67b9def81

--- automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.4.6.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.4.7.vsix"
