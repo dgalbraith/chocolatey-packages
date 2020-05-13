152412a87ee6b8d3ef53d3d76989e00779021615
dgalbraith <dg@lbraith.io>
2020-05-13 04:17:14 +0000

    AU: 3 updated - vscode-maven vscode-python vscode-spring-initializr [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/fc02d8e1ba9e7a846fb532fc396d16d2d28a5ddd

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2020.4.76186.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2020.5.78807.vsix"
--- automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.4.7.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.4.8.vsix"
