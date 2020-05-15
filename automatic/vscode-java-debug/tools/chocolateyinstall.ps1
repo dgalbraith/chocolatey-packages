aa3b4d4fc6bc04dad801ebdfc68597714d878eaa
dgalbraith <dg@lbraith.io>
2020-05-15 04:17:31 +0000

    AU: 3 updated - vscode-java-debug vscode-java-dependency vscode-vsonline [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/193ee0c08395ab082657d64bfca501aac576334f

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.24.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.26.0.vsix"
