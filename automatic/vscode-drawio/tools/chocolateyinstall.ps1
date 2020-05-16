88c6949e40681737da9a9d6b08da8259a795f33b
dgalbraith <dg@lbraith.io>
2020-05-16 16:26:03 +0100

    AU: 1 updated - vscode-drawio

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-0.1.3.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-0.2.0.vsix"
