f517eb10cd8d1eba0dda0bc3b7cc400dece2768c
dgalbraith <dg@lbraith.io>
2020-05-16 16:19:07 +0100

    (gh-68) Added Draw.io automatic package

---
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-0.1.0.vsix"
