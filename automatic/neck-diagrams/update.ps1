6e342a4a5ff6685347b617db4fe5e92152a5ee0b
dgalbraith <dg@lbraith.io>
2020-05-17 15:55:16 +0100

    (gh-77) Updated script to avoid README.md inclusion
    The update script was not supressing the inclusion of information from
    the README.md on package build due to a missing flag.  As a result
    inappropriate information was being pulled into the nuspec file.  The
    update to the flags passed to the script supresses this inclusion.

--- automatic/neck-diagrams/update.ps1
+++ automatic/neck-diagrams/update.ps1
@@ -5,16 +5,16 @@ $ErrorActionPreference = 'STOP'
 $releases = 'https://www.neckdiagrams.com/download'
 
 function global:au_SearchReplace {
-    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-          "(\s*url\s*=\s*)('.*')"             = "`$1'$($Latest.URL32)'"
-          "(\s*url64bit\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
-          "(\s*checksum\s*=\s*)('.*')"        = "`$1'$($Latest.Checksum32)'"
-          "(\s*checksumType\s*=\s*)('.*')"    = "`$1'$($Latest.ChecksumType32)'"
-          "(\s*checksum64\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum64)'"
-          "(\s*checksumType64\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType64)'"
-      }
+  @{
+      ".\tools\chocolateyInstall.ps1" = @{
+        "(\s*url\s*=\s*)('.*')"             = "`$1'$($Latest.URL32)'"
+        "(\s*url64bit\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+        "(\s*checksum\s*=\s*)('.*')"        = "`$1'$($Latest.Checksum32)'"
+        "(\s*checksumType\s*=\s*)('.*')"    = "`$1'$($Latest.ChecksumType32)'"
+        "(\s*checksum64\s*=\s*)('.*')"      = "`$1'$($Latest.Checksum64)'"
+        "(\s*checksumType64\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType64)'"
     }
+  }
 }
 
 function global:au_GetLatest {
@@ -61,4 +61,4 @@ function global:au_GetLatest {
   }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -NoCheckUrl -NoReadme
