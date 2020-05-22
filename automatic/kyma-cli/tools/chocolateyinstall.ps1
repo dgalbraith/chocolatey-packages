09adde3a82743c0621ba7550e03ff8824138c10d
dgalbraith <dg@lbraith.io>
2020-05-22 09:40:00 +0100

    (gh-79) Created an automatic package for the kyma-cli management tool

---
+++ automatic/kyma-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
+  $archive = Join-Path $toolsDir 'kyma_Windows_i386.zip'
+}
+else {
+  $archive = Join-Path $toolsDir 'kyma_Windows_x86_64.zip'
+}
+
+$unzipArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = $archive
+  Destination  = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
---
+++ automatic/kyma-cli/update.ps1
@@ -0,0 +1,58 @@
+import-module au
+
+$ErrorActionPreference = 'STOP'
+
+$domain   = 'https://github.com'
+$releases = "${domain}/kyma-project/cli/releases/latest"
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\README.md" = @{
+      "([\d]+\.[\d]+\.[\d]+)" = "$($Latest.Version)"
+    }
+
+    ".\legal\VERIFICATION.txt"      = @{
+      "([\d]+\.[\d]+\.[\d]+)" = "$($Latest.Version)"
+      "(Checksum32:\s)(.+)"   = "`${1}$($Latest.Checksum32)"
+      "(Checksum64:\s)(.+)"   = "`${1}$($Latest.Checksum64)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $reurl = '(kyma_Windows_(i386|x86_64).+zip)'
+  $reversion = '(\/(?<Version>([\d]+\.[\d]+\.[\d]+)))'
+
+  $re32 = 'i386\.zip'
+  $re64 = 'x86_64\.zip'
+
+  $urls = $download_page.links | where-object href -match $reurl | select-object -expand href | foreach-object { $domain + $_ }
+
+  $url32 = $urls -match $re32 | select-object -first 1
+  $url32SegmentSize = $([System.Uri]$url32).Segments.Length
+  $filename32 = $([System.Uri]$url32).Segments[$url32SegmentSize - 1]
+
+  $url64 = $urls -match $re64 | select-object -first 1
+  $url64SegmentSize = $([System.Uri]$url64).Segments.Length
+  $filename64 = $([System.Uri]$url64).Segments[$url64SegmentSize - 1]
+
+  $url -match $reversion
+  $version = $Matches.Version
+
+  return @{
+    FileType   = 'zip'
+    FileName32 = $filename32
+    Url32      = $url32
+    FileName64 = $filename64
+    Url64      = $url64
+    Version    = $version
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl -NoReadme
