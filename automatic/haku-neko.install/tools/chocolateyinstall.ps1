173ab18f111c7f10e039edc04b1eeedeeea55081
dgalbraith <dg@lbraith.io>
2020-05-23 23:13:23 +0100

    (gh-81) Added automatic package for the HakuNeko manga downloader

---
+++ automatic/haku-neko.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
+  $installer = Join-Path $toolsDir 'hakuneko-desktop_6.1.7_windows-setup_i386.exe'
+}
+else {
+  $installer = Join-Path $toolsDir 'hakuneko-desktop_6.1.7_windows-setup_amd64.exe'
+}
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  SoftwareName   = 'HakuNeko*'
+  File           = $installer
+  FileType       = 'exe'
+  SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  ValidExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+$files = Get-ChildItem $toolsDir -include *.exe -recurse
+
+foreach ($file in $files) {
+  #generate an ignore file
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
---
+++ automatic/haku-neko.portable/update.ps1
@@ -0,0 +1,67 @@
+import-module au
+
+$ErrorActionPreference = 'STOP'
+
+$domain   = 'https://github.com'
+$releases = "${domain}/manga-download/hakuneko/releases/latest"
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    "$($Latest.PackageName).nuspec" = @{
+      "(\/v)([\d]+\.[\d]+\.[\d]+)" = "`${1}$($Latest.Version)"
+    }
+
+    ".\README.md" = @{
+      "([\d]+\.[\d]+\.[\d]+)" = "$($Latest.Version)"
+    }
+
+    ".\legal\VERIFICATION.txt" = @{
+      "([\d]+\.[\d]+\.[\d]+)" = "$($Latest.Version)"
+      "(Checksum32:\s)(.+)"   = "`${1}$($Latest.Checksum32)"
+      "(Checksum64:\s)(.+)"   = "`${1}$($Latest.Checksum64)"
+    }
+
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(haku.+i386\.zip)"  = "$($Latest.FileName32)"
+      "(haku.+amd64\.zip)" = "$($Latest.FileName64)"
+    }
+
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $reurl = 'https:\/\/.+(hakuneko.+windows-portable_(i386|amd64)\.zip)'
+  $re32 = 'i386\.zip'
+  $re64 = 'amd64\.zip'
+  $reversion = '(\/v(?<Version>([\d]+\.[\d]+\.[\d]+)))'
+
+  $urls = $download_page.links | where-object href -match $reurl | select-object -expand href
+
+  $url32 = $urls -match $re32 | select-object -first 1
+  $url32SegmentSize = $([System.Uri]$url32).Segments.Length
+  $filename32 = $([System.Uri]$url32).Segments[$url32SegmentSize - 1]
+
+  $url64 = $urls -match $re64 | select-object -first 1
+  $url64SegmentSize = $([System.Uri]$url64).Segments.Length
+  $filename64 = $([System.Uri]$url64).Segments[$url64SegmentSize - 1]
+
+  $url32 -match $reversion
+  $version = $Matches.Version
+
+  return @{
+    FileType   = 'zip'
+    FileName32 = $filename32
+    Url32      = $url32
+    FileName64 = $filename64
+    Url64      = $url64
+    Version    = $version
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl -NoReadme
