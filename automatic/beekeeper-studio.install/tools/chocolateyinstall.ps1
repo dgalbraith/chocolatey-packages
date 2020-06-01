0c5ae6fd535559b94ca4ec73cb1ab0c4c81c8f41
dgalbraith <dg@lbraith.io>
2020-06-01 09:36:20 +0100

    (gh-99) Added beekeeper-studio automatic packages

---
+++ automatic/beekeeper-studio.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  softwareName   = 'Beekeeper Studio*'
+  file64         = "$toolsDir\Beekeeper-Studio-Setup-1.4.0.exe"
+  checksum64     = '1CFB3E1B59D06AB8519F1D4699932DB43C06A9A5CBC76C8DD107F8F673B7C7F6'
+  checksumType64 = 'sha256'
+  silentArgs     = @('/S')
+  validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$files = Get-ChildItem $toolsDir -include *.exe -recurse
+
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
---
+++ automatic/beekeeper-studio.install/update.ps1
@@ -0,0 +1,55 @@
+﻿import-module au
+
+$ErrorActionPreference = 'STOP'
+
+$domain   = 'https://github.com'
+$releases = "${domain}/beekeeper-studio/beekeeper-studio/releases/latest"
+
+$re64      = '(B.+Setup.+\.exe)'
+$reversion = '(\/v|-)(?<Version>([\d]+\.[\d]+\.[\d]+))'
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\README.md" = @{
+      "$($reversion)" = "`${1}$($Latest.Version)"
+    }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "$($reversion)" = "`${1}$($Latest.Version)"
+    }
+
+    ".\legal\VERIFICATION.txt" = @{
+      "(\/v.+|\s)$($re64)"  = "`${1}$($Latest.Filename64)"
+      "(Checksum64:\s)(.+)" = "`${1}$($Latest.Checksum64)"
+      "$($reversion)"       = "`${1}$($Latest.Version)"
+    }
+
+    ".\tools\chocolateyinstall.ps1" = @{
+      "($re64)" = "$($Latest.FileName64)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $urls = $download_page.links | where-object href -match $reurl | select-object -expand href | foreach-object { $domain + $_ }
+
+  $url64 = $urls -match $re64 | select-object -first 1
+  $url64SegmentSize = $([System.Uri]$url64).Segments.Length
+  $filename64 = $([System.Uri]$url64).Segments[$url64SegmentSize - 1]
+
+  $url64 -match $reversion
+  $version = $Matches.Version
+
+  return @{
+    FileName64 = $filename64
+    Url64      = $url64
+    Version    = $version
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl -NoReadme
