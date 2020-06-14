91db152f592a044c67834e49d4618a59d17846a6
dgalbraith <dg@lbraith.io>
2020-06-14 04:16:04 +0000

    AU: 2 updated - beekeeper-studio beekeeper-studio.portable [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/edacb24eea2d32874bfff849629fc6a92f003d9e

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$executable = Join-Path $installDir 'Beekeeper-Studio-1.4.1-portable.exe'
+$executable = Join-Path $installDir 'Beekeeper-Studio-1.5.1-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
