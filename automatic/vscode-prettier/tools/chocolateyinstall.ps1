eae33e2281d6b20e0d741370e2f384ef4147ec3e
dgalbraith <dg@lbraith.io>
2020-06-18 04:16:37 +0000

    AU: 3 updated - beekeeper-studio beekeeper-studio.portable vscode-prettier [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/42892c8400e66646569549d4cce9c5f2db17f434

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.0.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.0.1.vsix"
