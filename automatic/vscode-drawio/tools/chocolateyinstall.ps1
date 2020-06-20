f61a87c5d12730f065314cbaa9bd56aa14eb882e
dgalbraith <dg@lbraith.io>
2020-06-20 04:16:23 +0000

    AU: 3 updated - vscode-drawio vscode-prettier vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/19240fcd23f2007500cbdc1a82ae5188a12f182c

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-0.6.6.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-0.7.1.vsix"
