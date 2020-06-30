c7bf2304e82eca0c7d0f39fcd15c1c7192111975
dgalbraith <dg@lbraith.io>
2020-06-30 04:16:12 +0000

    AU: 4 updated - keys vscode-drawio vscode-go vscode-intellicode [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4666600b60943fee04116ba7cd9ac2cf5b388c58

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.14.4.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.15.0.vsix"
