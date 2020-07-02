44808bfdfe51585dba9c05815a7fb78c3b7b9f59
dgalbraith <dg@lbraith.io>
2020-07-02 04:16:10 +0000

    AU: 3 updated - vscode-chrome-debug vscode-prettier vscode-vsonline [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/8723ae1fb2356f435fe41543b98dc8c27cda82b9

--- automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.12.8.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.12.9.vsix"
