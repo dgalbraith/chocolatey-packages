a67cbe1d2fae7398919995c9b71d96b393807175
dgalbraith <dg@lbraith.io>
2020-07-03 04:16:00 +0000

    AU: 2 updated - vscode-prettier vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/d2ba071e9ff3b262537977214ce01f7d913a64a7

--- automatic/vscode-react-native/tools/chocolateyinstall.ps1
+++ automatic/vscode-react-native/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@0.16.0"
+Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@0.16.1"
