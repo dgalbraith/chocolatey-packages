472aa9cc71aad2a9f222e3bdeb78dc62f880bbce
dgalbraith <dg@lbraith.io>
2020-07-13 07:42:51 +0100

    AU: 1 updated - vscode-mongodb

--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.0.2.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.0.3.vsix"
