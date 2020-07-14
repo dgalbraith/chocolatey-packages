d035fc98f227099a1645c15b1d1d5e8fcb43ef12
dgalbraith <dg@lbraith.io>
2020-07-14 20:32:57 +0100

    AU: 1 updated - mongodb-shell

--- automatic/mongodb-shell/tools/chocolateyinstall.ps1
+++ automatic/mongodb-shell/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'mongosh-0.0.5-win32.zip'
+$archive = Join-Path $toolsDir 'mongosh-0.0.6-win32.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
