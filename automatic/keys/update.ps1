d22a604e9695ae357f8b30d444c526fe03360092
dgalbraith <dg@lbraith.io>
2020-07-18 20:12:59 +0100

    (gh-134) Corrected update script for Keys package
    Package updates were  failing due to incorrect file tokens being
    populated.  The AU scripts are not able to locate the filename as a
    result and a blank valuewas being used.
    
    Given the package supports both 32 and 64-bit versions in a single msi
    updated the file tokens so that they would be picked up by AU (use of
    FileName32) and corrected the validation and install files with the
    appropriate values.

--- automatic/keys/update.ps1
+++ automatic/keys/update.ps1
@@ -16,13 +16,13 @@ function global:au_SearchReplace {
     }
 
     ".\tools\chocolateyinstall.ps1" = @{
-      "(Keys.+msi)" = "$($Latest.Filename64)"
+      "(Keys.+msi)" = "$($Latest.Filename32)"
     }
 
     ".\legal\VERIFICATION.txt" = @{
-      "(Keys.+msi)"                = "$($Latest.Filename64)"
+      "(Keys.+msi)"                = "$($Latest.Filename32)"
+      "(Checksum:\s)(.+)"          = "`${1}$($Latest.Checksum32)"
       "(\/v)([\d]+\.[\d]+\.[\d]+)" = "`${1}$($Latest.Version)"
-      "(Checksum:\s)(.+)"          = "`${1}$($Latest.Checksum64)"
     }
   }
 }
@@ -33,17 +33,17 @@ function global:au_GetLatest {
   $reurl = '(Keys.+msi)'
   $reversion = '(\/v(?<Version>([\d]+\.[\d]+\.[\d]+))\/)'
 
-  $url = $download_page.links | where-object href -match $reurl | select-object -expand  href | select-object -first 1 | foreach-object { $domain + $_ }
-  $urlSegmentSize = $([System.Uri]$url).Segments.Length
-  $filename = $([System.Uri]$url).Segments[$urlSegmentSize - 1]
+  $url32 = $download_page.links | where-object href -match $reurl | select-object -expand  href | select-object -first 1 | foreach-object { $domain + $_ }
+  $url32SegmentSize = $([System.Uri]$url32).Segments.Length
+  $fileName32       = $([System.Uri]$url32).Segments[$urlSegmentSize - 1]
 
-  $url -match $reversion
+  $url32 -match $reversion
   $version = $Matches.Version
 
   return @{
-    Filename = $filename
-    URL = $url;
-    Version = $version;
+    FileName32 = $fileName32
+    Url32      = $url32
+    Version    = $version
   }
 }
 
