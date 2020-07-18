b724d5191443b4269f26771a74934dfe369c886c
dgalbraith <dg@lbraith.io>
2020-07-18 18:56:22 +0100

    (gh-131) Added automatic package for mongodb-compass

---
+++ automatic/mongodb-compass.install/update.ps1
@@ -0,0 +1,58 @@
+﻿import-module au
+
+$ErrorActionPreference = 'STOP'
+
+$domain   = 'https://github.com'
+$releases = "${domain}/mongodb-js/compass/releases/latest"
+
+$reFile32     = '(mongodb-c.+\.msi)'
+$reChecksum23 = '(Checksum:\s)(.+)'
+$reVersion    = '(\/v|\[|e-)(?<Version>([\d]+\.[\d]+\.[\d]+))'
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\README.md" = @{
+      "$($reVersion)" = "`${1}$($Latest.Version)"
+    }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "$($reVersion)" = "`${1}$($Latest.Version)"
+    }
+
+    ".\legal\VERIFICATION.txt" = @{
+      "$($reFile32)"     = "$($Latest.Filename32)"
+      "$($reChecksum32)" = "`${1}$($Latest.Checksum32)"
+      "$($reVersion)"    = "`${1}$($Latest.Version)"
+    }
+
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(')$($reFile32)" = "`${1}$($Latest.Filename32)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  
+  $urls  = $downloadPage.links | select-object -expand href | foreach-object { $domain + $_ }
+  
+  $url32 = $urls -match $reFile32 | select-object -first 1
+  $url32SegmentSize = $([System.Uri]$url32).Segments.Length
+  $fileName32       = $([System.Uri]$url32).Segments[$url32SegmentSize - 1]
+
+  $url32 -match $reVersion
+  $version = $Matches.Version
+
+  return @{
+    FileType   = 'msi'
+    FileName32 = $fileName32
+    Url32      = $url32
+    Version    = $version
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl -NoReadme
---
+++ automatic/mongodb-compass.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$archive  = Join-Path $toolsDir 'mongodb-compass-1.21.1-win32-x64.zip' 
+
+$unzipArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = $archive
+  Destination  = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
+
+$files = Get-ChildItem $toolsDir -include *.exe -recurse
+
+foreach ($file in $files) {
+  if ($file.Name -ne 'MongoDBCompass.exe') {
+    # generate an ignore file
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
+}
