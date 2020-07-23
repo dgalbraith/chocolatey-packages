789d24fd82c6fd7790b02830f44e381a023aebfe
dgalbraith <dg@lbraith.io>
2020-07-23 04:16:15 +0000

    AU: 4 updated - beekeeper-studio vscode-go vscode-mongodb vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/9bd1f5afcc00042773f5e273f128bd5b5119e0ab

--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.0.4.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.1.0.vsix"
--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.18.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.18.1.vsix"
