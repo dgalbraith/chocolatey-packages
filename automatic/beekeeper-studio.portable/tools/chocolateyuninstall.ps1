4b9352d4f2c479ba08beb44836186275a44f546c
dgalbraith <dg@lbraith.io>
2020-07-25 04:16:20 +0000

    AU: 2 updated - beekeeper-studio.portable vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/17c01425ed5f98e36a23f2b1f4b23c02841b3fc0

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$executable = Join-Path $installDir 'Beekeeper-Studio-1.6.9-portable.exe'
+$executable = Join-Path $installDir 'Beekeeper-Studio-1.6.10-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
