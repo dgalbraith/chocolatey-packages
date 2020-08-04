c57b91940ba317f0d9a3a92a7f7adb3bfcd85f90
dgalbraith <dg@lbraith.io>
2020-08-04 04:16:09 +0000

    AU: 2 updated - mongodb-shell vscode-go [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/34d0101a53158d0f443962cd63b3e2af030b61db

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.15.2.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.16.0.vsix"
