a1ddad65c65e3bd92b50f828727574259f4a7188
dgalbraith <dg@lbraith.io>
2020-08-06 04:18:27 +0000

    AU: 2 updated - vscode-azure-deploy vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/fff9591bc5dccbbc5ec49e129559fea955e263c3

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2020.7.4"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2020.8.0"
