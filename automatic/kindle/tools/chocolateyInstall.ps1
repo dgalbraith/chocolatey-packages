42944f381da90dfd8b2cdac1dd060e559f47ca77
dgalbraith <dg@lbraith.io>
2020-08-09 12:11:30 +0000

    AU: 1 updated - kindle [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/55ec3ad5be33993aecb0fc9a3548dda83a85aed5

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://s3.amazonaws.com/kindleforpc/57030/KindleForPC-installer-1.28.57030.exe'
-  checksum      = '6FEEA6EC44FF3D3B7BE23E7A969FE14AB884A7B19E23BC2C74237730411559F6'
+  url           = 'https://s3.amazonaws.com/kindleforpc/58059/KindleForPC-installer-1.29.58059.exe'
+  checksum      = '09b4d9cab09b7d59384ac83436d0cbe22e6514ec373e02b315ad9a3ed6047d5e'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
