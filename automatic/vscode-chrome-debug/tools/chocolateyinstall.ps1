e786bb01d1292c2178bb605e0be2d14c8af647d7
dgalbraith <dg@lbraith.io>
2020-08-18 04:16:14 +0000

    AU: 2 updated - vscode-chrome-debug vscode-java-test [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7736802de11738e8e22dd8f3dd3176c9fd1dcb9c

--- automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.12.9.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.12.10.vsix"
