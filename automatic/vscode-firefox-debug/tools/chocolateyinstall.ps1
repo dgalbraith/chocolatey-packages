421ff41687c712dc8b6bcce92d62eef1ee6881bf
dgalbraith <dg@lbraith.io>
2020-08-22 04:16:26 +0000

    AU: 3 updated - vscode-firefox-debug vscode-python vscode-vsonline [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/bb082fa18d709a01c9bfd7f6fcc2433a333852b1

--- automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.9.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.9.1.vsix"
