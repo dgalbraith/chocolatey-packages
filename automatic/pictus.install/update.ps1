6cc8e9680a9d64e688e34821a967b718a3d1a55d
dgalbraith <dg@lbraith.io>
2020-08-24 22:08:26 +0100

    (gh-176) Added Pictus package
    Added an automatic package for the Pictus Image Viewer.  The package
    provides a meta-package along with install/portable versions.

---
+++ automatic/pictus.install/update.ps1
@@ -0,0 +1,25 @@
+﻿. $PSScriptRoot\..\pictus\update.ps1
+
+$ErrorActionPreference = 'STOP'
+
+function global:au_BeforeUpdate {
+  $Latest.File32   = "$($Latest.FileNameInstall)"
+  $Latest.Url32    = "$($Latest.UrlInstall)"
+  $Latest.FileType = 'exe'
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\README.md" = @{
+      "$($reversion)" = "`${1}$($Latest.Version)"
+    }
+
+    ".\legal\VERIFICATION.txt" = @{
+      "$($reversion)"      = "`${1}$($Latest.Version)"
+      "(Checksum:\s*)(.+)" = "`${1}$($Latest.Checksum32)"
+    }
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl -NoReadme
---
+++ automatic/pictus.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$archive   = Join-Path $toolsDir 'pictus32.zip'
+$archive64 = Join-Path $toolsDir 'pictus64.zip'
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileFullPath   = $archive
+  FileFullPath64 = $archive64
+  Destination  = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
