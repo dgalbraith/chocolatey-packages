fc286bb25729c351c5f720ce5a151310608ae889
dgalbraith <dg@lbraith.io>
2020-08-24 22:08:26 +0100

    (gh-176) Added Pictus package
    Added an automatic package for the Pictus Image Viewer.  The package
    provides a meta-package along with install/portable versions.

---
+++ automatic/pictus.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  File           = "$toolsDir\PictInst.exe"
+  SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  ValidExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/pictus/update.ps1
@@ -0,0 +1,53 @@
+﻿import-module au
+
+$ErrorActionPreference = 'STOP'
+
+$domain   = 'https://github.com'
+$releases = "${domain}/poppeman/Pictus/releases/latest"
+
+$reversion = '(\/v|\[|-)(?<Version>([\d]+\.[\d]+\.[\d]+))'
+function global:au_BeforeUpdate {
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\README.md" = @{
+      "$($reversion)" = "`${1}$($Latest.Version)"
+    }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "$($reversion)" = "`${1}$($Latest.Version)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $urls = $downloadPage.links | where-object href -match 'Pict.{4}\.[exe|zip]' | select-object -expand href | foreach-object { $domain + $_ }
+
+  $urlInstall      = $urls -match '.*exe' | Select-Object -First 1
+  $fileNameInstall = $urlInstall -split '/' | Select-Object -Last 1
+
+  $urlPortable32      = $urls -match '.*32\.zip' | Select-Object -First 1
+  $fileNamePortable32 = $urlPortable32 -split '/' | Select-Object -Last 1
+
+  $urlPortable64      = $urls -match '.*64\.zip' | Select-Object -First 1
+  $fileNamePortable64 = $urlPortable64 -split '/' | Select-Object -Last 1
+
+  $UrlInstall -Match $reversion
+  $version = $Matches.Version
+
+  return @{
+    UrlInstall         = $urlInstall
+    FileNameInstall    = $fileNameInstall
+    UrlPortable32      = $urlPortable32
+    FileNamePortable32 = $fileNamePortable32
+    UrlPortable64      = $urlPortable64
+    FileNamePortable64 = $fileNamePortable64
+    Version            = $version
+  }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor none -NoCheckUrl -NoReadme
+}
