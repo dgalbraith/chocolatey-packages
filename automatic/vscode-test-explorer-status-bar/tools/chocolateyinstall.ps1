d6f75ceda10508e1f0bc1c9d98b04ce39cbb740e
dgalbraith <dg@lbraith.io>
2020-08-30 19:28:53 +0100

    (gh-149) Add Test Explorer Status Bar package
    Add automatic package for the Test Explorer Status Bar VSCode extension.

---
+++ automatic/vscode-test-explorer-status-bar/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-VsCodeExtension -extensionId "$toolsDir\connorshea.vscode-test-explorer-status-bar-1.1.2.vsix"
