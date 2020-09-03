8b7fec26280021893069298f9e112a1b1b0d6cc5
dgalbraith <dg@lbraith.io>
2020-09-03 04:16:36 +0000

    AU: 4 updated - vscode-cloud-code vscode-go vscode-java-dependency vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/56b20671430a3865df283c52257342de10256531

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.12.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.13.0.vsix"
--- automatic/vscode-maven/tools/chocolateyinstall.ps1
+++ automatic/vscode-maven/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-maven-0.24.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-maven-0.24.2.vsix"
