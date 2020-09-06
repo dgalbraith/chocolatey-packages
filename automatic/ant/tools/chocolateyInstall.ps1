3fac62224994a96412abedebf8bd9692bcfff5c7
dgalbraith <dg@lbraith.io>
2020-09-06 03:33:48 +0100

    (gh-181) Initial Ant package updates
    Initial updates of the Apache Ant package following import from
    anthonymastreon.  Changes to bring the package inline with repository
    standards and implement an AU automatic update.

--- automatic/ant/tools/chocolateyInstall.ps1
+++ automatic/ant/tools/chocolateyInstall.ps1
@@ -1,19 +1,28 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$package = Split-Path $tools
-$ant_home = Join-Path $package 'apache-ant-1.10.5'
-$ant_bat = Join-Path $ant_home 'bin/ant.bat'
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage `
-    -PackageName 'ant' `
-    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.zip' `
-    -Checksum '2E48F9E429D67708F5690BC307232F08440D01EBE414059292B6543971DA9C7CD259C21533B9163B4DD753321C17BD917ADF8407D03245A0945FC30A4E633163' `
-    -ChecksumType 'SHA512' `
-    -UnzipLocation $package
+$tools = Split-Path $MyInvocation.MyCommand.Definition
 
-Install-ChocolateyEnvironmentVariable `
-    -VariableName 'ANT_HOME' `
-    -VariableValue $ant_home `
-    -VariableType 'Machine'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$archive  = Join-Path $toolsDir 'apache-ant-1.10.5-bin.zip'
+
+$unzipArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = $archive
+  Destination  = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
+
+$ant_home = $archive -split '-bin' | select -first 1
+$ant_bat  = Join-Path $ant_home 'bin/ant.bat'
+
+$environmentArgs = @{
+    VariableName  = 'ANT_HOME'
+    VariableValue = $ant_home
+    VariableType  = 'Machine'
+}
+
+Install-ChocolateyEnvironmentVariable @environmentArgs
 
 Install-BinFile -Name 'ant' -Path $ant_bat
 
--- automatic/ant/tools/chocolateyUninstall.ps1
+++ automatic/ant/tools/chocolateyUninstall.ps1
@@ -1,6 +1,18 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$package = Split-Path $tools
-$ant_home = Join-Path $package 'apache-ant-1.10.5'
-$ant_bat = Join-Path $ant_home 'bin/ant.bat'
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$ant_home = Join-Path $toolsDir 'apache-ant-1.10.5'
+$ant_bat  = Join-Path $ant_home 'bin/ant.bat'
+
+$environmentArgs = @{
+    VariableName  = 'ANT_HOME'
+    VariableValue = $null
+    VariableType  = 'Machine'
+}
+
+Install-ChocolateyEnvironmentVariable @environmentArgs
 
 Uninstall-BinFile -Name 'ant' -Path $ant_bat
+
+Update-SessionEnvironment
