cac602ce4a55fe878c73b353ae31190730bed2c5
dgalbraith <dg@lbraith.io>
2020-09-10 04:16:33 +0000

    AU: 2 updated - vscode-prettier vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a31f549eb86766452710fd8f4c146c98f7c7539d

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.5.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.6.0.vsix"
