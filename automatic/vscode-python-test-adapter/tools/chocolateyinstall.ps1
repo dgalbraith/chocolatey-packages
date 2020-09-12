e378e81a210d8433093a52c83316ec251bd9a6df
dgalbraith <dg@lbraith.io>
2020-09-12 04:16:52 +0000

    AU: 3 updated - keys vscode-pull-request-github vscode-python-test-adapter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4cf284e9793dfdeb1a922c268c504be068c11f56

--- automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
+++ automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.4.6.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.5.0.vsix"
