076136dd2440fca8f6bd9cfa2a67b323f7415ed5
dgalbraith <dg@lbraith.io>
2020-10-12 04:16:29 +0000

    AU: 1 updated - stunnel [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040

--- automatic/stunnel/tools/chocolateyInstall.ps1
+++ automatic/stunnel/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  File           = "$toolsDir\stunnel-5.56-win64-installer.exe"
+  File           = "$toolsDir\stunnel-5.57-win64-installer.exe"
   SilentArgs     = '/S'
   ValidExitCodes = @(0)
 }
