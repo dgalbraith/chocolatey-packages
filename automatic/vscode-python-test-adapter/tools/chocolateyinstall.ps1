4ee170c2aa1166f9d405cdaccdbf271a615104d4
dgalbraith <dg@lbraith.io>
2020-11-20 04:18:09 +0000

    AU: 3 updated - vscode-java-dependency vscode-python vscode-python-test-adapter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040

--- automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
+++ automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.2.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.3.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2020.11.367453362.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2020.11.371526539.vsix"
