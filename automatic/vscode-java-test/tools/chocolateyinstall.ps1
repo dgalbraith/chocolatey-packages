7a17211591073e98da4517f0be39e6dc506e039c
dgalbraith <dg@lbraith.io>
2020-12-09 04:16:32 +0000

    AU: 2 updated - vscode-java-test vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/1179dd82f6a2520a5f4e66c518a2f463485ddf80

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.26.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.26.1.vsix"
