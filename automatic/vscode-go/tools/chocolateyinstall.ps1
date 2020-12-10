6a8857b3abee20e68a04381d810ed125af6d4f85
dgalbraith <dg@lbraith.io>
2020-12-10 04:16:39 +0000

    AU: 4 updated - tunein-radio vscode-go vscode-pylance vscode-python-test-adapter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7b02e350f2bd51f10e447de5b9717c96ffe8f8f2

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.19.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.19.1.vsix"
