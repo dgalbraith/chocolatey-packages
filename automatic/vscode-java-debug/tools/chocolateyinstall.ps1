7b6756de48449dea7b9487a145a7e060d470a620
dgalbraith <dg@lbraith.io>
2020-12-18 04:16:48 +0000

    AU: 2 updated - vscode-java vscode-java-debug [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3ee5057566a4dfe495d6db844fef49de51c785f2

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.29.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.30.0.vsix"
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-0.72.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-0.73.0.vsix"
