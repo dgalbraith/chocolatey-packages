bbd5583d74055a0239afb64c7c842afac9fe9695
dgalbraith <dg@lbraith.io>
2021-01-21 04:16:40 +0000

    AU: 5 updated - vscode-go vscode-java vscode-pylance vscode-python-test-adapter vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a9827246bd1bd482425eb4288db3ae36f345fc01

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-0.73.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-0.74.0.vsix"
--- automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
+++ automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.5.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.6.vsix"
