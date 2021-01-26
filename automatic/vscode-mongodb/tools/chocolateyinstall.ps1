8c8746240f723dcb383d7a60054b5fd94e085433
dgalbraith <dg@lbraith.io>
2021-01-26 04:16:44 +0000

    AU: 2 updated - vscode-kubernetes-tools vscode-mongodb [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a68db8ec5571bdb3c63dfc2906ca1f4448df3922

--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.3.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.4.0.vsix"
