522ab95f810bd1ad66a45c4797708df6da9aa251
dgalbraith <dg@lbraith.io>
2021-01-29 12:41:06 +0000

    (gh-181) Initial isorecorder and isowriter package updates
    Initial updates of the Prime95 package to bring the package inlines with
    repository standards.
    
    Packages were also updated to utilise archive.org as a source given the
    original distribution site is no longer available.

--- manual/isowriter/tools/chocolateyInstall.ps1
+++ manual/isowriter/tools/chocolateyInstall.ps1
@@ -1,9 +1,13 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage `
-    -PackageName 'isowriter' `
-    -Url 'http://isorecorder.alexfeinman.com/static/ISOWriter.0.6.1.0.zip' `
-    -Checksum '1983E9144BFF25571B9D4897F82406AC68B2A016B3458D4D7210DE014F18DA2B' `
-    -ChecksumType 'SHA256' `
-    -UnzipLocation $content
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'https://web.archive.org/web/20171001223928/http://isorecorder.alexfeinman.com/download/ISORecorder/ISOWriter.0.6.1.0.zip'
+  checksum      = '1983E9144BFF25571B9D4897F82406AC68B2A016B3458D4D7210DE014F18DA2B'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
