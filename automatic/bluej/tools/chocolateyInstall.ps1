401dcf0e493645f7b001ddfb4a10a37de7ceea69
dgalbraith <dg@lbraith.io>
2021-01-29 04:16:58 +0000

    AU: 1 updated - bluej [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4e82f6b06af5a7d74491accfeb64750be30e17b7

--- automatic/bluej/tools/chocolateyInstall.ps1
+++ automatic/bluej/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'msi'
-  Url64Bit       = 'http://www.bluej.org/download/files/BlueJ-windows-422.msi'
-  Checksum64     = '6a70e4a9fd48bc7e22143c90ad0bba624b88187dcf70a61893d020fd2b61f033'
+  Url64Bit       = 'https://www.bluej.org/download/files/BlueJ-windows-500.msi'
+  Checksum64     = 'cfef2f65665ab78af32fd442b39bdce2d78c3805f9f76547f7da6b3b006a0a4f'
   ChecksumType64 = 'sha256'
   SilentArgs     = '/quiet /qn /norestart'
   ValidExitCodes = @(0,3010)
