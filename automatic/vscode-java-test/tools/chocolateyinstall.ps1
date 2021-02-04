e59dfab64c6c516aea886759320bf8ed6e9a94f2
dgalbraith <dg@lbraith.io>
2021-02-04 04:17:19 +0000

    AU: 2 updated - vscode-java-test vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3e731342abdb5df0a8b7ccd0b7058aa45866449a

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.27.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.28.0.vsix"
