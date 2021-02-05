5daaa5065907098bfccf9f79c8ed1def290f19a3
dgalbraith <dg@lbraith.io>
2021-02-05 04:16:44 +0000

    AU: 3 updated - mongodb-shell vscode-prettier vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/9b0aea8866cd7de847898d6543c7062b1b0f6e9b

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.8.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.9.1.vsix"
