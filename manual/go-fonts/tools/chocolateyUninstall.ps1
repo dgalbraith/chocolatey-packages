5486dfec65ef485ab1b239d6d1b62b3c4ce4eba3
dgalbraith <dg@lbraith.io>
2021-02-05 17:59:58 +0000

    (gh-218) Add package for the Go TrueType fonts
    Add a manual package for the Go TrueType fonts.

---
+++ manual/go-fonts/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$files = Get-ChildItem $toolsDir -include *.ttf -recurse
+
+foreach ($file in $files) {
+  Uninstall-ChocolateyFont $file.Name | Out-Null
+}
