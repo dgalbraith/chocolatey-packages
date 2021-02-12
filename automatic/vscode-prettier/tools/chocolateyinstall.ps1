5f2f60e24c141b6d028b1a6d07f273b483df3ebe
dgalbraith <dg@lbraith.io>
2021-02-12 04:17:28 +0000

    AU: 4 updated - vscode-prettier vscode-pull-request-github vscode-pylance vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/d149ef573995f07b9dfbcaf2d1e85d5deb04d145

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.9.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-5.9.2.vsix"
