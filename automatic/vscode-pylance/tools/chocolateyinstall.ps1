a55548c6d6c029d6e1a4bbc8d4990311a9d997b7
dgalbraith <dg@lbraith.io>
2021-02-25 04:16:48 +0000

    AU: 2 updated - vscode-pylance vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3d4b7fa042c27b53e53abafe4964c063c8bcd437

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.2.3"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.2.4"
