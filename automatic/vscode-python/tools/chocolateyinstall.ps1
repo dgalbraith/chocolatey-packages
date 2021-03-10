1f8d495189a4bc5f22b33b0c0f202d6e4c2f608a
dgalbraith <dg@lbraith.io>
2021-03-10 04:16:49 +0000

    AU: 2 updated - vscode-maven vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e28d8c7842aaaf54ec716cc38978206d0b181fd4

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2021.2.633441544.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2021.2.636928669.vsix"
