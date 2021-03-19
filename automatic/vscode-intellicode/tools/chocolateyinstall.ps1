2a9f1e2975a98394b09eac2258d560dc1ac50deb
dgalbraith <dg@lbraith.io>
2021-03-19 04:17:26 +0000

    AU: 4 updated - vscode-intellicode vscode-python-test-adapter vscode-test-explorer vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a20e39a2f391f9339af3807fc754efe662cc586e

--- automatic/vscode-intellicode/tools/chocolateyinstall.ps1
+++ automatic/vscode-intellicode/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.11'
+Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.12'
