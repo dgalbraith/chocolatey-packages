42090799d3d75583fe4ccc43a49199b0bbf3afa0
dgalbraith <dg@lbraith.io>
2021-03-20 04:16:42 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.portable vscode-oracle-devtools vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e647b30915cf859000b5b5927ff07d7c08d8545e

--- automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
+++ automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-19.3.3.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-19.3.4.vsix"
