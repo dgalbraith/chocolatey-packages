6ece5e52cc0feab19853446fe768cb935111bf05
dgalbraith <dg@lbraith.io>
2021-03-30 04:17:13 +0000

    AU: 3 updated - jruby vscode-jupyter vscode-spring-boot [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/fb5d72eb59e120b972814940982d51a222810e0c

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.5.689556049'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.5.697286501'
