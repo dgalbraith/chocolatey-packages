dc38d29fe07a9ac9e8b300141a2614ea71a90c70
dgalbraith <dg@lbraith.io>
2021-04-06 04:16:42 +0000

    AU: 2 updated - stunnel vscode-prettier [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/eb80e292965c1d5a155c446ff9c73b840f8bd070

--- automatic/stunnel/tools/chocolateyInstall.ps1
+++ automatic/stunnel/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  File           = "$toolsDir\stunnel-5.58-win64-installer.exe"
+  File           = "$toolsDir\stunnel-5.59-win64-installer.exe"
   SilentArgs     = '/S'
   ValidExitCodes = @(0)
 }
