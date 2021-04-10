c354f6be7d913359edb8cd16df2aef340fd5529b
dgalbraith <dg@lbraith.io>
2021-04-10 04:17:06 +0000

    AU: 4 updated - kindlepreviewer vscode-go vscode-jupyter vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/0e039678a200e6d12bc0559a824ca487410b8528

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.728949111'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.732501275'
