1bd6bd4f7b9a3e76948be078fb57729c4ede113e
dgalbraith <dg@lbraith.io>
2021-04-17 04:16:51 +0000

    AU: 1 updated - vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e9b411403307eba3c3ea59bd687e5690cd311ef2

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.751425733'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.755784270'
