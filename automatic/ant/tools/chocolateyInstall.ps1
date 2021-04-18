443bdb923ada219ea8f551f9c2a80e96fa8a7bb3
dgalbraith <dg@lbraith.io>
2021-04-18 03:30:44 +0000

    AU: 2 updated - ant vscode-firefox-debug [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/69344c33cd23b920db363f14ae6b662053b251e8

--- automatic/ant/tools/chocolateyInstall.ps1
+++ automatic/ant/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
-$archive  = Join-Path $toolsDir 'apache-ant-1.10.9-bin.zip'
+$archive  = Join-Path $toolsDir 'apache-ant-1.10.10-bin.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
