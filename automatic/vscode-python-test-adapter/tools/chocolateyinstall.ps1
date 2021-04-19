25865f801cb978205ecce40e5b0ed7dd3f256590
dgalbraith <dg@lbraith.io>
2021-04-19 04:16:38 +0000

    AU: 1 updated - vscode-python-test-adapter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/76edd1c52334e8777034f5d276d9b14bc2e7d60e

--- automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
+++ automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.7.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.6.8.vsix"
