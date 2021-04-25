678b0a6e3a7ff866ab7d771802eed10a56cc26e7
dgalbraith <dg@lbraith.io>
2021-04-25 14:05:48 +0100

    (gh-278) Remove certificate installation
    Direct installation of certificates is no longer required to ensure a
    silent install.  Removed the certificate addition/removal from the
    package setup.

--- automatic/virtualclonedrive/tools/chocolateyInstall.ps1
+++ automatic/virtualclonedrive/tools/chocolateyInstall.ps1
@@ -1,24 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir     = Split-Path -parent $MyInvocation.MyCommand.Definition
-$certificate  = Join-Path $toolsDir 'ElaborateBytesAG.p7b'
-
-# serial numbers for the certificates contained in the ElaborateBytesAG.p7b file
-$certificates = @{ 
-  '0400000000012F4EE1355C' = $null # Elaborate Bytes AG
-  '040000000001154B5AC394' = $null # GlobalSign Code SIgning CA - G2
-  '11219EC29016D73F2CC88EB9CC8B4F8E78FB' = $null # GlobalSign Root CA
-}
-
-$existingCertificates = @{}
-
-# determine if any of the certificates that we will install are already present
-Get-ChildItem -Path Cert:\LocalMachine\TrustedPublisher -Recurse `
-    | where-object { $certificates.containsKey($_.SerialNumber) } `
-    | foreach-object { $existingCertificates.Add($_.SerialNumber, $_) }
-
-# register the certificate used to sign the driver in the Trusted Publisher store
-certutil -addstore 'TrustedPublisher' $certificate
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -44,8 +26,3 @@ if (-not $pp.AddToDesktop) {
   $desktopPath = [System.Environment]::GetFolderPath('CommonDesktopDirectory')
   Remove-Item "$desktopPath\Virtual CloneDrive.lnk" -ErrorAction SilentlyContinue -Force
 }
-
-# remove any certificates which were added once the package/driver has been installed
-Get-ChildItem -Path Cert:\LocalMachine\TrustedPublisher -Recurse `
-    | where-object { $certificates.containsKey($_.SerialNumber) } `
-    | foreach-object { if (-Not $existingCertificates.ContainsKey($_.SerialNumber)) { certutil -delstore 'TrustedPublisher' $_.SerialNumber } }
