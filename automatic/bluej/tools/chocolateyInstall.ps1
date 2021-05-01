6d52f480cf9b1e548c8f4a3b923c174ade4e56be
dgalbraith <dg@lbraith.io>
2021-05-01 04:17:23 +0000

    AU: 5 updated - bluej mongodb-shell vscode-java vscode-java-debug vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e65835b5fac05260efb2c705afa48fef6f84ff76

--- automatic/bluej/tools/chocolateyInstall.ps1
+++ automatic/bluej/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'msi'
-  Url64Bit       = 'https://www.bluej.org/download/files/BlueJ-windows-500.msi'
-  Checksum64     = 'cfef2f65665ab78af32fd442b39bdce2d78c3805f9f76547f7da6b3b006a0a4f'
+  Url64Bit       = 'https://www.bluej.org/download/files/BlueJ-windows-501.msi'
+  Checksum64     = 'd44b73c0c758abc2e57caaf6f835409a686fd37817c01c0ea319d3d3cce9a01c'
   ChecksumType64 = 'sha256'
   SilentArgs     = '/quiet /qn /norestart'
   ValidExitCodes = @(0,3010)
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.795492900'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.6.798918866'
