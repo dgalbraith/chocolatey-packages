40bd4c2dc4dca04e2f35fd2132787c428cc666d3
dgalbraith <dg@lbraith.io>
2021-05-02 04:16:36 +0000

    AU: 1 updated - vscode-haskell [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/911558fe93cae2c54963ce08baf1bac7b751b36e

--- automatic/vscode-haskell/tools/chocolateyinstall.ps1
+++ automatic/vscode-haskell/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-1.2.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-1.3.0.vsix"
