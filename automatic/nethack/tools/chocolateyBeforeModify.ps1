ee3b229bd5867f20e0adf47501b1a40d7755f261
dgalbraith <dg@lbraith.io>
2021-05-23 19:10:53 +0100

    (gh-293) Initial nethack package updates
    Initial updates of the nethack package to bring the package
    inline with repository standards and implement an AU automatic updater.

---
+++ automatic/nethack/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$nethackLink        = 'Nethack.lnk'
+$nethackClassicLink = 'Nethack (Classic).lnk'
+
+$desktop         = [Environment]::GetFolderPath('Desktop')
+$commonDesktop   = [Environment]::GetFolderPath('CommonDesktopDirectory')
+$startMenu       = [Environment]::GetFolderPath('StartMenu')
+$commonStartMenu = [Environment]::GetFolderPath('CommonStartMenu')
+
+$paths = @(
+  (Join-Path $desktop $nethackLink),
+  (Join-Path $desktop $nethackClassicLink),
+  (Join-Path $commonDesktop $nethackLink),
+  (Join-Path $commonDesktop $nethackClassicLink),
+  (Join-Path $startMenu 'NetHack' | Join-Path -childpath $nethackLink),
+  (Join-Path $startMenu 'NetHack' | Join-Path -childpath $nethackClassicLink),
+  (Join-Path $startMenu 'NetHack'),
+  (Join-Path $commonStartMenu 'NetHack' | Join-Path -childpath $nethackLink),
+  (Join-Path $commonStartMenu 'NetHack' | Join-Path -childpath $nethackClassicLink)
+  (Join-Path $commonStartMenu 'NetHack')
+)
+
+$paths.GetEnumerator() | ForEach-Object {
+  if (Test-Path -Path $_) {
+    Remove-Item $_ -ErrorAction SilentlyContinue -Force | Out-Null
+  }
+}
--- automatic/nethack/tools/chocolateyInstall.ps1
+++ automatic/nethack/tools/chocolateyInstall.ps1
@@ -1,24 +1,68 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
-$classic = Join-Path $content 'NetHack.exe'
-$classic_shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'NetHack for Windows (Classic).lnk'
-$tiles = Join-Path $content 'NetHackW.exe'
-$tiles_shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'NetHack for Windows (Tiles).lnk'
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage `
-    -PackageName 'nethack' `
-    -Url 'https://www.nethack.org/download/3.6.2/NetHack-362-win-x86.zip' `
-    -Checksum 'b189c83e58a87319a89e578846af28ae' `
-    -UnzipLocation $content
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$archive  = Join-Path $toolsDir 'NetHack-362-win-x86.zip'
 
-New-Item -Type File -Path "$tiles.ignore" -Force | Out-Null
+$unzipArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = $archive
+  Destination  = $toolsDir
+}
 
-Install-ChocolateyShortcut `
-    -ShortcutFilePath $classic_shortcut `
-    -WorkingDirectory $content `
-    -TargetPath $classic
+Get-ChocolateyUnzip @unzipArgs
 
-Install-ChocolateyShortcut `
-    -ShortcutFilePath $tiles_shortcut `
-    -WorkingDirectory $content `
-    -TargetPath $tiles
+$pp = Get-PackageParameters
+
+$paths = New-Object System.Collections.ArrayList
+
+if ($pp.count -gt 0) {
+  $pp.GetEnumerator() | foreach-object {
+    switch ($_.name) {
+        'AddToDesktop' {
+          Write-Verbose("Desktop shortcuts will be created for $env:ChocolateyPackageName")
+          if ($pp.User) {
+            $desktopPath = [Environment]::GetFolderPath('Desktop')
+          } else {
+            $desktopPath = [Environment]::GetFolderPath('CommonDesktopDirectory')
+          }
+
+          $paths.add($desktopPath) | Out-Null
+        }
+        'AddToStartMenu' {
+          Write-Verbose("$env:ChocolateyPackageName will be added to the Start Menu")
+          if ($pp.User) {
+            $startMenuPath = [Environment]::GetFolderPath('StartMenu')
+          } else {
+            $startMenuPath = [Environment]::GetFolderPath('CommonStartMenu')
+          }
+
+          $startMenuFolder = Join-Path $startMenuPath 'NetHack'
+          if (-Not (Test-Path -PathType Container $startMenuFolder)) {
+            New-Item -Path $startMenuFolder -ItemType directory -Force | Out-Null
+          }
+          
+          $paths.Add($startMenuFolder) | Out-Null
+        }
+        'User' {
+          # ignore - no need to handle independently as it is a qualifier for other options
+        }
+        Default {
+          Write-Verbose("Unknown parameter $_.name will be ignored")
+        }
+    }
+  }
+
+  if ($paths.Count -gt 0) {
+    $executable        = Join-Path $toolsDir 'NetHack.exe'
+    $classicExecutable = Join-Path $toolsDir 'NetHackW.exe'
+    $icon              = Join-Path $toolsDir 'nethack.ico'
+    
+    $paths.GetEnumerator() | foreach-object {
+      $shortcutPath = Join-Path $_ 'Nethack.lnk'
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $executable -IconLocation $icon
+
+      $shortcutPath = Join-Path $_ 'Nethack (Classic).lnk'
+      Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $classicExecutable -IconLocation $icon
+    }
+  }
+}
