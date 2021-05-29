e506b57e500fc219911d496c3463a7a23c546520
dgalbraith <dg@lbraith.io>
2021-05-29 19:14:00 +0100

    (gh-181) Initial mousewithoutborders package updates
    Initial updates of the mousewithoutborders package to bring the package
    inline with repository standards and implement an AU automatic updater.

--- automatic/mousewithoutborders/tools/chocolateyInstall.ps1
+++ automatic/mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,7 +1,13 @@
-﻿Install-ChocolateyPackage `
-  -PackageName 'mousewithoutborders' `
-  -Url 'https://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi' `
-  -Checksum 'c73d373275519de5545824ff20e886e4c2d76770cb77f8b685c0b52a1c07e97d' `
-  -ChecksumType 'SHA256' `
-  -FileType 'MSI' `
-  -SilentArgs '/QN'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = 'msi'
+  Url            = 'https://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi'
+  Checksum       = 'c73d373275519de5545824ff20e886e4c2d76770cb77f8b685c0b52a1c07e97d'
+  ChecksumType   = 'sha256'
+  SilentArgs     = '/quiet /qn /norestart'
+  ValidExitCodes = @(0, 1605)
+}
+
+Install-ChocolateyPackage @packageArgs
