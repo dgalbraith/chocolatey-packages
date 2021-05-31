3128b2ad53b4726a91c17e36ebda648a1cea5d87
dgalbraith <dg@lbraith.io>
2021-05-31 04:16:46 +0000

    AU: 1 updated - vscode-test-explorer [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a43fe47b03e842f862453a2a893abd5feb113f87

--- automatic/vscode-test-explorer/tools/chocolateyinstall.ps1
+++ automatic/vscode-test-explorer/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hbenl.vscode-test-explorer-2.20.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hbenl.vscode-test-explorer-2.20.2.vsix"
