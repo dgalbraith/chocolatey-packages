f86bb19c749e9c8236f033f2f582f42b92daac2b
dgalbraith <dg@lbraith.io>
2021-06-07 03:34:55 +0000

    AU: 1 updated - sqljdbc [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/54cc33ec17733692f761bf4f66a8f4475d860769

--- automatic/sqljdbc/tools/chocolateyInstall.ps1
+++ automatic/sqljdbc/tools/chocolateyInstall.ps1
@@ -9,21 +9,21 @@ public struct Language {
 "@
 
 $languageDefinitions =  @{
-    'br'    = [Language] @{ Name = 'Brazilian (Portuguese)'; Mapping = 'ptb'; Checksum = 'B01BB14599BC9222B98BFC937BE4C87EDEA8ECCC3AA7D09B5800F26E38C503BB' }
-    'de'    = [Language] @{ Name = 'German';                 Mapping = 'deu'; Checksum = '369154D14A483531722D490550C4CBC2944E6C7153840AD55E0E2B3D1B269F62' }
-    'en'    = [Language] @{ Name = 'English';                Mapping = 'enu'; Checksum = 'EDCC5E277289BA0B526F6DC9F732087DAC004DE54C64E95F9049F68218662EE6' }
-    'es'    = [Language] @{ Name = 'Spanish';                Mapping = 'esn'; Checksum = 'F7BE52C654CE9207AB2904BBC745FC62B9787E02E35C09B7CF459E10D9D65F59' }
-    'fr'    = [Language] @{ Name = 'French';                 Mapping = 'fra'; Checksum = '4711573DC4E54B18928221AF9DD0F9927B741B026A742968BD4CF5AF428CD137' }
-    'it'    = [Language] @{ Name = 'Italian';                Mapping = 'ita'; Checksum = '8F95B4B6063383360F4FBE818E276F04D5866FBB60E7B3BD0A7970B8D9387B6C' }
-    'ja'    = [Language] @{ Name = 'Japanese';               Mapping = 'jpn'; Checksum = '0DE8342018EA328434E5A26217E64F042AAC0F892EAA1AD9EC009563E52C9623' }
-    'ko'    = [Language] @{ Name = 'Korean';                 Mapping = 'kor'; Checksum = '67D7D947562D72D9484DB9D6A831EEB91157DF8699418FC8FDAF64DB606E641C' }
-    'ru'    = [Language] @{ Name = 'Russian';                Mapping = 'rus'; Checksum = '417144F34DBBDA8FFA18219B3BB6CD94A28F1095211E8B161F3E3122E8EB9FAC' }
-    'zh'    = [Language] @{ Name = 'Chinese';                Mapping = 'chs'; Checksum = '0C60DFE98BFD37F3D3E1F6F5AFC4A40365E5A8BCBA491B8E73BE67C9A78B98E4' }
-    'zh-CN' = [Language] @{ Name = 'Chinese (Simplified)';   Mapping = 'chs'; Checksum = '0C60DFE98BFD37F3D3E1F6F5AFC4A40365E5A8BCBA491B8E73BE67C9A78B98E4' }
-    'zh-TW' = [Language] @{ Name = 'Chinese (Traditional)';  Mapping = 'cht'; Checksum = 'CE04122DBEDA8B88C690F666368ADAE6D73669E1196450819F5D1633BD1AC48F' }
+    'br'    = [Language] @{ Name = 'Brazilian (Portuguese)'; Mapping = 'ptb'; Checksum = 'b01bb14599bc9222b98bfc937be4c87edea8eccc3aa7d09b5800f26e38c503bb' }
+    'de'    = [Language] @{ Name = 'German';                 Mapping = 'deu'; Checksum = '369154d14a483531722d490550c4cbc2944e6c7153840ad55e0e2b3d1b269f62' }
+    'en'    = [Language] @{ Name = 'English';                Mapping = 'enu'; Checksum = 'edcc5e277289ba0b526f6dc9f732087dac004de54c64e95f9049f68218662ee6' }
+    'es'    = [Language] @{ Name = 'Spanish';                Mapping = 'esn'; Checksum = 'f7be52c654ce9207ab2904bbc745fc62b9787e02e35c09b7cf459e10d9d65f59' }
+    'fr'    = [Language] @{ Name = 'French';                 Mapping = 'fra'; Checksum = '4711573dc4e54b18928221af9dd0f9927b741b026a742968bd4cf5af428cd137' }
+    'it'    = [Language] @{ Name = 'Italian';                Mapping = 'ita'; Checksum = '8f95b4b6063383360f4fbe818e276f04d5866fbb60e7b3bd0a7970b8d9387b6c' }
+    'ja'    = [Language] @{ Name = 'Japanese';               Mapping = 'jpn'; Checksum = '0de8342018ea328434e5a26217e64f042aac0f892eaa1ad9ec009563e52c9623' }
+    'ko'    = [Language] @{ Name = 'Korean';                 Mapping = 'kor'; Checksum = '67d7d947562d72d9484db9d6a831eeb91157df8699418fc8fdaf64db606e641c' }
+    'ru'    = [Language] @{ Name = 'Russian';                Mapping = 'rus'; Checksum = '417144f34dbbda8ffa18219b3bb6cd94a28f1095211e8b161f3e3122e8eb9fac' }
+    'zh'    = [Language] @{ Name = 'Chinese';                Mapping = 'chs'; Checksum = '0c60dfe98bfd37f3d3e1f6f5afc4a40365e5a8bcba491b8e73be67c9a78b98e4' }
+    'zh-CN' = [Language] @{ Name = 'Chinese (Simplified)';   Mapping = 'chs'; Checksum = '0c60dfe98bfd37f3d3e1f6f5afc4a40365e5a8bcba491b8e73be67c9a78b98e4' }
+    'zh-TW' = [Language] @{ Name = 'Chinese (Traditional)';  Mapping = 'cht'; Checksum = 'ce04122dbeda8b88c690f666368adae6d73669e1196450819f5d1633bd1ac48f' }
 }
 
-$base = 'https://download.microsoft.com/download/4/c/3/4c31fbc1-62cc-4a0b-932a-b38ca31cabcd'
+$base = 'https://download.microsoft.com/download/4/c/3/4c31fbc1-62cc-4a0b-932a-b38ca31cd410'
 
 $pp = Get-PackageParameters
 
