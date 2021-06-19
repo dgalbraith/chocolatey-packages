33e301921c473becf81682aba8d68e5e2b359a86
dgalbraith <dg@lbraith.io>
2021-06-19 04:16:47 +0000

    AU: 4 updated - mongodb-shell sqlite.analyzer sqlite.shell vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ae8b3e3cd0cff2cc32ceeef552ff8fc2b8320f48

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.945818462'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.949309867'
