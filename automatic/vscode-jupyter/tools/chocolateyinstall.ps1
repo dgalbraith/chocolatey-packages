445eda4c28bb3ad7b8a733b7f155e9050c4d0082
dgalbraith <dg@lbraith.io>
2021-06-22 04:17:06 +0000

    AU: 4 updated - kui usbdeview vscode-jupyter vscode-prettier [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/5cd4507d02ff757283804153430a50bae70dfce4

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.949309867'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.956490266'
