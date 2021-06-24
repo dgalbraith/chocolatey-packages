e703d62561ecd4f09188751ed24e6dcf589990fa
dgalbraith <dg@lbraith.io>
2021-06-24 04:17:45 +0000

    AU: 3 updated - vscode-jupyter vscode-prettier vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/350363443170caa583a7d3d137b0e733f49bdd7f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.960048241'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.963708582'
