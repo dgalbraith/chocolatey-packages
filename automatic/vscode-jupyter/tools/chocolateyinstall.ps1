708ee57b33299eb3adfa8bf1ac97ab99a558fdc9
dgalbraith <dg@lbraith.io>
2021-06-30 04:17:00 +0000

    AU: 2 updated - vscode-cloud-code vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3ec359cd28afbb46bebe42c4b7569df1547984a1

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.978384885'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.7.982069998'
