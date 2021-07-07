6457803d69423c7cda26a73e756940a45938e88d
dgalbraith <dg@lbraith.io>
2021-07-07 00:37:20 +0000

    AU: 4 updated - kui vscode-jupyter vscode-pylance vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/78fdd7ec3132dc5f777f92540ab6737a42f3db1e

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.8.1000539794'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.8.1003809038'
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.6.3"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.7.1"
