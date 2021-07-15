eb73753a27155757a021a1d3be3ec2571f2f8e41
dgalbraith <dg@lbraith.io>
2021-07-15 04:17:04 +0000

    AU: 2 updated - dotcover-cli vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/93abc4b699c915acf05353e84c4f43d40c435251

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.7.2"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.7.3"
