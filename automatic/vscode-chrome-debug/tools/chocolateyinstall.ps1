bc07007dece239f8d4685e53fe6d49b36b0185ad
dgalbraith <dg@lbraith.io>
2021-08-05 04:16:50 +0000

    AU: 5 updated - mongodb-shell vscode-chrome-debug vscode-haskell vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/f48cf8531d02d78cae5489e385ee6bbe2983eb08

--- automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-chrome-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.12.12.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\msjsdiag.debugger-for-chrome-4.13.0.vsix"
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.7.7"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.8.0"
