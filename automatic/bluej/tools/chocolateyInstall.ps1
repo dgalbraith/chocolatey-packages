7b454a90e4b44b68a287b9cc96d168f2235507e6
dgalbraith <dg@lbraith.io>
2021-08-07 04:17:12 +0000

    AU: 2 updated - bluej vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/6bf0628d12e0114794a354ca421f2a49e6bf6c26

--- automatic/bluej/tools/chocolateyInstall.ps1
+++ automatic/bluej/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'msi'
-  Url64Bit       = 'https://www.bluej.org/download/files/BlueJ-windows-501.msi'
-  Checksum64     = 'd44b73c0c758abc2e57caaf6f835409a686fd37817c01c0ea319d3d3cce9a01c'
+  Url64Bit       = 'https://www.bluej.org/download/files/BlueJ-windows-502.msi'
+  Checksum64     = 'f022a962381ccc6e9f5cd7085bc830a6b0d1f211a310659d23c24dafe374c44c'
   ChecksumType64 = 'sha256'
   SilentArgs     = '/quiet /qn /norestart'
   ValidExitCodes = @(0,3010)
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2021.8.1102490946.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2021.8.1105858891.vsix"
