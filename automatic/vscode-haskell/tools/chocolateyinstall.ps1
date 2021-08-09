d7f9a4ea4f93850e20b71853b365b01d98e83e6a
dgalbraith <dg@lbraith.io>
2021-08-09 04:16:52 +0000

    AU: 2 updated - vscode-haskell vscode-test-explorer [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4e46c4df50ce07aeded7a9a98a0ac52d67cc4b25

--- automatic/vscode-haskell/tools/chocolateyinstall.ps1
+++ automatic/vscode-haskell/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-1.5.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-1.6.0.vsix"
