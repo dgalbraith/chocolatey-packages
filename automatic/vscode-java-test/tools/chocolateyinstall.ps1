224e9cfa546401e8e6cfca95131a6697ca264cb9
dgalbraith <dg@lbraith.io>
2021-08-10 04:16:55 +0000

    AU: 3 updated - vscode-go vscode-java-test vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/d6a28c4ed4d599edc849b0f8d8fc8bb12fbe6cbe

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.31.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.31.1.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.8.1195043623'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.8.2022228455'
