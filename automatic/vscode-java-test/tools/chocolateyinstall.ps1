6a65c1b35031d0cfe3c6c1af9b0d862a77483042
dgalbraith <dg@lbraith.io>
2021-08-24 04:16:57 +0000

    AU: 3 updated - vscode-java-test vscode-jupyter vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/9ef24011ac0383e4bd7413263da3cc93fe67415e

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.31.2.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.31.3.vsix"
