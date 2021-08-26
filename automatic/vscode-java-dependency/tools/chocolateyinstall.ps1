6c26d17a2664afca7dc4706d61cd9faf07f9e9b6
dgalbraith <dg@lbraith.io>
2021-08-26 04:16:58 +0000

    AU: 5 updated - vscode-haskell vscode-java-dependency vscode-jupyter vscode-maven vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7e2ff0ec5d2900f025b9a6ea6e5d0dff1f9900c0

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.18.6.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.18.7.vsix"
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.8.2"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.8.3"
