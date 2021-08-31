00399f780b5a26146809f226996e06a4d01b33ce
dgalbraith <dg@lbraith.io>
2021-08-31 04:17:10 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-drawio vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4ab4debbf2890c0986da844743d4f4a26bdc03b8

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-1.13.2-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-2.0.0-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
