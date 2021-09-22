c066b82128c1e80720aa1c7460dce572996b2d68
dgalbraith <dg@lbraith.io>
2021-09-22 04:17:01 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable kui vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ae34067abf3fc59305c6cb1a52265100433bec2a

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-2.1.3-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-2.1.4-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
