da335e05a3c209f8cf6aa4404837992b0d50b3e4
dgalbraith <dg@lbraith.io>
2021-09-24 04:17:04 +0000

    AU: 3 updated - vscode-java-debug vscode-java-test vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/1e3a1247b34ab386b500741b6f18f8240545073f

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.31.3.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.32.0.vsix"
