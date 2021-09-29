b6f28b550ba86552753f387c64f0972997992a1d
dgalbraith <dg@lbraith.io>
2021-09-29 04:16:54 +0000

    AU: 2 updated - vscode-jupyter vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a7e9b0bae2349fbd550028bf289fbf1ee63989b1

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.9.1001280315'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.9.1001281857'
