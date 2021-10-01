21261e84f16413e98f1d83593e26c68e8bd01cd6
dgalbraith <dg@lbraith.io>
2021-10-01 04:17:19 +0000

    AU: 5 updated - kui mongodb-cli mongodb-cli.install vscode-arduino vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/425841bb1af6e601b9cbcca0f725a01fff181cba

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.9.1001286107'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.9.1001290345'
