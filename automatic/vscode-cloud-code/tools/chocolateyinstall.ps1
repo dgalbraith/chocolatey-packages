525169ac835620827e0048856a6dad7016e89889
dgalbraith <dg@lbraith.io>
2021-10-08 04:17:17 +0000

    AU: 4 updated - mongodb-shell vscode-cloud-code vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/4afcc798c4ec18fe1e9cf4ef2086bb54a73d4468

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.14.1'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.15.0'
