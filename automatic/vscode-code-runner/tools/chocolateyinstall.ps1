8ef7f6f74bc334664899aad83b2871813fba28ce
dgalbraith <dg@lbraith.io>
2021-10-11 04:16:46 +0000

    AU: 1 updated - vscode-code-runner [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/0a71da5d05efa3e3d4ee4898ece1ec52c3732d3e

--- automatic/vscode-code-runner/tools/chocolateyinstall.ps1
+++ automatic/vscode-code-runner/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\formulahendry.code-runner-0.11.5.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\formulahendry.code-runner-0.11.6.vsix"
