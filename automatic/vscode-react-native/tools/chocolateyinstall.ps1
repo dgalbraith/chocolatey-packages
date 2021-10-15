9bf332462b5c5115596ef34ca1bd8a431977b9f0
dgalbraith <dg@lbraith.io>
2021-10-15 04:17:09 +0000

    AU: 4 updated - kui vscode-jupyter vscode-pylance vscode-react-native [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a29566ce9ce319d06d9bf1d2c04a52d4d2d6ed93

--- automatic/vscode-react-native/tools/chocolateyinstall.ps1
+++ automatic/vscode-react-native/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@1.7.0"
+Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@1.7.1"
