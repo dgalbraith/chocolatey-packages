ef237add7e3b984243f2d983c16c167551ecb540
dgalbraith <dg@lbraith.io>
2021-10-27 04:17:07 +0000

    AU: 3 updated - nushell nushell.install vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/eaa41f42bd9b7e577c8119f49abcdcb8d3975a0f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.10.1001380329'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.10.1001386521'
