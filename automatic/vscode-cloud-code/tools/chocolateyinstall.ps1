22716fb8b0fbbc7a430e2f5f2d14b5dd71fd516f
dgalbraith <dg@lbraith.io>
2021-11-19 04:16:53 +0000

    AU: 3 updated - nheko-reborn vscode-cloud-code vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/1c4975877031e21a06a956f65485b2238fc2f8aa

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.16.0'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.16.1'
