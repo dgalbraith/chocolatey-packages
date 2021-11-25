80cb57b4e484f0e84302bb85adb366feb2c0c1b9
dgalbraith <dg@lbraith.io>
2021-11-25 04:16:54 +0000

    AU: 5 updated - mongodb-shell vscode-java-debug vscode-java-test vscode-jupyter vscode-spring-initializr [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/f570a724e4a69b46cc7b3fc33041eb9aa99125be

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.36.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.37.0.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.11.1001493992'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.11.1001498571'
