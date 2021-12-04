af2b5ba2851bbe8272683a6b91dc2009533dcba4
dgalbraith <dg@lbraith.io>
2021-12-04 04:16:55 +0000

    AU: 3 updated - vscode-mongodb vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/df3af4110308a6e2762bc31a3c15bc5a79212c2d

--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.6.14.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-0.7.0.vsix"
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.11.2"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2021.12.0"
