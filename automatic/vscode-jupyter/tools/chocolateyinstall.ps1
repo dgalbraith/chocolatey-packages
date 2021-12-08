76cdcfcb85ba981dc268ad6df66c94ab89c4fe5a
dgalbraith <dg@lbraith.io>
2021-12-08 04:16:58 +0000

    AU: 5 updated - nushell nushell.install vscode-haskell vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/06a6c00bce0ccc479efc3a3f800a82a28db0c2fa

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.11.1001532801'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.11.1001550889'
