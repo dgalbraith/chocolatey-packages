578feb30dd96bb7e119f842fd43f44a01823d92d
dgalbraith <dg@lbraith.io>
2021-12-11 04:16:59 +0000

    AU: 3 updated - dotcover-cli vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/df7b60b8d730d2b18af18f43ac317ba33427639f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2021.11.1001550889'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001562662'
