a28abb1618bd5d78f885bafa656d99f03397414b
dgalbraith <dg@lbraith.io>
2021-12-22 04:16:47 +0000

    AU: 2 updated - nheko-reborn vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/6ec4603874e6d84614e15b83434aadbb0f41c989

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001601240'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001605991'
