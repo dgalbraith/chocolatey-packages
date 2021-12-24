18d89d7902d5c39a19bb7c86dfeb8ade22911299
dgalbraith <dg@lbraith.io>
2021-12-24 04:16:52 +0000

    AU: 5 updated - vscode-firefox-debug vscode-java-dependency vscode-jupyter vscode-maven vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/937850c0f13b8b37685b91917b4feebda3563532

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001605991'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001614873'
