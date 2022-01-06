995bd6079cfaa075f3f299de2ef30248d5091924
dgalbraith <dg@lbraith.io>
2022-01-06 04:17:05 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/0e0d19da0014db09139b1fee74b6573ede491d11

--- automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
@@ -8,6 +8,6 @@ foreach ($file in $files) {
   New-Item "$file.ignore" -type file -force | Out-Null
 }
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.0.3-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.0.7-portable.exe'
 
 Install-BinFile -UseStart -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.0.3-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.0.7-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
