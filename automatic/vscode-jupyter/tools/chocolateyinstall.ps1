25bbd11ccf005f8f1aa324e77d765e87dfc6b1be
dgalbraith <dg@lbraith.io>
2022-01-08 04:17:09 +0000

    AU: 3 updated - vscode-jupyter vscode-prettier vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/922078e6a7aa8d1d02025dfd21b7e8a22b67857d

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001614873'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001666841'
--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.35.2021123010.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.35.2022010609.vsix"
