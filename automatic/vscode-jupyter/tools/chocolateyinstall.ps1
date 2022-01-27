d84da7568985a684590dbd993cf02aee4659fa55
dgalbraith <dg@lbraith.io>
2022-01-27 04:17:05 +0000

    AU: 3 updated - kui vscode-jupyter vscode-todo-tree [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ff5d70df48c19329b7e5ef0cbae1881a6cbfbc01

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001744497'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.1.1001749921'
