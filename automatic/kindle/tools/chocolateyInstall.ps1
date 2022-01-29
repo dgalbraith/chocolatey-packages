e4acdb5d10251291e56cc37041e010948de6a114
dgalbraith <dg@lbraith.io>
2022-01-29 04:16:55 +0000

    AU: 3 updated - kindle kyma-cli vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/9faaada5c318955797fa9f7a90ead2288c1b9e51

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.amazonaws.com/62002/KindleForPC-installer-1.33.62002.exe'
-  checksum      = '6f5616d9ccf8b76ce394a71ef5313aacfa833ade4e3cfcb2a61b35193d2d6b38'
+  url           = 'https://kindleforpc.s3.amazonaws.com/63103/KindleForPC-installer-1.34.63103.exe'
+  checksum      = '3dc62b3895954fc171d4a3d08f2b7a1a503233e373c163adb7bc7fd34cdeff49'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
