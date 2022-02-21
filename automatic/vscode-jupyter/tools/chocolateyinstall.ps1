7c1bf5b494db08f1998f80ca6d140b316b45ff43
dgalbraith <dg@lbraith.io>
2022-02-21 21:45:23 +0000

    AU: 2 updated - vscode-jupyter vscode-prettier [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/6932d645b66333aee983d766b6d305717a03a00f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.2.1010501003'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.2.1010531003'
