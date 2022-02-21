2fbef7fc8092fcdc9bf55b85879315802af45936
dgalbraith <dg@lbraith.io>
2022-02-21 22:40:50 +0000

    (gh-371) Correct package update script
    The package update script was not correctly handling updates due to the
    portable package filename being mapped from the intended .zip file to a
    .msi file.
    
    This was occuring on file retrieval in AU due to a fallback
    to a .msi file.  With no FileType being specified the downloaded file was
    being saved as a .msi rather than a .zip and this was propagated
    forward which resulted in an incorrect filename being used.
    
    An explicit filetype needed to be specified in the case of the zip file.

--- automatic/mongodb-cli.portable/tools/chocolateyinstall.ps1
+++ automatic/mongodb-cli.portable/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'mongocli_1.19.0_windows_x86_64.msi'
+$archive = Join-Path $toolsDir 'mongocli_1.19.0_windows_x86_64.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
