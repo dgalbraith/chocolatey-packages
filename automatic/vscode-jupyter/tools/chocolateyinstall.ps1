906f7741d1bcc02217a5d09255ec7be32fb2cd91
dgalbraith <dg@lbraith.io>
2022-02-28 22:14:13 +0000

    AU: 3 updated - kui mongodb-cli vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/97bb3d6d5ee5b7e81e4960b86507d2af0a69399d

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.2.1010571005'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.2.1010601211'
