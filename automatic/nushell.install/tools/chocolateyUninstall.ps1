a15e451110e9a2b639c62d5c65ac7367b37c0862
dgalbraith <dg@lbraith.io>
2022-02-28 22:07:18 +0000

    (gh-370) Correct names used for shim generation
    Shims for the package were being installed with an additional .exe
    appended.  The names used for shim generation needed updating to remove
    the .exe as this is added automatically in Install-BinFile - if a file
    already has the .exe extension it would be added again resulting in a
    shim ending with .exe.exe.

--- automatic/nushell.install/tools/chocolateyUninstall.ps1
+++ automatic/nushell.install/tools/chocolateyUninstall.ps1
@@ -4,5 +4,5 @@ $uninstallKey    = Get-UninstallRegistryKey -SoftwareName 'Nu'
 $installLocation = $uninstallKey.InstallLocation
 
 Get-ChildItem $installLocation -recurse -include '*.exe' | foreach-object {
-  Uninstall-BinFile -Name $_.Name -Path $_.FullName
+  Uninstall-BinFile -Name ($_.Name -Replace '\..*') -Path $_.FullName
 }
