3454ab9c57d1665355f19877af6b71bdc7b57bc1
dgalbraith <dg@lbraith.io>
2022-03-04 04:17:02 +0000

    AU: 5 updated - vscode-jupyter vscode-oracle-devtools vscode-pylance vscode-react-native vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/166a74bf57db4e67ba7d28467b982ae7952f464d

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000632050'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000642447'
--- automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
+++ automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-21.3.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-21.4.0.vsix"
