9242db18e6ea6be9be0c1cff5a487f639b1f6a49
dgalbraith <dg@lbraith.io>
2022-03-10 04:17:19 +0000

    AU: 4 updated - nheko-reborn vscode-go vscode-jupyter vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/972795ee16ef5afa0ca6edf488dc985e58f76cf0

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2022.3.0"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2022.3.1"
