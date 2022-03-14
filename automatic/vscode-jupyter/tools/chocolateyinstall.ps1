b19bdaccf1eb110bebc4682bce28dcfeff114649
dgalbraith <dg@lbraith.io>
2022-03-14 21:45:27 +0000

    AU: 4 updated - vscode-jupyter vscode-kubernetes-tools vscode-pull-request-github watchman [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ada439fcb2ed5278d30d2419e7bff92b5390a259

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000721005'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000751005'
