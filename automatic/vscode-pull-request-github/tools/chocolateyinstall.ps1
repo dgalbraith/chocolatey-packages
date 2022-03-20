f3c9344ed928418550314372e7acb2378b2f2d20
dgalbraith <dg@lbraith.io>
2022-03-20 04:17:06 +0000

    AU: 2 updated - vscode-maven vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/826152772fe9f5c3e889ae5830e53e620a202e66

--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.39.2022031409.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.39.2022031709.vsix"
