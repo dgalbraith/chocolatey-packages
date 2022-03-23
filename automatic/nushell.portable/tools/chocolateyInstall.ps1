1036641145d07e681705fa132eee5e4a7e051ec5
dgalbraith <dg@lbraith.io>
2022-03-23 04:17:07 +0000

    AU: 6 updated - nushell nushell.install nushell.portable vscode-jupyter vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/03defa4af219cdc3daab71189ee0a73722b98fe6

--- automatic/nushell.portable/tools/chocolateyInstall.ps1
+++ automatic/nushell.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $unzipArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  FileFullPath64 = Join-Path $toolsDir 'nu_0_44_0_windows.zip'
+  FileFullPath64 = Join-Path $toolsDir 'nu_0_60_0_windows.zip'
   Destination    = $toolsDir
 }
 
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000821004'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.3.1000831004'
