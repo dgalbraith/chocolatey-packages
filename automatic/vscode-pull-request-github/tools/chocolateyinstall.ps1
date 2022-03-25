6fe2ce22aa9c69eea0ee79d0ce49601b0fa662b9
dgalbraith <dg@lbraith.io>
2022-03-25 04:17:54 +0000

    AU: 3 updated - nheko-reborn vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/2568860175bc7982d8fb2ee1fc369425f7e34a25

--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.39.2022032209.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.39.2022032409.vsix"
