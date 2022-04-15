1b8fb2d83c971ce6e93defff198f23f6ed5e8430
dgalbraith <dg@lbraith.io>
2022-04-15 04:17:38 +0000

    AU: 4 updated - vscode-haskell vscode-java vscode-java-debug vscode-java-dependency [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ba2609c7194f3dd413dbc8c414fc1f973864d5a5

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.19.2022041402.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.19.2022041500.vsix"
