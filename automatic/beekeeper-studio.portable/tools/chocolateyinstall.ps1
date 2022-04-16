3032f82137405fda4a53f59952da04c50776cc61
dgalbraith <dg@lbraith.io>
2022-04-16 04:17:08 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/eb957a13ca28d225e87c10f366c43b07bdba6675

--- automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
@@ -8,6 +8,6 @@ foreach ($file in $files) {
   New-Item "$file.ignore" -type file -force | Out-Null
 }
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.3.8-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.4.1-portable.exe'
 
 Install-BinFile -UseStart -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.3.8-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.4.1-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
