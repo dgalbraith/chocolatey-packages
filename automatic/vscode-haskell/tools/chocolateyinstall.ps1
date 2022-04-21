dace71df1423d91af4284c3de5cc2aae8f9c72c2
dgalbraith <dg@lbraith.io>
2022-04-21 04:17:11 +0000

    AU: 6 updated - dbgate.portable vscode-haskell vscode-java-test vscode-jupyter vscode-pylance vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/21e17e05a17197b9ef6f7072ffeaf77461873853

--- automatic/vscode-haskell/tools/chocolateyinstall.ps1
+++ automatic/vscode-haskell/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.1.2.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.1.3.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.5.11091652.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.5.11101004.vsix"
