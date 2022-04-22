03bc9fd9f218cb74e8d5044d1a714d9f3a114978
dgalbraith <dg@lbraith.io>
2022-04-22 04:16:55 +0000

    AU: 3 updated - vscode-intellicode vscode-jupyter vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/cc776ec5be338ac23b8ad37c40cd729ae55ad4f6

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.4.1001122341'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.4.1001131006'
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.5.11101004.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.5.11111003.vsix"
