4897caa0a494b77e2d8a7e94ffe974c0608eda5a
dgalbraith <dg@lbraith.io>
2022-04-26 04:16:57 +0000

    AU: 3 updated - fluidsynth vscode-jupyter vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/cec14186ae057942d6cd901ebace968f537b3159

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.4.1001141006'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.4.1001171005'
