e9681dd56122985223245e39c157f6504d2ac094
dgalbraith <dg@lbraith.io>
2022-05-13 04:16:59 +0000

    AU: 3 updated - vscode-intellicode vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/a3d56eaa4aca532077fb0114837c7fa36970f1a3

--- automatic/vscode-intellicode/tools/chocolateyinstall.ps1
+++ automatic/vscode-intellicode/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.20'
+Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.21'
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001331008'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001341010'
