2d7453898b746c254c2662c490fae4258f98772f
dgalbraith <dg@lbraith.io>
2022-05-19 04:17:47 +0000

    AU: 7 updated - kui vscode-java vscode-java-test vscode-jupyter vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/3893bb97011e18fa71335e014c540edaf7a1c69f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001391015'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001401022'
