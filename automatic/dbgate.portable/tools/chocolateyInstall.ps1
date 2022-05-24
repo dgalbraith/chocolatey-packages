77bbc87dd8454cb36957d6c8444494504c0203f9
dgalbraith <dg@lbraith.io>
2022-05-24 04:17:14 +0000

    AU: 5 updated - dbgate dbgate.install dbgate.portable vscode-jupyter watchman [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/15d02f72ff89d584c9af060c8d0ebbfda7dae10e

--- automatic/dbgate.portable/tools/chocolateyInstall.ps1
+++ automatic/dbgate.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'dbgate-4.8.8-win_x64.zip'
+$archive = Join-Path $toolsDir 'dbgate-5.0.0-win_x64.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
--- automatic/watchman/update.ps1
+++ automatic/watchman/update.ps1
@@ -46,7 +46,7 @@ function global:au_GetLatest {
   # there are no windows binaries available.  The nuspec has not been parsed at this stage so the current version is not
   # available in the environment - hardcode here and use the package update process to rewrite
   if ([string]::IsNullOrWhiteSpace($version)) {
-    $version = '2022.05.16.00'
+    $version = '2022.05.23.00'
   }
 
   return @{
