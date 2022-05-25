b1ce0ebc447cce95088b1262b8b26fdf931fdf88
dgalbraith <dg@lbraith.io>
2022-05-25 04:18:48 +0000

    AU: 7 updated - dbgate dbgate.install dbgate.portable nushell nushell.install nushell.portable vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/09bcf30b48b680d27365331aad34e4db0eeeb931

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001451024'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.5.1001461016'
