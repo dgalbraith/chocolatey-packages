52433e27dbf32300e7ba926c0edbb90365f6822a
dgalbraith <dg@lbraith.io>
2022-06-09 04:18:34 +0000

    AU: 1 updated - vscode-cloud-code [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/f9b6b7abcb3044407fd7dd2fd2b94d707d5baa60

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.18.2'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@1.18.3'
