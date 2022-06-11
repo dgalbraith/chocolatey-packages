0482a786d324e1e1530593f7a6e49ce5bc911379
dgalbraith <dg@lbraith.io>
2022-06-11 04:17:37 +0000

    AU: 8 updated - dbgate dbgate.install dbgate.portable vscode-java vscode-java-dependency vscode-java-test vscode-jupyter vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/56367f58b678963c863ff79b2bbe6d08317e862d

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.8.2022060904.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.8.2022061004.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001621021'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001631011'
