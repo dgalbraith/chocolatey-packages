24737b0935dd0990c23612c977915ceabdcd7d1f
dgalbraith <dg@lbraith.io>
2022-06-17 04:17:24 +0000

    AU: 4 updated - vscode-firefox-debug vscode-java vscode-java-dependency vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/661dc331469a98f6d456b9f954d7fad58af1653d

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.19.2022061600.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.19.2022061700.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001681045'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001691033'
