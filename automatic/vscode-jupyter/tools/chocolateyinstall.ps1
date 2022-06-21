34a4eda0956b2671d72596552263df96c9dc23d7
dgalbraith <dg@lbraith.io>
2022-06-21 04:17:05 +0000

    AU: 3 updated - vscode-jupyter vscode-pull-request-github watchman [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/cb052106941cd5d264f100e2ad627d356997af8a

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001691033'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001740425'
--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.45.2022061609.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.45.2022062009.vsix"
