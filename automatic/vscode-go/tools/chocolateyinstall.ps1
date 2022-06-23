2348d5e61c4db6945f0f2896e51f30126ffd7851
dgalbraith <dg@lbraith.io>
2022-06-23 04:17:12 +0000

    AU: 7 updated - vscode-go vscode-java-dependency vscode-jupyter vscode-maven vscode-pull-request-github vscode-pylance vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/995f5ce3d7600518d3f1670bf286e60bdb7849bd

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.33.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.34.0.vsix"
--- automatic/vscode-maven/tools/chocolateyinstall.ps1
+++ automatic/vscode-maven/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-maven-0.35.2022061703.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-maven-0.35.2022062203.vsix"
