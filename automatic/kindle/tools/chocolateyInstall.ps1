3beb9e3fcaca2062c0243988fbb880411b37862e
dgalbraith <dg@lbraith.io>
2022-06-25 04:17:39 +0000

    AU: 3 updated - kindle vscode-java vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/45c26bc24a9d54185af7575aab44ae87e2f88b4d

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.amazonaws.com/65107/KindleForPC-installer-1.36.65107.exe'
-  checksum      = '1c3879dda9b11f96e7232896e6fab0168c7536fd966594f1f4072df536bd0fd5'
+  url           = 'https://kindleforpc.s3.amazonaws.com/65274/KindleForPC-installer-1.37.65274.exe'
+  checksum      = '11f6501142960337f7106ce6acfe9ec26d6d3289c5190b3f154272b2517de760'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.8.2022062104.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.8.2022062304.vsix"
