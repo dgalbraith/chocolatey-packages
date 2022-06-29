2d7f2fbe218ef58042d093956c9147a7c05b62c7
dgalbraith <dg@lbraith.io>
2022-06-29 04:17:48 +0000

    AU: 3 updated - vscode-java-debug vscode-jupyter vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/72444bea7f6df8ac5b6d4455ff14e04c7094a08f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001801035'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.6.1001811740'
