6a2336aaec849fcf7809d12fc5f3852f14e99e1e
dgalbraith <dg@lbraith.io>
2022-07-05 04:17:11 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable cloc watchman [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/0b2ae0c5806654cedaad0f957e00de08862c0528

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.4.3-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-3.5.1-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/cloc/tools/chocolateyUninstall.ps1
+++ automatic/cloc/tools/chocolateyUninstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $binArgs = @{
   name = $env:ChocolateyPackageName
-  path = "$toolsDir\cloc-1.92.exe"
+  path = "$toolsDir\cloc-1.94.exe"
 }
 
 Remove-BinFile @binArgs
