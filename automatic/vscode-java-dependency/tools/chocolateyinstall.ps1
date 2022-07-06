b4898148bc37bcd7611e6c747e7223aa5c2c3478
dgalbraith <dg@lbraith.io>
2022-07-06 23:49:37 +0000

    AU: 5 updated - kyma-cli vscode-java-dependency vscode-jupyter vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/decc35baa43f840762641b647a6c9013d7d84147

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.20.2022063000.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.20.2022070600.vsix"
