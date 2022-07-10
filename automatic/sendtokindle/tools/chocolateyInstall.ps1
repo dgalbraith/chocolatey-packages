76648667ff514558a85742f0de27dfcfcfa52fd4
Piotr Walesiuk <49117833+pwalesiuk@users.noreply.github.com>
2022-07-10 16:56:05 +0100

    (gh-401) Update sendtokindle version to 1.1.1.253
    Sendtokindle has been updated.  The package needs to be Updated to version 1.1.1.253 to match.
    
    Closes gh-357
    Fixes gh-401

--- automatic/sendtokindle/tools/chocolateyInstall.ps1
+++ automatic/sendtokindle/tools/chocolateyInstall.ps1
@@ -7,6 +7,6 @@ Install-ChocolateyPackage `
     -FileType 'EXE' `
     -Silent '/S' `
     -Url 'http://s3.amazonaws.com/sendtokindle/SendToKindleForPC-installer.exe' `
-    -Checksum '2A6F3D5F6E0477934177FE442D52861EDCE78086E3FDBBDA9FA19954C5391477' `
+    -Checksum '38A173669B3056F1C81E24145153BE2B781C2C9E44E0EAFD01E5C93D6DEEB1F0' `
     -ChecksumType 'SHA256' `
     -ValidExitCodes @(0, 1223)
