b026fbb8c11c81dd44431d9c3d4c1f501f881b61
dgalbraith <dg@lbraith.io>
2022-07-25 04:17:18 +0000

    AU: 3 updated - dbgate dbgate.install dbgate.portable [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/0694a16fcc0acb6ae1b2cf604a772a7a01da89a5

--- automatic/dbgate.install/tools/chocolateyInstall.ps1
+++ automatic/dbgate.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$installer = Join-Path $toolsDir 'dbgate-5.0.8-win_x64.exe'
+$installer = Join-Path $toolsDir 'dbgate-5.0.9-win_x64.exe'
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
