8b0559b901c673787f4691d0e01bd84c6e31aac0
dgalbraith <dg@lbraith.io>
2022-07-26 04:17:13 +0000

    AU: 3 updated - flashpoint-launcher vscode-jupyter vscode-maven [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/f9cd04d657d83b1f7a64e090a83ae0407373b28f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.7.1002051049'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.7.1002081042'
