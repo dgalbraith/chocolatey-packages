f02334169ba25de5bfa7bac47d9362d6c6cae01f
dgalbraith <dg@lbraith.io>
2022-08-01 04:17:16 +0000

    AU: 1 updated - flashpoint-launcher [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e3f3ca041a7a4653e4d5fc9f86ba43847867f4bb

--- automatic/flashpoint-launcher/tools/chocolateyinstall.ps1
+++ automatic/flashpoint-launcher/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $toolsDir   = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $installDir = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
 
 if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
-  $fileName = 'Flashpoint-10.1.3_win-ia32.7z'
+  $fileName = 'Flashpoint-10.1.4_win-ia32.7z'
 } else {
-  $fileName = 'Flashpoint-10.1.3_win-x64.7z'
+  $fileName = 'Flashpoint-10.1.4_win-x64.7z'
 }
 
 $archive = Join-Path $toolsDir $fileName
