cf3020be0ff5cb292a759d2277af1c818512e0c8
dgalbraith <dg@lbraith.io>
2022-08-07 04:16:55 +0000

    AU: 2 updated - vscode-pull-request-github vscode-spring-initializr [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/cd1fc4707254d2929226d7e7fdf097fe701d9fad

--- automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
+++ automatic/vscode-pull-request-github/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.47.2022080211.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\GitHub.vscode-pull-request-github-0.49.2022080416.vsix"
