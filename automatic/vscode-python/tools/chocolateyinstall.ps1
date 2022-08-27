480517dd72dc464f5279203241631ba9bf10f8e7
dgalbraith <dg@lbraith.io>
2022-08-27 04:17:05 +0000

    AU: 2 updated - vscode-jupyter vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7a2ec883f5cf45d5ce11d3958fbf0040ae10fd3b

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.13.12371004.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.13.12381007.vsix"
