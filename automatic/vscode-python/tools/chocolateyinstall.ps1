d36c1b3a3fbd6625f4508768095b1be8ba9f96f7
dgalbraith <dg@lbraith.io>
2022-09-15 04:17:38 +0000

    AU: 7 updated - kui vscode-java vscode-java-dependency vscode-jupyter vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/9f5dd056b35e4a82bac2f3ac27dbbd7a923e9425

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.15.12532020.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.15.12571012.vsix"
