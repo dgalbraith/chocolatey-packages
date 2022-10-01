687cd59f903fab2859cde41194e59870306e655d
dgalbraith <dg@lbraith.io>
2022-10-01 23:43:48 +0000

    AU: 10 updated - dbgate dbgate.install dbgate.portable fluidsynth koodo-reader kyma-cli mongodb-shell nushell nushell.install nushell.portable [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/c01420d4f7eb18a79ac407159391d3f44d6ea39e

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.3.9.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.4.8.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
