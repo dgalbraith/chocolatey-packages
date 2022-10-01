94adc45e679d16350b0502c56022b99e617c13cb
dgalbraith <dg@lbraith.io>
2022-10-01 04:18:33 +0000

    AU: 2 updated - vscode-java-debug vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/eddf69a869cb82588117d30e88c837e77476dadd

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.15.12711056.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2022.15.12731017.vsix"
