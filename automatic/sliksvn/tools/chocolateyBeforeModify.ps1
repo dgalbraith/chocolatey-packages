b267e75f09b8b38719fd35971139558b5420e864
dgalbraith <dg@lbraith.io>
2022-10-16 17:39:45 +0100

    (gh-441) Correct sliksvn package updates
    The sliksvn package was no longer updating due to a change in the
    resource format - the package binary distribution had changed from raw
    msi files to msi files additionally packaged in zip files.
    
    Modified the updater to successfully retrieve and handle the msi files
    packaged in zip files and enhanced the package to utilise shims to
    provide a defaut option of not adding the install\bin directory to the
    path.

---
+++ automatic/sliksvn/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$uninstallKey    = Get-UninstallRegistryKey -SoftwareName 'Slik Subversion*'
+$installLocation = $uninstallKey.InstallLocation
+
+Get-ChildItem $installLocation -recurse -include '*.exe' | foreach-object {
+  Uninstall-BinFile -Name ($_.Name -Replace '\..*') -Path $_.FullName
+}
--- automatic/sliksvn/tools/chocolateyInstall.ps1
+++ automatic/sliksvn/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageDir = $($toolsDir | Split-Path -parent)
 
-$silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log`""
+$silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log`" INSTALLLEVEL=300"
 
 $pp = Get-PackageParameters
 
@@ -10,6 +11,15 @@ if ($pp.InstallDir) {
   Write-Verbose("$env:ChocolateyPackageName will be installed to $($_.value)")
   $silentArgs += " INSTALLLOCATION=`"$($pp.InstallDir)`""
 }
+
+$unzipArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileFullPath   = Join-Path $toolsDir 'Slik-Subversion-1.14.1-win32.zip'
+  FileFullPath64 = Join-Path $toolsDir 'Slik-Subversion-1.14.1-x64.zip'
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
@@ -23,23 +33,58 @@ $packageArgs = @{
 # cache the current system path prior to package install for a restore in the case where /NoPath is specified
 $path = Get-EnvironmentVariable -name 'Path' -scope 'Machine' -PreserveVariables
 
-Write-Host('SilentArgs: ' + $silentArgs)
-
 Install-ChocolateyInstallPackage @packageArgs
 
-if (!$pp.AddToStart) {
+if (-Not $pp.AddToStart) {
   # the installer adds the documentation folder automatically so remove unless it has been explicitly requested
-  Write-Host('Removing documentation folder from the start menu')
+  Write-Verbose('Removing documentation folder from the start menu')
   $subversionStart = Join-Path ([Environment]::GetFolderPath('CommonStartMenu')) 'Programs' | Join-Path -ChildPath 'Subversion'
 
   if (Test-Path $subversionStart) {
     Remove-Item -Path $subversionStart -Recurse -Force | Out-Null
   }
-} 
+}
 
-if ($pp.NoPath) {
+if (-Not $pp.AddToPath) {
   # the installer adds the bin folder of the install to the path so remove it if the path inclusion is not desired
   # removal is achieved by setting the path to the version cached immediately prior to the package install
   Write-Verbose("Restoring cached path $path")
   Install-ChocolateyEnvironmentVariable -VariableName 'PATH' -VariableValue $path -VariableType Machine
+
+  # the bin folder has not been added to the path so generate shims instead
+  if ($pp.InstallDir) {
+    $installLocation = $pp.InstallDir
+  } else {
+    if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
+      $installLocation = [Environment]::GetFolderPath('ProgramFilesX86') + [IO.Path]::DirectorySeparatorChar + 'SlikSvn'
+    } else {
+      $installLocation = [Environment]::GetFolderPath('ProgramFiles') + [IO.Path]::DirectorySeparatorChar + 'SlikSvn'
+    }
+  }
+
+  Get-ChildItem $installLocation -recurse -include '*.exe' | foreach-object {
+    Install-BinFile -Name ($_.Name -Replace '\..*') -Path $_.FullName
+  }
+}
+
+# update the registry key to set the InstallLocation - the installer leaves this blank
+$installKey   = Get-InstallRegistryKey -SoftwareName 'Slik Subversion*'
+$registryPath = $installKey.PSPath
+
+if (Test-Path $RegistryPath) {
+  New-ItemProperty -Path $RegistryPath -Name 'InstallLocation' -Value $installLocation -PropertyType String -Force | Out-Null
+}
+
+# locate the zip content file for the installed version in the package folder
+$zipContentFile = Get-ChildItem -Path "$packageDir\*" -Filter '*.zip.txt' | Select-Object -ExpandProperty FullName
+
+if ((Test-Path -path $zipContentFile)) {
+  # extract the name of original .zip file that the contents was unpacked from and use the Chocolatey helper to remove the contents
+  $zipPackage = $zipContentFile -Match '(?<ZipContentFile>^(?<Path>(?<Drive>[a-zA-Z]:)(?:\\[^:]+)?)\\(?<FileName>[^\\\n]+?)(?<Extension>\.[^.]*$|$))' | Foreach-Object { $Matches.FileName }
+  Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName $zipPackage
+}
+
+# remove the additional files added through the installation process
+foreach ($file in Get-ChildItem -Path "$packageDir\*" -Include @('*.zip.txt','*.ignore') -Recurse | Select-Object -ExpandProperty FullName) {
+  Remove-Item $file -ErrorAction SilentlyContinue -Force
 }
