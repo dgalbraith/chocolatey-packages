06421e763b77976f80a52bfba2ae0f8bf9819202
dgalbraith <dg@lbraith.io>
2022-10-27 04:19:49 +0000

    AU: 6 updated - kyma-cli vscode-java vscode-jupyter vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/2cc651953c7bbb293bc1744a94fabd00ea65fca9

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.10.1103001107'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.10.1103012217'
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2022.10.31"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2022.10.41"
