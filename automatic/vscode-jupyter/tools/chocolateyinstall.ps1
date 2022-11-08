e53a7fa534ea00012c4a05988c0b46bf547a39f2
dgalbraith <dg@lbraith.io>
2022-11-08 04:19:13 +0000

    AU: 3 updated - vscode-go vscode-java-dependency vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/931f770d4552a85c27196cf994c4a90e6a203b23

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003101024'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003131031'
