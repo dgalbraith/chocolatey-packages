1075d5396972c9bd23b7ab37dbc26f34533f8346
dgalbraith <dg@lbraith.io>
2022-11-09 04:18:49 +0000

    AU: 8 updated - dotcover-cli kyma-cli nushell nushell.install nushell.portable vscode-jupyter vscode-pull-request-github vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/5931dd343f7c4a4132b8179eb703ef90f48aaa83

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003131031'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003141027'
