e7e8ba8348511c6e001196ca567bcfdc54e5126b
dgalbraith <dg@lbraith.io>
2022-11-11 04:18:44 +0000

    AU: 4 updated - vscode-java vscode-jupyter vscode-pull-request-github vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/e4fed79cefaf0aa3e48bf2ab8e4548016bb91193

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003160202'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2022.11.1003161031'
--- automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.8.2022110900.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.8.2022111100.vsix"
