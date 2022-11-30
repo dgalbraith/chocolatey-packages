3c4c64348e05089311f34b27f664fa36b7345ceb
dgalbraith <dg@lbraith.io>
2022-11-30 04:19:45 +0000

    AU: 5 updated - nushell nushell.install nushell.portable vscode-java vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/40ece102b2dec53da36b120a700a886fe0170aaa

--- automatic/nushell.portable/tools/chocolateyInstall.ps1
+++ automatic/nushell.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $unzipArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  FileFullPath64 = Join-Path $toolsDir 'nu-0.71.0-x86_64-pc-windows-msvc.zip'
+  FileFullPath64 = Join-Path $toolsDir 'nu-0.72.0-x86_64-pc-windows-msvc.zip'
   Destination    = $toolsDir
 }
 
--- automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.8.2022112500.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.9.2022113000.vsix"
