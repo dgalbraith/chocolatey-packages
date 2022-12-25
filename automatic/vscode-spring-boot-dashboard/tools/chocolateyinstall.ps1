310d5fc50d4f9d3c2e537265312fdfb1cea4a7d0
dgalbraith <dg@lbraith.io>
2022-12-25 04:18:54 +0000

    AU: 1 updated - vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ad09f5eeca164e75059a5147f90cf9fe4f3df066

--- automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.9.2022121600.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.9.2022122400.vsix"
