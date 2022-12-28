19a1d21c9104d4e9f7bd0ae1e351f5576db0ed89
dgalbraith <dg@lbraith.io>
2022-12-28 04:18:54 +0000

    AU: 2 updated - kyma-cli vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/ee2b051398d437f861c0ebf31a228a4bb680687d

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.1003591019'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.1003632427'
