0a2faed20c69ca53d025ec62c665716424157f79
dgalbraith <dg@lbraith.io>
2022-12-30 04:19:22 +0000

    AU: 3 updated - fluidsynth vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/1eb1051579cd428a0ee76a8579aae8a78c942db4

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.1003632427'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.1003651031'
