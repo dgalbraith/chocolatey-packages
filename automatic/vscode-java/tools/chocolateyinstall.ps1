0146893e55c5c136b2cf3571bf73c04de77d013a
dgalbraith <dg@lbraith.io>
2023-01-06 04:20:01 +0000

    AU: 3 updated - vscode-java vscode-pull-request-github vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/fa07e8b159b414ac08d03b0db9a2ddc916a88949

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.14.2022122003.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.14.2023010503.vsix"
--- automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot-dashboard/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.9.2023010400.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-boot-dashboard-0.9.2023010600.vsix"
