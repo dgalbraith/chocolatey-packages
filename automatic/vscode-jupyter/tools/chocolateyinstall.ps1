92416a85af37c0f5f7a5dd511f62d630c82ffb5e
dgalbraith <dg@lbraith.io>
2023-01-12 04:18:35 +0000

    AU: 2 updated - vscode-jupyter vscode-spring-boot-dashboard [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/6d8aec43c42159fcf343f9427efb0266c3efa7e4

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.2000102234'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.1.2000121059'
