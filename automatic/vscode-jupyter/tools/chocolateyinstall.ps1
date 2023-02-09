6e4a65225bb02a70b28c3e93c7d218c0e449180a
dgalbraith <dg@lbraith.io>
2023-02-09 04:18:40 +0000

    AU: 2 updated - remote-desktop-client vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/00d2bf4c09a986d103c2d5cf40e9d4d93b1dc412

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000392435'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000401035'
