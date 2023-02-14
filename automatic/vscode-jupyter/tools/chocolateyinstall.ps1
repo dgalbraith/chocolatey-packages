fcb570697d0bd6c62f8e29b66ac79748e992c513
dgalbraith <dg@lbraith.io>
2023-02-14 04:19:18 +0000

    AU: 3 updated - kyma-cli mongodb-shell vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/edb59b5d4dc5b265e239a57196ed2642312fa23c

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000421627'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000451018'
