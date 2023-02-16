1a95e72981c5fa1f5165719ec5f5a07ea4d56a38
dgalbraith <dg@lbraith.io>
2023-02-16 04:20:13 +0000

    AU: 4 updated - vscode-java-debug vscode-java-dependency vscode-java-test vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/8aa68041005b23aacec9e5fbc8931c9592f6363d

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.21.2023013100.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.21.2023021500.vsix"
