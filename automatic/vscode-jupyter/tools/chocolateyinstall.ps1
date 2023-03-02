f08fa8375ae3065f182d22edd88f72fcd2934075
dgalbraith <dg@lbraith.io>
2023-03-02 00:25:13 +0000

    AU: 2 updated - vscode-jupyter watchman [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/5ef0ec98297e3bb64e7c989446a3164f0960456e

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000561009'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.2.1000592019'
