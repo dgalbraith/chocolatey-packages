37ab111311d04b0419d2918740e99f45195f3560
dgalbraith <dg@lbraith.io>
2023-03-15 04:19:20 +0000

    AU: 5 updated - nushell nushell.install nushell.portable vscode-java vscode-jupyter [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/6a2b3b0c20d7fe68a42046136f17aad2b233c061

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.16.2023031304.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.16.2023031404.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.3.1000711009'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.3.1000752049'
