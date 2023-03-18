55d04a15e63d081fc29f4c2329470a2a60bb301f
dgalbraith <dg@lbraith.io>
2023-03-18 04:19:45 +0000

    AU: 9 updated - dbgate.install dbgate.portable kyma-cli mongodb-cli mongodb-cli.install mongodb-cli.portable nushell.install nushell.portable vscode-pylance [skip ci] https://gist.github.com/aef61e2663a09f3dfdfa40482177e040/7f1c237f03cb5ce5c5bcea0514210aee52061378

--- automatic/dbgate.install/tools/chocolateyInstall.ps1
+++ automatic/dbgate.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$installer = Join-Path $toolsDir 'dbgate-5.2.4-win_x64.exe'
+$installer = Join-Path $toolsDir 'dbgate-5.2.5-win_x64.exe'
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2023.3.23"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2023.3.25"
