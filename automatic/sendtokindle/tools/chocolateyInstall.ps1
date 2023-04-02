d7a4e24ec6ea019087e433a8744077423daba872
dgalbraith <dg@lbraith.io>
2023-04-02 19:00:45 +0100

    (gh-453) Add automatic update handling
    Package was previously not updating automatically.
    
    Added support for automated updates leveraging the ETag hanlding helper
    function to determine when the binary has been udpated/a new version may
    be available.

--- automatic/sendtokindle/tools/chocolateyInstall.ps1
+++ automatic/sendtokindle/tools/chocolateyInstall.ps1
@@ -6,13 +6,12 @@ $url      = 'http://s3.amazonaws.com/sendtokindle/SendToKindleForPC-installer.ex
 $checksum = '38A173669B3056F1C81E24145153BE2B781C2C9E44E0EAFD01E5C93D6DEEB1F0'
 
 $packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  fileType     = 'exe'
-  url64bit     = $url
-  checksum     = $checksum
-  checksumType = 'sha256'
-  silentArgs   = '/S'
-  validExitCodes= @(0, 1223)
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = 'exe'
+  Url            = $url
+  Checksum       = $checksum
+  SilentArgs     = '/S'
+  ValidExitCodes = @(0, 1223)
 }
 
 Install-ChocolateyPackage @packageArgs
