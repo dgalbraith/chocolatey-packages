ce68cd895154237c541ffbb85aa63df73f1cc61a
dgalbraith <dg@lbraith.io>
2023-04-10 18:24:17 +0000

    AU: 4 updated - dotcover-cli vscode-haskell vscode-jupyter watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/d40f8e8a6b6df33046fe5b98c0d16430cd453a5c

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.4.1000991007'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.4.1001021018'
