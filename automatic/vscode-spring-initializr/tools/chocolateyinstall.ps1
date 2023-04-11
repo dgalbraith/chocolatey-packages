3fc83c142f68c91556a9084d0b30d9bfbb92d2d7
dgalbraith <dg@lbraith.io>
2023-04-11 04:19:48 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-spring-initializr [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/fbe401bd32d00769d44453136de986e92b01ce69

--- automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.11.2023031603.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.11.2023041103.vsix"
