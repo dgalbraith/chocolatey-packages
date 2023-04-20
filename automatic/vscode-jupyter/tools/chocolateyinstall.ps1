fec47ba5aa969e3ee7cb47484038034cd7aa6c6f
dgalbraith <dg@lbraith.io>
2023-04-20 04:18:56 +0000

    AU: 6 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-java vscode-jupyter vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/d8693a12b1aaa77f9f356831283b8bf693dbbc05

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.4.1001091014'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.4.1001111035'
