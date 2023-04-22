1bfad6dd28fdd409e0629b685e65cbbd64d64411
dgalbraith <dg@lbraith.io>
2023-04-22 04:18:52 +0000

    AU: 3 updated - vscode-java vscode-jupyter vscode-prettier [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/433e67711a877d6752ea3b6988ff95d19aef45e6

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-9.10.4.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-9.12.0.vsix"
