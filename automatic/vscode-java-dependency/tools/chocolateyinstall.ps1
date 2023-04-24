31ed90beeb1b010374955e4a75764905995220b6
dgalbraith <dg@lbraith.io>
2023-04-24 04:19:01 +0000

    AU: 2 updated - vscode-java vscode-java-dependency [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b7f903ef43ba4ad8b3cc44b947eb5199d90c7cbb

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.21.2023042200.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.22.2023042400.vsix"
