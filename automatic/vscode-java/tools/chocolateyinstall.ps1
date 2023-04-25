6804928d50c3996ad9c0a772ad457d018b13eec6
dgalbraith <dg@lbraith.io>
2023-04-25 04:19:19 +0000

    AU: 4 updated - kyma-cli mongodb-shell vscode-java watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9152db02be11c43f57af840c89b8e89d3fea2920

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.18.2023042309.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.18.2023042404.vsix"
