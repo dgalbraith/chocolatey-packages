0b7cbadc8deea5da7a6a0b4cfaa3133a30697a61
dgalbraith <dg@lbraith.io>
2023-04-27 04:18:58 +0000

    AU: 5 updated - dotcover-cli vscode-java vscode-java-dependency vscode-pylance vscode-spring-boot-dashboard [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/c582f80836a4c808e464a372d92e4db6e0142e0d

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.22.2023042400.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.22.2023042700.vsix"
