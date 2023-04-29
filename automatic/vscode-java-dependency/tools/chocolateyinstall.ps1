48b32e12bf2080b6be64bf797cc546a5807b9782
dgalbraith <dg@lbraith.io>
2023-04-29 04:19:07 +0000

    AU: 3 updated - mongodb-shell vscode-java vscode-java-dependency [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/94195889097b0d3c2aee61ca13cc2e34b73db6c9

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.22.2023042700.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.22.2023042900.vsix"
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.18.2023042604.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.19.2023042804.vsix"
