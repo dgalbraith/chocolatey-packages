273cd754ff86342a5849494c17a80ea70196585d
dgalbraith <dg@lbraith.io>
2023-05-18 04:18:40 +0000

    AU: 3 updated - dotcover-cli vscode-jupyter vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e2258866e9cab3075dcdd47a2263ed964da7beac

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.5.1001382346'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.5.1001391100'
