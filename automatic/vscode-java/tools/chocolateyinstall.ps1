41731fe6ef8090d51463dde61d405f58690ddabe
dgalbraith <dg@lbraith.io>
2023-05-22 04:18:51 +0000

    AU: 3 updated - koodo-reader vscode-java vscode-java-dependency [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6d327b1c627762caf7281ce92dfce913f9ad957b

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.19.2023051604.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.19.2023052004.vsix"
