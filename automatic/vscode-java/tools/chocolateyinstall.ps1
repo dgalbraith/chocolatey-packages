9cc3c9741d7a400ccc4171eba0844f0039c75676
dgalbraith <dg@lbraith.io>
2023-06-10 04:19:02 +0000

    AU: 2 updated - vscode-java vscode-kubernetes-tools [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/2bdd0626f838d219f704e72591b30ad9857f8a3b

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.20.2023060804.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.20.2023060904.vsix"
--- automatic/vscode-kubernetes-tools/tools/chocolateyinstall.ps1
+++ automatic/vscode-kubernetes-tools/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-kubernetes-tools.vscode-kubernetes-tools-1.3.12.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-kubernetes-tools.vscode-kubernetes-tools-1.3.13.vsix"
