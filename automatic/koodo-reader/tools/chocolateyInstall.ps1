9d889f55ef0f8cd43206ac95d1e7e5bbb20c46b9
dgalbraith <dg@lbraith.io>
2023-06-25 04:18:50 +0000

    AU: 2 updated - koodo-reader vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5b4ac150661351e89272b41759b1f9b22b8caaf8

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.5.5.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.5.6.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
