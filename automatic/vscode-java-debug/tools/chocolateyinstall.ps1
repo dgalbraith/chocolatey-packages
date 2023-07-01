2af902da3355be11eff3873b5cd495edd8966e28
dgalbraith <dg@lbraith.io>
2023-07-01 04:18:53 +0000

    AU: 4 updated - vscode-java vscode-java-debug vscode-prettier vscode-spring-initializr [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e31ebf75556359b1ddf151bb66e5b0f12b29d796

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.51.2023062607.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.52.2023063003.vsix"
