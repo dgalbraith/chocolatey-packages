2a535fecf02a479644e8f40a2075951d3091135b
dgalbraith <dg@lbraith.io>
2023-07-22 04:19:03 +0000

    AU: 2 updated - vscode-java-dependency vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/4b1363ccd37eb3d5db9281aa1e419d1250f66cc7

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023060200.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023072200.vsix"
