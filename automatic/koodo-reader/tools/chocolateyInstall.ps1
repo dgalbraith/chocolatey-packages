1e6ff49bdf142538dc9a5e5fdb015b6a0b8ae466
dgalbraith <dg@lbraith.io>
2023-07-29 04:18:49 +0000

    AU: 3 updated - koodo-reader mongodb-shell vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/eeed3e68474a447e24c2e0dab3e1cbb37e63afa4

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.5.6.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.5.7.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
