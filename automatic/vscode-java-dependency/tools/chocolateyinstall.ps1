2b1aa21b9cc39167c56a390ad71dff5d31aace31
dgalbraith <dg@lbraith.io>
2023-08-03 04:18:20 +0000

    AU: 3 updated - dotcover-cli vscode-java-dependency vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/42b479319c599918b5510d038affcb3a62ab4524

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023072200.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023080300.vsix"
