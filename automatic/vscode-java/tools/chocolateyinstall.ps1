fd78030c09c5950c3ad6a79cb2ee70f6b6f7e8ea
dgalbraith <dg@lbraith.io>
2023-08-04 04:19:02 +0000

    AU: 7 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-java vscode-java-dependency vscode-java-test vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b3cedb295cde74aaa95c858c82b0b0fd942e333a

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023073104.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023080304.vsix"
