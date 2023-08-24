e5c65881f7675d4b98d1246d9a5125cd65a1d4c0
dgalbraith <dg@lbraith.io>
2023-08-24 04:19:07 +0000

    AU: 3 updated - vscode-jupyter vscode-mongodb vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/46834c8a7d6e1603f023ff2c99f86c4b2de596db

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2023.8.31"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2023.8.41"
