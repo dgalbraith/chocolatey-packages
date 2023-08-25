f1437781f69ec5c8fb63cec4891120cdcf22352e
dgalbraith <dg@lbraith.io>
2023-08-25 04:19:24 +0000

    AU: 5 updated - dotcover-cli sqlite.analyzer sqlite.shell vscode-java vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9b750aff2d0bc0649e16e52965bf7aa836caf7ad

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023081804.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023082404.vsix"
