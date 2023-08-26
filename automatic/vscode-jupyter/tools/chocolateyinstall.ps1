bfa3dfbe7bf4eaf99d4748f16bf23d61f5f2d62f
dgalbraith <dg@lbraith.io>
2023-08-26 04:18:46 +0000

    AU: 3 updated - mongodb-shell vscode-java vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/4488dfd3458a4888fd87747f3ef4e59f09d4a64e

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.8.1002381100'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.8.1002391100'
