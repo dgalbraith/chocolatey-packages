a1a977c617e910cbb7f28999604a0d9c99f15c63
dgalbraith <dg@lbraith.io>
2023-09-08 04:18:52 +0000

    AU: 3 updated - mongodb-shell vscode-java vscode-java-test [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6b77ad7cbba87e741c22d04e6cb1b7850e6f82cc

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.39.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.39.2023090502.vsix"
