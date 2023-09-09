f9661f4df4cdf3287f899cf277081c5cfae3b7be
dgalbraith <dg@lbraith.io>
2023-09-09 04:18:59 +0000

    AU: 3 updated - rsync vscode-java vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6fd2b76a0262c9b77bf16f6238ce68f84289002a

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023090704.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.22.2023090804.vsix"
