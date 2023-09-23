d298312e662663831126003fa5820082f57f6f6f
dgalbraith <dg@lbraith.io>
2023-09-23 04:19:38 +0000

    AU: 4 updated - kyma-cli remote-desktop-client vscode-java-test vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b8196fe9ae2cb5b7dde6a78db97337a5d280e625

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.39.2023090502.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.39.2023092208.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002661100'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002671100'
