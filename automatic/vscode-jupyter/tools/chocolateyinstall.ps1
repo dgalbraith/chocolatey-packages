9aeb1b5caf211420f16caafcfa090d8746da71ee
dgalbraith <dg@lbraith.io>
2023-09-26 04:19:18 +0000

    AU: 3 updated - fluidsynth vscode-java-test vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/33ffd275d15f65d963ebc6a8a72e7901d5b4d115

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002671100'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002701100'
