3ef523ea7992cb2271e2f7e337420ba63ce3f5ac
dgalbraith <dg@lbraith.io>
2023-09-30 04:18:50 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b475ffba7d98cfcebc11db263721fc3b63e4ef03

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.0.0-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.0.3-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002740130'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.9.1002741100'
