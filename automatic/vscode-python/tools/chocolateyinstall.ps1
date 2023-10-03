5394415a1c92fb4dac98abc1b046b3614f248b0f
dgalbraith <dg@lbraith.io>
2023-10-03 04:18:55 +0000

    AU: 3 updated - kyma-cli vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/7154961c5edae516f488679eba6e9aad0bc2131b

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.17.12721006.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.17.12751008.vsix"
