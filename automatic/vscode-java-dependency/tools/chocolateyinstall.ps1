aa8fbf3b583ab1bef8a48c3335411cff13d7f2b6
dgalbraith <dg@lbraith.io>
2023-10-08 04:19:26 +0000

    AU: 6 updated - dbgate dbgate.install vscode-java vscode-java-dependency vscode-java-test vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/2f6fe995c6c7b5874216b0dcb32665d2f9e762cb

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023080400.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2023100802.vsix"
