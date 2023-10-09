8503199b72324c636792d76873027e5711248842
dgalbraith <dg@lbraith.io>
2023-10-09 04:19:13 +0000

    AU: 2 updated - vscode-java vscode-java-test [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e44195d21528e97f90c7595fd9bab8dc3f80b3db

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.40.2023100708.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.40.2023100808.vsix"
