0ac4a98d2e31825646fdffd95f83c40ec5ad3593
dgalbraith <dg@lbraith.io>
2023-10-10 04:19:01 +0000

    AU: 6 updated - rsync vscode-code-runner vscode-jupyter vscode-mongodb vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/2ec4a62982cae12219420b826daa817482ece19c

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.10.1002811100'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.10.1002841100'
