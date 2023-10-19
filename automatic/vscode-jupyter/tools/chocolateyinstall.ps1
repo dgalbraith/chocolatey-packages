6f7372a4ad06a6cb13c91eb3db0dcc7be28d3cba
dgalbraith <dg@lbraith.io>
2023-10-19 04:19:11 +0000

    AU: 5 updated - jruby vscode-java vscode-jupyter vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b22442a06f51ff1e2f4f6b00650d7a1b83222a70

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.10.1002921100'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2023.10.1002931100'
