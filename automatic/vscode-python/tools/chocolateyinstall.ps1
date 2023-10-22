b5fa8f0eb4aeef9140e0bfc66f2446b50188a6ef
dgalbraith <dg@lbraith.io>
2023-10-22 04:18:36 +0000

    AU: 1 updated - vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/69677d567e1ac85f97a742e588d22f09edbd07e3

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.19.12921011.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.19.12931008.vsix"
