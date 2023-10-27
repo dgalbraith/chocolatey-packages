025d914ff109c19740afc084ea2673c5aa3e4781
dgalbraith <dg@lbraith.io>
2023-10-27 20:39:59 +0000

    AU: 6 updated - naps2 naps2.install naps2.portable vscode-java vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b60cc424b80dda4e5f39a09ae6e73781eca6f03e

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.24.2023102504.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.25.2023102704.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.19.12991923.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.19.13001012.vsix"
