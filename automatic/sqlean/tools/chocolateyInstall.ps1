b9b59f0421f464442883819d04d49a99e344603e
dgalbraith <dg@lbraith.io>
2023-10-30 21:44:42 +0000

    (gh-437) Add package for sqlean Add an automatic package for the sqlean extensions to sqlite.

---
+++ automatic/sqlean/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$archive  = Join-Path $toolsDir 'sqlean-win-x64.zip'
+
+$pp = Get-PackageParameters
+
+if ($pp.InstallDir) {
+  Write-Verbose("$env:ChocolateyPackageName will be installed to $pp.InstallDir")
+  $installDir = $pp.InstallDir
+} else {
+  $installDir = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
+}
+
+$unzipArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileFullPath64 = $archive
+  Destination    = $installDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
