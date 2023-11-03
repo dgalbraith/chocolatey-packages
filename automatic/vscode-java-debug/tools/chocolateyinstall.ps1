d8e0cc131224bcc59b241943bffc9433365f9917
dgalbraith <dg@lbraith.io>
2023-11-03 04:19:32 +0000

    AU: 1 updated - vscode-java-debug [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5a30c8060c8649b0ce8fb57c7c19320d447db9ae

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.54.2023092623.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.55.0.vsix"
