754f5af0c96e1a6c09836a01877b2438f046b4cb
dgalbraith <dg@lbraith.io>
2023-11-10 04:19:04 +0000

    AU: 4 updated - openvpn vscode-java vscode-jupyter vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/42b721069347c0da81b30c2c3fb1803c98136f00

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.25.2023110304.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.25.2023110903.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.21.13071013.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.21.13140305.vsix"
