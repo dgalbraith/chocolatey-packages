6232910c15c3802fd6f0074be1060bd90b2a8f2c
dgalbraith <dg@lbraith.io>
2023-12-23 04:19:14 +0000

    AU: 4 updated - naps2 naps2.install vscode-jupyter vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/0f988a821e03b2b00a12bcbde7d3da3046886f64

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.1.2023121901'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.1.2023122201'
