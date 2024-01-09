b5cb74367f182a7f4024561cc52ae833cf7d7e5d
dgalbraith <dg@lbraith.io>
2024-01-09 04:19:03 +0000

    AU: 2 updated - fend vscode-oracle-devtools [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/347260de36c0d05075b984b68c6bb2c57a18b7ed

--- automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
+++ automatic/vscode-oracle-devtools/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-21.7.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\Oracle.oracledevtools-21.8.0.vsix"
