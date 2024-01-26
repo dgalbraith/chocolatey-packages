c67f9f92f36d5a5bdb4d9997312d4ba055ec0b85
dgalbraith <dg@lbraith.io>
2024-01-26 04:19:05 +0000

    AU: 5 updated - vscode-go vscode-java vscode-jupyter vscode-mongodb vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/7d8e1fd699010284ad8bcb3ffbc627ebbbddc6d4

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.27.2024012308.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.27.2024012408.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.1.2024012301'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.1.2024012502'
