4f67de9fce0ed436222c0a48b814a35365ce49c2
dgalbraith <dg@lbraith.io>
2024-02-01 04:21:32 +0000

    AU: 4 updated - mongodb-cli mongodb-cli.portable vscode-java vscode-java-debug [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/8d5bced91adb020e7996173ca89e964a8e86d94b

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.55.2023121302.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.56.2024013105.vsix"
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.27.2024013008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.27.2024013108.vsix"
