93cde51cd8fbf1d6c493654cd5dacb6082a6976f
dgalbraith <dg@lbraith.io>
2024-02-02 04:18:49 +0000

    AU: 1 updated - vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/92f43e1fe9cec8bf9b973b6c58a85657c6c795db

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.1.102"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.1.104"
