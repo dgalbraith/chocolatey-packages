a0d2b412336731b3d74e329d42c9f9584d8c35a4
dgalbraith <dg@lbraith.io>
2024-02-03 04:19:31 +0000

    AU: 4 updated - sqlean vscode-java vscode-jupyter vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/81fe739defaf1ad19ade360ecf43c406b2171e03

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.27.2024013108.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.28.2024020208.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2023.25.10292213.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.0.0.vsix"
