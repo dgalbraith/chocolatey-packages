46e5155b6559edb84c2ff8d0c42b6c7ae0fcd06e
dgalbraith <dg@lbraith.io>
2024-02-06 04:19:27 +0000

    AU: 2 updated - stunnel vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9ccd9cb3538681fb4969547ac51462ef9b44b795

--- automatic/stunnel/tools/chocolateyInstall.ps1
+++ automatic/stunnel/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  File           = "$toolsDir\stunnel-5.71-win64-installer.exe"
+  File           = "$toolsDir\stunnel-5.72-win64-installer.exe"
   SilentArgs     = '/S'
   ValidExitCodes = @(0)
 }
