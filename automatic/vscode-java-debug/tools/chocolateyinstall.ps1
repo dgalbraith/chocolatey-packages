3af36da19f2617a8b45c93f63a985184bb255d8a
dgalbraith <dg@lbraith.io>
2024-02-08 04:20:06 +0000

    AU: 4 updated - remote-desktop-client vscode-java-debug vscode-jupyter vscode-python-test-adapter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ba18e4874f186df742f8fdd94491bbe0c06bc378

--- automatic/vscode-java-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.56.2024013105.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-debug-0.56.2024020601.vsix"
--- automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
+++ automatic/vscode-python-test-adapter/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.8.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\LittleFoxTeam.vscode-python-test-adapter-0.8.2.vsix"
