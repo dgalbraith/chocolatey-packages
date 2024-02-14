6f758f9a4e0db7a5a2535579622b8fe216531d7e
dgalbraith <dg@lbraith.io>
2024-02-14 04:19:19 +0000

    AU: 5 updated - authme authme.install authme.portable vscode-java vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/917ea60307732807904609c8fb32469b07910dd1

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.2.1"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.2.100"
