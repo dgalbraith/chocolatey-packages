7521ffc9f49e3c50bfa7497ffd00288c986df8ef
dgalbraith <dg@lbraith.io>
2024-02-21 04:19:30 +0000

    AU: 7 updated - authme authme.install authme.portable jruby vscode-java vscode-jupyter vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e329ca9a3d0fa7354011c78160ea1d8c4aebb80f

--- automatic/authme.install/tools/chocolateyInstall.ps1
+++ automatic/authme.install/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$installer  = Join-Path $toolsDir 'authme-5.0.0-windows-x64.msi'
+$installer  = Join-Path $toolsDir 'authme-5.0.1-windows-x64.msi'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.29.2024021608.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.29.2024022008.vsix"
