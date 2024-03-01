24c151d93ac167cea6e1e98d961f1a0145df8ba7
dgalbraith <dg@lbraith.io>
2024-03-01 04:22:37 +0000

    AU: 2 updated - androidstudio vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/361349b73d9529891dac67c0e660d68581227ed4

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.2.103"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.2.105"
