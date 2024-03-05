c03d45b6fa0338e3a23796e7c2e425a01064d0d7
dgalbraith <dg@lbraith.io>
2024-03-05 04:19:45 +0000

    AU: 3 updated - vscode-jupyter vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/0184715ef5b2177c487868319d9609e00ac13e2a

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.3.10611008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.3.10641005.vsix"
