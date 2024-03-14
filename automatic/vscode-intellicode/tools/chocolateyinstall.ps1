513f33edb05ed060b1879a8f5a3a2d115b03d3d9
dgalbraith <dg@lbraith.io>
2024-03-14 04:19:19 +0000

    AU: 8 updated - fend fend.install fend.portable remote-desktop-client rsync sqlean vscode-intellicode vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/60a45d8e99d62202f33b6cce8bae6f5d485608d0

--- automatic/vscode-intellicode/tools/chocolateyinstall.ps1
+++ automatic/vscode-intellicode/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.2.30'
+Install-VsCodeExtension -extensionId 'VisualStudioExptTeam.vscodeintellicode@1.3.0'
