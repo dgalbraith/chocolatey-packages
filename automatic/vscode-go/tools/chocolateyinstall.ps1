69db74364db463d0dcf0cac2264af10ac5250514
dgalbraith <dg@lbraith.io>
2024-03-17 04:19:19 +0000

    AU: 3 updated - vscode-go vscode-java vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/219d2aaaf955acc9f2cf65f81e8ff91b258abc25

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.41.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.41.2.vsix"
