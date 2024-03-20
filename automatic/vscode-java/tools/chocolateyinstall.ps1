4cc45e5a108c2dd28e50cf9e4e43fa4c4c4f916f
dgalbraith <dg@lbraith.io>
2024-03-20 04:19:23 +0000

    AU: 4 updated - mongodb-shell vscode-cloud-code vscode-java vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/74ed32b267b0f067243249fa35eab212fc0d2282

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.29.2024031808.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.29.2024031908.vsix"
