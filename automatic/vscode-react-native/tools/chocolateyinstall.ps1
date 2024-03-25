ba014a2b6eaf08483a72e45ec27ddbfd1560f326
dgalbraith <dg@lbraith.io>
2024-03-25 04:19:14 +0000

    AU: 1 updated - vscode-react-native [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/2f79c6642a837ba592d8efe7ed87d0b7104c1657

--- automatic/vscode-react-native/tools/chocolateyinstall.ps1
+++ automatic/vscode-react-native/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@1.12.3"
+Install-VsCodeExtension -extensionId "msjsdiag.vscode-react-native@1.13.0"
