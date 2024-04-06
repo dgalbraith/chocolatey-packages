dbcace54123ed8f7258c5b810c74f3b087fa4e7e
dgalbraith <dg@lbraith.io>
2024-04-06 04:18:59 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-code-runner vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/4308cd3c7925dd629c0118f8d7ff8077089b32f5

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.3.2024032701'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.4.2024040501'
