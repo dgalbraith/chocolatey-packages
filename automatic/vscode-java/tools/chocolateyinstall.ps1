5d25f05f1ee89e4c428d62fa651bf9ad54046be6
dgalbraith <dg@lbraith.io>
2024-04-10 04:20:59 +0000

    AU: 6 updated - androidstudio dotcover-cli fluidsynth vscode-java vscode-jupyter vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/2835eaac42e92696271f3ba429fffc4de91c65f5

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.30.2024040408.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.30.2024040908.vsix"
