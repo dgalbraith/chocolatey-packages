892d8fa94b9e725efc93dc0b298c185389dab258
dgalbraith <dg@lbraith.io>
2024-04-17 04:19:29 +0000

    AU: 3 updated - openvpn vscode-java vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/936d642e0846e6f7c13f7f4051562e8647dbf4e8

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.4.2024041201'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.4.2024041603'
