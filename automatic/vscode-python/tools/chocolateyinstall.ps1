0321817b968938f90c27082e2003b685a4637206
dgalbraith <dg@lbraith.io>
2024-04-19 04:19:28 +0000

    AU: 5 updated - remote-desktop-client vscode-java vscode-jupyter vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b16fa5694b5d470624aebcdebbd1f3bc476be2b8

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.5.11082240.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.5.11091012.vsix"
