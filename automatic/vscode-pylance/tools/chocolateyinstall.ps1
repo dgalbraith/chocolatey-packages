039269972aacec49e7693b2062588dd0a32d7a62
dgalbraith <dg@lbraith.io>
2024-05-02 04:19:23 +0000

    AU: 2 updated - remote-desktop-client vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5ec79ad9d169474fac5e9204dd9501f88050f557

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.4.105"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.4.106"
