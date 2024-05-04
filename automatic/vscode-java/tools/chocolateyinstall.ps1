a11c107bb51a76b7669032b76fe914604d81f302
dgalbraith <dg@lbraith.io>
2024-05-04 04:19:37 +0000

    AU: 5 updated - fend fend.install fend.portable vscode-java vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b6954b0353654763e4bc4e480ba4eeda6caecc45

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.31.2024050208.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.31.2024050308.vsix"
