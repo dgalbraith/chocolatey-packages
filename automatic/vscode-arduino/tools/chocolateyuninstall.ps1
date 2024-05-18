67b4698e32c91e0783958d520dfa5e2fbd9305d6
dgalbraith <dg@lbraith.io>
2024-05-18 11:49:03 +0000

    AU: 1 updated - vscode-arduino [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ebaee697b8b5713c9749ce82392a8ae9cb84592c

--- automatic/vscode-arduino/tools/chocolateyuninstall.ps1
+++ automatic/vscode-arduino/tools/chocolateyuninstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Uninstall-VsCodeExtension -extensionId 'vsciot-vscode.vscode-arduino-0.4.11'
+Uninstall-VsCodeExtension -extensionId 'vsciot-vscode.vscode-arduino-0.6.230727001'
