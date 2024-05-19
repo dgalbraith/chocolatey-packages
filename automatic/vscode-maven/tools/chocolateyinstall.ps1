7179afef7a5ea5c94baf8fef5daae22e3c06aad6
dgalbraith <dg@lbraith.io>
2024-05-19 17:43:18 +0000

    AU: 1 updated - vscode-maven [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e4aba38938d3056aca36ff1f2e34f60930c31dc0

--- automatic/vscode-maven/tools/chocolateyinstall.ps1
+++ automatic/vscode-maven/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-maven-0.38.2022090603.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\vscjava.vscode-maven-0.44.2024013105.vsix"
