2cc3a2a52cb708b7910b77478eb6e04047a71b2f
dgalbraith <dg@lbraith.io>
2024-05-23 04:19:44 +0000

    AU: 3 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/165864aba469427631774a1121ec22453d73fafa

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.3.1-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.3.4-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
