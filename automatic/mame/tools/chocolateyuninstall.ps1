bddced4153d145cc32765623c2e699536ccfb43d
dgalbraith <dg@lbraith.io>
2024-05-27 21:36:13 +0000

    AU: 6 updated - mame vscode-java vscode-jupyter vscode-pull-request-github vscode-python vscode-spring-boot-dashboard [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ddd15d242c936e50a9589162ab77354d4b957efd

--- automatic/mame/tools/chocolateyuninstall.ps1
+++ automatic/mame/tools/chocolateyuninstall.ps1
@@ -4,4 +4,4 @@ Get-ChildItem $installLocation -recurse -include '*.exe' | foreach-object {
     Uninstall-BinFile -Name ($_.Name -Replace '\..*') -Path $_.FullName
   }
 
-Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName 'mame0264b_64bit.exe'
+Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName 'mame0265b_64bit.exe'
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.5.2024052401'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.5.2024052701'
