6716e1deab323267a2914896330cf2d997ca3fd2
dgalbraith <dg@lbraith.io>
2024-06-25 04:20:25 +0000

    AU: 6 updated - dotcover-cli kindle mongodb-shell vscode-jupyter vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/4c6014aac93e60ce0ff828a67b0567b802816d74

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.amazonaws.com/70840/KindleForPC-installer-2.3.70840.exe'
-  checksum      = 'bc8e142402d72db80a27028b4c4c0b0531284647e386108481288466a8c9755e'
+  url           = 'https://kindleforpc.s3.amazonaws.com/70904/KindleForPC-installer-2.4.70904.exe'
+  checksum      = '2e2e4e5bb9fd585947244a4a62ce5baca47818c439d0213cc9a5a96f9a692119'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.6.2024062001'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.6.2024062401'
