d262f037a397cffb7709320336b5bc10daa0c550
dgalbraith <dg@lbraith.io>
2024-06-26 04:20:40 +0000

    AU: 8 updated - nushell nushell.install nushell.portable vscode-cloud-code vscode-java vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e79187bdd71e1d6973bae650082188f2f37451ef

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.12.0'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.13.0'
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.9.11762332.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.9.2024062507.vsix"
