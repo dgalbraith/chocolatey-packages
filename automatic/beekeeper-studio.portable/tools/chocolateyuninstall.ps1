d1c9fa7687ce20c867457311d1c000068d1aa695
dgalbraith <dg@lbraith.io>
2024-06-28 04:20:33 +0000

    AU: 8 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable kyma-cli openvpn vscode-java-debug vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/8a5de2bec3ebe352afaada44d40f10207a2210b8

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.6.0-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-4.6.2-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
