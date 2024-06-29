731bd50d3f22bf0ee81745eb49b0f19c5f543532
dgalbraith <dg@lbraith.io>
2024-06-29 04:19:34 +0000

    AU: 3 updated - vscode-java-dependency vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/93a7a331f38a2022608f38f6a1cd2508c8c4991e

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2024062100.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.23.2024062807.vsix"
