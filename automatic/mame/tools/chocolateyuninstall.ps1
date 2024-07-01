aef2c6775c055f27127ac814122e18d14278fa2a
dgalbraith <dg@lbraith.io>
2024-07-01 04:19:28 +0000

    AU: 1 updated - mame [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/00af44ff70b320ac3f39bdcbc45db9786b700f7a

--- automatic/mame/tools/chocolateyuninstall.ps1
+++ automatic/mame/tools/chocolateyuninstall.ps1
@@ -4,4 +4,4 @@ Get-ChildItem $installLocation -recurse -include '*.exe' | foreach-object {
     Uninstall-BinFile -Name ($_.Name -Replace '\..*') -Path $_.FullName
   }
 
-Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName 'mame0266b_64bit.exe'
+Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName 'mame0267b_64bit.exe'
