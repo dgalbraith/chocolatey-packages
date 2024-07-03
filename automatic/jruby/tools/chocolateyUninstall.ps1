daf5f332d7e82f268a1d700d56dcbc20dd3ba27c
dgalbraith <dg@lbraith.io>
2024-07-03 04:19:37 +0000

    AU: 1 updated - jruby [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1f8fe187275ee66c92cc1f7fd7644055e56d7e4a

--- automatic/jruby/tools/chocolateyUninstall.ps1
+++ automatic/jruby/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  PackageSearch  = 'jruby 9.4.7.0'
+  PackageSearch  = 'jruby 9.4.8.0'
   SilentArgs     = '-q'
   ValidExitCodes = @(0)
 }
