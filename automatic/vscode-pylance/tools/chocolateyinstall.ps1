9bb412943fdd14429f98305f3c3a1a3adb722f4c
dgalbraith <dg@lbraith.io>
2024-07-04 04:19:39 +0000

    AU: 2 updated - vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/3e5e007c8064fb189c2a9e2ee8c6b45c1b46ee43

--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.6.103"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.6.104"
