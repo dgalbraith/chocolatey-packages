f25efdf5de8974b8feeaeacf0e002add291f9bea
dgalbraith <dg@lbraith.io>
2024-07-10 04:20:15 +0000

    AU: 9 updated - authme authme.install authme.portable mongodb-shell remote-desktop-client vscode-cloud-code vscode-jupyter vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/bc63348fb46aaf2a6267ca04278f031d5e3c8fe9

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.13.0'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.14.0'
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024070802.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024070901.vsix"
