d5589a51291e00273f610aed52bf14d3d1853c52
dgalbraith <dg@lbraith.io>
2024-07-16 04:19:49 +0000

    AU: 5 updated - vscode-mongodb vscode-pull-request-github vscode-pylance vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/af3c4602a5ed39784e04357d26eb4cb66946297f

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024071403.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024071501.vsix"
--- automatic/watchman/tools/chocolateyinstall.ps1
+++ automatic/watchman/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'watchman-v2024.07.08.00-windows.zip'
+$archive = Join-Path $toolsDir 'watchman-v2024.07.15.00-windows.zip'
 
 $unzipArgs = @{
   PackageName = $env:ChocolateyPackageName
