d291827cab99dd41c2ab2d8dea076337c207464d
dgalbraith <dg@lbraith.io>
2024-07-17 04:20:19 +0000

    AU: 3 updated - vscode-java vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a85e7a6df77aa356660e9546da4026acea248ef6

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024071501.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.11.2024071602.vsix"
