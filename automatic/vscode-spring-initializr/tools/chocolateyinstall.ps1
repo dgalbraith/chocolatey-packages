8b422b7563e3f7ced66aada6fc65c019bd3b77d1
dgalbraith <dg@lbraith.io>
2024-07-30 04:20:54 +0000

    AU: 8 updated - nushell nushell.install nushell.portable vscode-maven vscode-pylance vscode-spring-boot-dashboard vscode-spring-initializr watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/13a0221acae3801852789730f81f6e04fcf7d9c8

--- automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-initializr/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.11.2023070103.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-spring-initializr-0.11.2024072912.vsix"
