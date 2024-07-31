cb3f9df9378ae11a9bb643afd5daf223de867c57
dgalbraith <dg@lbraith.io>
2024-07-31 04:20:18 +0000

    AU: 2 updated - mongodb-shell vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e11f82bde53cb43e91d4c67b06d6331cf9b4fb8f

--- automatic/mongodb-shell/tools/chocolateyinstall.ps1
+++ automatic/mongodb-shell/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'mongosh-2.2.12-win32-x64.zip'
+$archive = Join-Path $toolsDir 'mongosh-2.2.15-win32-x64.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.33.2024072708.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.33.2024073008.vsix"
