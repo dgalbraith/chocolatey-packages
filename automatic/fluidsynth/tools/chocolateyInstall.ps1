8f517236cf3dd03ec684ad462a34e32cd070673b
dgalbraith <dg@lbraith.io>
2024-08-04 04:19:48 +0000

    AU: 3 updated - fluidsynth sqlean vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1db8983437e08361f1f81d00c4b952f8ffa2e111

--- automatic/fluidsynth/tools/chocolateyInstall.ps1
+++ automatic/fluidsynth/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $toolsDir  = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 if ((Get-ProcessorBits 32) -eq 'true' -or $env:ChocolateyForceX86 -eq 'true') {
-  $archive = Join-Path $toolsDir 'fluidsynth-2.3.5-winXP-x86.zip'
+  $archive = Join-Path $toolsDir 'fluidsynth-2.3.6-winXP-x86.zip'
 }
 else {
-  $archive = Join-Path $toolsDir 'fluidsynth-2.3.5-win10-x64.zip'
+  $archive = Join-Path $toolsDir 'fluidsynth-2.3.6-win10-x64.zip'
 }
 
 $installDir = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
