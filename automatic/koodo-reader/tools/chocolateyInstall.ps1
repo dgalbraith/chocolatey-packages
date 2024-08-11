b55ae837d1ebff90c6fde490e6cb605ba2835782
dgalbraith <dg@lbraith.io>
2024-08-11 04:20:15 +0000

    AU: 5 updated - koodo-reader naps2 naps2.install naps2.portable vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/db478bfac4848121e539ca85a6f4cbca1148f7f8

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.6.7.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.6.9.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
