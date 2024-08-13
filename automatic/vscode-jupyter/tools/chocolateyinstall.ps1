40dbc7799a007e748dd5b6d13f3355cdc53c47d0
dgalbraith <dg@lbraith.io>
2024-08-13 04:19:31 +0000

    AU: 3 updated - vscode-jupyter vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/445a1f7b9d0579dfc08f38d4f71ccc94b9017425

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.8.2024080901'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.8.2024081201'
--- automatic/vscode-pylance/tools/chocolateyinstall.ps1
+++ automatic/vscode-pylance/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.8.100"
+Install-VsCodeExtension -extensionId "ms-python.vscode-pylance@2024.8.101"
