e133a3ca9ffb948513b941e3d8736bab18ede664
dgalbraith <dg@lbraith.io>
2024-08-15 04:19:05 +0000

    AU: 4 updated - vscode-java vscode-prettier vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a63bf683b3ca390bd30283a3820b038d467887e3

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-10.4.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-11.0.0.vsix"
