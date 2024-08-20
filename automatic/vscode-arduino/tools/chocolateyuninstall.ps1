b194d2e28ac624d37fe6a87270e78e1ed4a35492
dgalbraith <dg@lbraith.io>
2024-08-20 04:19:27 +0000

    AU: 2 updated - vscode-arduino vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a82f7a4c2a95faf3ea2093acb315d39f90cae6de

--- automatic/vscode-arduino/tools/chocolateyuninstall.ps1
+++ automatic/vscode-arduino/tools/chocolateyuninstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Uninstall-VsCodeExtension -extensionId 'vsciot-vscode.vscode-arduino-0.6.230727001'
+Uninstall-VsCodeExtension -extensionId 'vsciot-vscode.vscode-arduino-0.7.0'
