5c460c59d08a5d87b28ab24596a157f411c8dd71
dgalbraith <dg@lbraith.io>
2024-09-07 04:19:43 +0000

    AU: 1 updated - kindle [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/249c9511755660f9fe0d601d251f622ac37fe66d

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.amazonaws.com/70946/KindleForPC-installer-2.4.70946.exe'
-  checksum      = '37f4aab2c87d68214bdea017a62c6bafe7fabb2f1d9dc151f9e6c4ce1f232413'
+  url           = 'https://kindleforpc.s3.amazonaws.com/70951/KindleForPC-installer-2.5.70951.exe'
+  checksum      = '2df949a98aa90bf05e87d69755e1fec3dab8fa40458a768debc3444777a13541'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
