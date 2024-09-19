b96ec96e693a94ed1e41833a337b79d862737352
dgalbraith <dg@lbraith.io>
2024-09-19 04:19:27 +0000

    AU: 8 updated - nushell nushell.install nushell.portable remote-desktop-client vscode-jupyter vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/f98e2c06b7b61e98c81a1919615bf52d3611672b

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.9.2024091301'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.9.2024091801'
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.15.2024091601.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.15.2024091801.vsix"
