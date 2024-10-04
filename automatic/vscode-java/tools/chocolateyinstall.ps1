bc743e456eba74c89d2efcc9a4cc538881407d0f
dgalbraith <dg@lbraith.io>
2024-10-04 04:19:52 +0000

    AU: 3 updated - vscode-java vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/4250b066f01463b873cc188691d13d76de3fa82d

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.36.2024100208.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.36.2024100308.vsix"
