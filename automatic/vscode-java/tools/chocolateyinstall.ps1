2c8057aa4a00f0748bcb3c6e6c5946d996a4fe7d
dgalbraith <dg@lbraith.io>
2024-10-17 04:19:32 +0000

    AU: 6 updated - nushell nushell.install nushell.portable vscode-cloud-code vscode-java vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1628986d26dc0723cb8a2a3327d2290604ed6574

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.36.2024101108.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.36.2024101604.vsix"
