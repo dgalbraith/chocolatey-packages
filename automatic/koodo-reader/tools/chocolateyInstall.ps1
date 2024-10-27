588a74111d737c30b1462d33361e58200ee71527
dgalbraith <dg@lbraith.io>
2024-10-27 04:20:54 +0000

    AU: 4 updated - koodo-reader sqlean-shell sqlite-shell-x64 vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/474fe98081d8c7161fbf5d5b264770cf062804ad

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.7.0.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.7.1.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
