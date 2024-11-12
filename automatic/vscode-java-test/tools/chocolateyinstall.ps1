f2159bebd1ac0f081c79278187b1a75cad826b63
dgalbraith <dg@lbraith.io>
2024-11-12 04:20:06 +0000

    AU: 4 updated - vscode-java-test vscode-pull-request-github vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/90dda8b687240589b4fef73963b8055c8ce8ccd3

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.42.2024102102.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.43.0.vsix"
