a4645a45549d3405a4163a5384d8e6d2fa9e8c66
dgalbraith <dg@lbraith.io>
2024-11-14 04:20:21 +0000

    AU: 7 updated - dotcover-cli nushell nushell.install nushell.portable vscode-java vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/859d0f47f2abb87cbb6911c792620b7d25738a8f

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.37.2024110808.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.37.2024111308.vsix"
