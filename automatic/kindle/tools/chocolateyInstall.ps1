b8f70504a23d8548ab872c719537e47f7fa1f8f2
dgalbraith <dg@lbraith.io>
2024-11-19 04:21:06 +0000

    AU: 5 updated - kindle vscode-go vscode-jupyter vscode-pull-request-github watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/859c151c736f237f8814c5e5480131d589b84fd7

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.amazonaws.com/70951/KindleForPC-installer-2.5.70951.exe'
-  checksum      = '2df949a98aa90bf05e87d69755e1fec3dab8fa40458a768debc3444777a13541'
+  url           = 'https://kindleforpc.s3.us-east-1.amazonaws.com/70964/KindleForPC-installer-2.6.70964.exe'
+  checksum      = 'b1988b305420c68bfde3952b9bda35d567fa0836f616a68e708779578819c9bf'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
