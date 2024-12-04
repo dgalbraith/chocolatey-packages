cfa8887df632ac14ece9c497d74c089522411fc8
dgalbraith <dg@lbraith.io>
2024-12-04 04:19:17 +0000

    AU: 3 updated - vscode-java vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/8e5f36cab3edac626999050cbb696827475e691f

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.11.2024112701'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2024.11.2024120301'
