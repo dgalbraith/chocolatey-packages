2c4b2459eb230a4918e3568816c5ac8aa477a906
dgalbraith <dg@lbraith.io>
2024-12-12 04:20:00 +0000

    AU: 7 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable dotcover-cli vscode-firefox-debug vscode-mongodb vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9689643e7891d81ada658ca1062b6bb7bad65f02

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.0.6-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.0.7-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-1.10.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-1.11.0.vsix"
