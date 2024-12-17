0d8137438e3e4d55834bc90083dc2113c693d5a7
dgalbraith <dg@lbraith.io>
2024-12-17 04:19:26 +0000

    AU: 4 updated - vscode-go vscode-pylance vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/50482c0d8b33fda476b0365b697c90bb13692c56

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.43.4.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.44.0.vsix"
