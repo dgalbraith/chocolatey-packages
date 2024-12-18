06a789e97f814050cbe1a41720736f0c26500421
dgalbraith <dg@lbraith.io>
2024-12-18 04:20:05 +0000

    AU: 5 updated - mongodb-shell vscode-go vscode-java vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a013f87d0a4fb2e85bf32546358586d8e5b5dc3a

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.38.2024121308.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.38.2024121708.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.23.2024121601.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.23.2024121701.vsix"
