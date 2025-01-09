dd73f098d21784a86c1aa79e67e4c1dfde1b1d7b
dgalbraith <dg@lbraith.io>
2025-01-09 04:19:15 +0000

    AU: 6 updated - authme authme.install authme.portable vscode-java vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/d4aad32a380010cf9cc4a0275069b6471874a27d

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.39.2025010708.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.39.2025010808.vsix"
