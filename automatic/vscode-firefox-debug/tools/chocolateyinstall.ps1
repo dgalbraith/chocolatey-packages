9c742bfd3abf2b471064e8c4a2e9e565772113f0
dgalbraith <dg@lbraith.io>
2025-01-11 04:19:11 +0000

    AU: 2 updated - vscode-firefox-debug vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/27e183c1d724af350e391821f5e46b1076902d52

--- automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.12.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.13.0.vsix"
