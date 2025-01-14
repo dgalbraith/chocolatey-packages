64eeca316a87ef2d2cf989cd337636302b983aa3
dgalbraith <dg@lbraith.io>
2025-01-14 04:18:53 +0000

    AU: 4 updated - vscode-firefox-debug vscode-pull-request-github vscode-python watchman [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9868c804ca11bde7f5e971cd27c70fc615192403

--- automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
+++ automatic/vscode-firefox-debug/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.13.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\firefox-devtools.vscode-firefox-debug-2.14.0.vsix"
