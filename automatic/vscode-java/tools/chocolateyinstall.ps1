f3c4164a8f8b230cbe04a37b63b29a406b6079d5
dgalbraith <dg@lbraith.io>
2025-01-18 04:19:26 +0000

    AU: 2 updated - vscode-java vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/58e7f0b865d80436a595f09c3dc65fcb155a84f0

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.39.2025011008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.39.2025011708.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.1.2025010601'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.1.2025011702'
