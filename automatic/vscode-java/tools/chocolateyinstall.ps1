df7b14ccddeaa4cf6083589daa7f0404aaee1ba0
dgalbraith <dg@lbraith.io>
2025-01-26 04:20:27 +0000

    AU: 2 updated - fluidsynth vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/451462312f2f3202174d5f7745d1f242acf99454

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025012308.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025012508.vsix"
