27a843eb029409fec8361bcfaafb9e15baf218b1
dgalbraith <dg@lbraith.io>
2025-01-29 04:19:29 +0000

    AU: 4 updated - vscode-drawio vscode-jupyter vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/08e7804acaaf5c4241fa06dce15c2b63d3c47b05

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.6.6.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.7.2025012806.vsix"
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.23.2025012401.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.23.2025012801.vsix"
