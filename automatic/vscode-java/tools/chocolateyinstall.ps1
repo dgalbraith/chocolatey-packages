7b3d96bf412d03864bed439928dee21e9ac8a760
dgalbraith <dg@lbraith.io>
2025-01-31 04:19:28 +0000

    AU: 4 updated - vscode-java vscode-jupyter vscode-mongodb vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6f008ce28ef6b07abe7ff00313ab809a5cb99597

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025012908.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025013008.vsix"
