e956394479729707b0f6afc8cac319649192554e
dgalbraith <dg@lbraith.io>
2025-02-07 04:19:58 +0000

    AU: 4 updated - sqlite.analyzer sqlite.shell vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/3c4e0616badf5d35d6a4e6cb36aa95fae0dfc742

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2024.23.2025012801.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.1.2025020602.vsix"
