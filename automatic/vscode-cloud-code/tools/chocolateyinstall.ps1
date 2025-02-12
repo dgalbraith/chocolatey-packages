8ee3de359eaed7e3ed777c33c36c9b4dbec0b0e8
dgalbraith <dg@lbraith.io>
2025-02-12 04:20:52 +0000

    AU: 8 updated - authme authme.install authme.portable jruby vscode-cloud-code vscode-go vscode-java vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/92d2d1c99ed392358a7ffc04fb6f37ececf65b12

--- automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
+++ automatic/vscode-cloud-code/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.25.0'
+Install-VsCodeExtension -extensionId 'GoogleCloudTools.cloudcode@2.26.0'
--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025021008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.40.2025021108.vsix"
