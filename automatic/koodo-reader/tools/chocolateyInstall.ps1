824857db9960f95f05cf11795eac1aa9501fe6d7
dgalbraith <dg@lbraith.io>
2025-02-17 04:19:54 +0000

    AU: 2 updated - koodo-reader vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/f83bb7fc12e470982364a3a8a05803f20cb273fd

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.7.8.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.7.9.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
