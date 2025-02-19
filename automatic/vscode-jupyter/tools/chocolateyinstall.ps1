4ed7ae2b3d395cda5f089d754059cb1de1037958
dgalbraith <dg@lbraith.io>
2025-02-19 04:20:45 +0000

    AU: 6 updated - koodo-reader sqlite.analyzer sqlite.shell vscode-jupyter vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/c9b73b9d50ec777a149be7dff238786ff8c66ea5

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.2.2025020701'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.2.2025021801'
