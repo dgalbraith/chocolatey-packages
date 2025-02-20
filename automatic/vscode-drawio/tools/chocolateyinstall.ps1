5dd281bac2fe088efc0433800086c8174c424199
dgalbraith <dg@lbraith.io>
2025-02-20 04:19:45 +0000

    AU: 3 updated - vscode-drawio vscode-go vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5cb8a6d1da98a1cdcfe724423480234101fec28b

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.8.250217011.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.9.250219012.vsix"
