456ec70f5c14e199fcf19cab9c2578aaf7bb9221
dgalbraith <dg@lbraith.io>
2025-02-24 04:19:44 +0000

    AU: 1 updated - koodo-reader [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e6c6a2e88e15a23843136d3ddaf55cec4587670e

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.8.0.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.8.1.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
