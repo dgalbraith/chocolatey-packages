749490af2ab217d5a0213f706d0c58aad0797372
dgalbraith <dg@lbraith.io>
2025-03-01 04:20:48 +0000

    AU: 6 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable dotcover-cli vscode-firefox-debug vscode-go [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/8d83b7dbc7676822e5459493393ced6fd773ea65

--- automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyinstall.ps1
@@ -8,6 +8,6 @@ foreach ($file in $files) {
   New-Item "$file.ignore" -type file -force | Out-Null
 }
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.0.9-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.1.4-portable.exe'
 
 Install-BinFile -UseStart -Name 'BeekeeperStudio' -Path "$executable"
--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.0.9-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.1.4-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
