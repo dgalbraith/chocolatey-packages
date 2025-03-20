d208ac9711bd63253667b3a013c1d9a0c7624dd0
dgalbraith <dg@lbraith.io>
2025-03-20 04:20:06 +0000

    AU: 3 updated - vscode-cloud-code vscode-java vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6a159b7d86ee72fd6c62a6de7b6f9b35ad81a557

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.41.2025031808.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.41.2025031912.vsix"
