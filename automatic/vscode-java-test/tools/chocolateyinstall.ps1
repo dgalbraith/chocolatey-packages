a3d6419e6e2a838ac3ce4b3d98fbe5b2760cb9f9
dgalbraith <dg@lbraith.io>
2025-04-04 04:20:08 +0000

    AU: 3 updated - dotcover-cli vscode-java-test vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/81d904c99902fb19c96d88934a25ba51fe11509c

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.43.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.43.2025040304.vsix"
