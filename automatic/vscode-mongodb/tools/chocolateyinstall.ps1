9b617d227836fa1f50bacd5220e44ea4185cde98
dgalbraith <dg@lbraith.io>
2025-04-05 04:19:15 +0000

    AU: 2 updated - vscode-mongodb vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/0d0553d06ce6d1db74953bda8a6b01d4b1dc1a07

--- automatic/vscode-mongodb/tools/chocolateyinstall.ps1
+++ automatic/vscode-mongodb/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-1.12.1.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\mongodb.mongodb-vscode-1.13.0.vsix"
