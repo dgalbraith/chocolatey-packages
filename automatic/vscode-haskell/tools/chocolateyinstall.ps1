947eec0a05a87e5ee4776ee58f3a209adc67a749
dgalbraith <dg@lbraith.io>
2025-04-18 04:26:12 +0000

    AU: 4 updated - vscode-haskell vscode-jupyter vscode-pull-request-github vscode-spring-boot-dashboard [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/df19df605e8f33c76956e0732080752321028786

--- automatic/vscode-haskell/tools/chocolateyinstall.ps1
+++ automatic/vscode-haskell/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.5.3.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.6.0.vsix"
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.4.2025041601'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.4.2025041701'
