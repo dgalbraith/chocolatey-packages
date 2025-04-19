60360a67f919d193867b97a4315022e7fb23be0e
dgalbraith <dg@lbraith.io>
2025-04-19 16:25:33 +0100

    (gh-602) Added support for universal archive Automatic updates for  the package were failing due to a change in the distribution structure - rather than platform-specifc packages the package, as of version 2005.1, is not ditributed as a universal archive. Modified the documentation along with the package updater and installation to reflect this.

--- automatic/dotcover-cli/tools/chocolateyInstall.ps1
+++ automatic/dotcover-cli/tools/chocolateyInstall.ps1
@@ -2,22 +2,25 @@
 
 $toolsDir  = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$archive32 = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.windows-x86.2024.3.7.zip'
-$archive64 = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.windows-x64.2024.3.7.zip'
-
-if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
-  $archive = $archive32
-} else {
-  $archive = $archive64
-}
+$archive = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.windows-x86.2024.3.7.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = $archive
   Destination  = $toolsDir
+  Checksum     = 'de35cb48a8924dac6dd3e7ef54fa7014ba4920b0dab0476d36d0733eef68951f'
+  ChecksumType = 'sha256'
 }
 
 Get-ChocolateyUnzip @unzipArgs
 
-Remove-Item $toolsDir\*.zip -ea 0
+$unzipArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = $archive -replace '\.(zip|gz)$', ''
+  Destination  = $toolsDir
+}
+
+Get-ChocolateyUnzip @unzipArgs
+
+Remove-Item "$toolsDir\*.{zip,tar,gz}" -ea 0
 Get-ChildItem $toolsDir -include *.exe -exclude 'dotCover.exe' -recurse  | Select-Object { New-Item "$_.ignore" -type file -force } | Out-Null
