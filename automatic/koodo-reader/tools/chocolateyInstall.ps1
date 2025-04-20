66e0ccba814bdb8a51677add66cbca861a4291f6
dgalbraith <dg@lbraith.io>
2025-04-20 10:20:55 +0000

    AU: 2 updated - koodo-reader vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/d52f5e3b985f61d301d5326532bbade858498c41

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-1.9.3.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-1.9.4.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
