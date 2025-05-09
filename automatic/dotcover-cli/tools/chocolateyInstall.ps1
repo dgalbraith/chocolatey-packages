3c0073b67f24f8f1905e6dd9b933fd22065756b5
dgalbraith <dg@lbraith.io>
2025-05-09 04:20:38 +0000

    AU: 6 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable dotcover-cli vscode-java vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/35314b67da089db8270da3df3eed6c06a74545e4

--- automatic/dotcover-cli/tools/chocolateyInstall.ps1
+++ automatic/dotcover-cli/tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $toolsDir  = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$archive = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.2025.1.1.tar.gz'
+$archive = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.2025.1.2.tar.gz'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = $archive
   Destination  = $toolsDir
-  Checksum     = '2BF3D9898F2CC60B17EE370C3E8AFFE551704ACFA5DE9543A71B144787AD950A'
+  Checksum     = 'B519D5030FC6766F35ABEF2449D278B0541B1DB86EAED46BDE93C3D253A9DADC'
   ChecksumType = 'sha256'
 }
 
