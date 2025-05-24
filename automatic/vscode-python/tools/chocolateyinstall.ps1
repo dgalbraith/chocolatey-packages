c86facd27af36d09581cdb64b80ef9eb7741bc70
dgalbraith <dg@lbraith.io>
2025-05-24 04:20:09 +0000

    AU: 6 updated - nushell nushell.install nushell.portable vscode-java vscode-jupyter vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/f67abd673b7e9306bd220b687e8367a9ccaf5205

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.7.2025052102.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.7.2025052301.vsix"
