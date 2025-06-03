0bf332d55f1b77528fabe0d5f018101a3326d6a7
dgalbraith <dg@lbraith.io>
2025-06-03 04:20:15 +0000

    AU: 1 updated - vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/924c43d9b456973b8dd33ca4d844ea28be470a4f

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025053100.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025060300.vsix"
