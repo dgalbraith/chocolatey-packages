0344a6e27a94d5c7ce83e5d67e93f0703082d572
dgalbraith <dg@lbraith.io>
2025-06-05 04:21:11 +0000

    AU: 5 updated - vscode-java vscode-jupyter vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/d861d0d6e8c7bedbdcca7740fea66da913f9ad21

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.43.2025060308.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.43.2025060408.vsix"
--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025060311.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025060500.vsix"
