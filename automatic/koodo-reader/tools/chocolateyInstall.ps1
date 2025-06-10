af0b84bdd9a116f532cbe2332cde9b46f55779cc
dgalbraith <dg@lbraith.io>
2025-06-10 04:20:35 +0000

    AU: 4 updated - koodo-reader vscode-mongodb vscode-pylance vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/306aeebc6b210b5aa00d7834366a4de05f40e7b8

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-2.0.0.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-2.0.1.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025060700.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025061000.vsix"
