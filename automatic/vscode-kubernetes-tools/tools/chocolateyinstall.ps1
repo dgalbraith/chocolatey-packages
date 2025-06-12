25520dfd8f88da5d87641f2b63ac63d3fdf0891b
dgalbraith <dg@lbraith.io>
2025-06-12 04:20:47 +0000

    AU: 3 updated - vscode-java vscode-kubernetes-tools vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/25d0d914c7c8f0e589cf4a00d0d51a1527039fbc

--- automatic/vscode-kubernetes-tools/tools/chocolateyinstall.ps1
+++ automatic/vscode-kubernetes-tools/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-kubernetes-tools.vscode-kubernetes-tools-1.3.23.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-kubernetes-tools.vscode-kubernetes-tools-1.3.24.vsix"
--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.63.2025061000.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-1.64.2025061200.vsix"
