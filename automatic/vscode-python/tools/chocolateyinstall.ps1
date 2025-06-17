7d07dcf2c2b4eead0ba76a34904db7894183f583
dgalbraith <dg@lbraith.io>
2025-06-17 04:20:29 +0000

    AU: 3 updated - vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ff351d987cd01ada810549d57e425fc20e137f16

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.7.2025060501.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.8.0.vsix"
