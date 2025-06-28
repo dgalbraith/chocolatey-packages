94fa4c494acc67e9a40a8be65a05da22b2c5e38f
dgalbraith <dg@lbraith.io>
2025-06-28 04:20:29 +0000

    AU: 6 updated - openvpn vscode-java vscode-jupyter vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/eedcfe296caa7f2a1bb975a4c80c3f8f37b49680

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.43.2025062508.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025062800.vsix"
