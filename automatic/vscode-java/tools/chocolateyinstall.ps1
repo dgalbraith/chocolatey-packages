b832c9b1b96622e9c9057b6bc2619fa64dfcab41
dgalbraith <dg@lbraith.io>
2025-07-24 04:20:14 +0000

    AU: 7 updated - nushell nushell.install nushell.portable remote-desktop-client rsync vscode-java vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1462f076ed17b7bcd783d5a9ad9b14bbc42468bf

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025071808.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025072308.vsix"
