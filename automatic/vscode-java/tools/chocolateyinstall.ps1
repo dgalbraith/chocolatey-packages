1e94d2370d10ff9c56fd98d3b656baaaae5849fe
dgalbraith <dg@lbraith.io>
2025-07-26 04:20:58 +0000

    AU: 6 updated - dbgate dbgate.install dbgate.portable vscode-java vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b809c671c523776bd45e924addf053b9ef4debbc

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025072408.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025072508.vsix"
