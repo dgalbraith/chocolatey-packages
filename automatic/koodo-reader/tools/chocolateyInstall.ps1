e6117e067ef4ed11bfc63e1718419057f0c7cbe3
dgalbraith <dg@lbraith.io>
2025-07-28 04:21:14 +0000

    AU: 3 updated - koodo-reader simple-sticky-notes vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/6f04d6563d2b23053b02d8888b730f8f41ffb56b

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-2.0.7.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-2.0.8.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
