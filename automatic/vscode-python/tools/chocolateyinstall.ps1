a961232d091405e42cc69899482f74bcd49b874e
dgalbraith <dg@lbraith.io>
2025-07-30 04:21:24 +0000

    AU: 5 updated - nushell nushell.install nushell.portable vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/7752b19b08fe4e0b4e6d39ef229220584616627a

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.11.2025072801.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.11.2025072901.vsix"
