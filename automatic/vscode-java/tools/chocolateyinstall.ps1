157b7b0ed04c7b334a9746fbaecf6c4d4acbeef8
dgalbraith <dg@lbraith.io>
2025-07-31 04:21:11 +0000

    AU: 5 updated - sqlite.analyzer sqlite.shell vscode-java vscode-pull-request-github vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/93690e0d5c694fa128380aa5f86e1ae04b9306e7

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025072508.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.44.2025073008.vsix"
