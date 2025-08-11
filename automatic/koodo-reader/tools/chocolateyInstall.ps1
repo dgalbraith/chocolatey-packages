674fbac7b8b57d249991602a5cdcbe614d420872
dgalbraith <dg@lbraith.io>
2025-08-11 04:21:15 +0000

    AU: 2 updated - koodo-reader vscode-jupyter [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a8baf8a0fd61b6ad379bcf201881eb83c00b3a2f

--- automatic/koodo-reader/tools/chocolateyInstall.ps1
+++ automatic/koodo-reader/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$installer  = Join-Path $toolsDir 'Koodo-Reader-2.0.9.exe'
+$installer  = Join-Path $toolsDir 'Koodo-Reader-2.1.0.exe'
 $silentArgs = '/S /AllUsers'
 
 $pp = Get-PackageParameters
--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.8.2025080601'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.8.2025081101'
