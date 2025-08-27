f878a0324cf67f95921b865ebbea2a7061474082
dgalbraith <dg@lbraith.io>
2025-08-27 04:21:59 +0000

    AU: 4 updated - spyder vscode-java-debug vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/403187c65635a96f70e78df28396d2a0d5ebb55d

--- automatic/spyder/tools/chocolateyInstall.ps1
+++ automatic/spyder/tools/chocolateyInstall.ps1
@@ -43,8 +43,8 @@ $silentArgs += ($installParams.GetEnumerator() | ForEach-Object { " /$($_.name)=
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url64          = 'https://github.com/spyder-ide/spyder/releases/download/v6.0.7/Spyder-Windows-x86_64.exe'
-  checksum64     = '40c4bab88e3913d59a9fa3c6ac49816c336df1421cbe99605513b91cb7b46063'
+  url64          = 'https://github.com/spyder-ide/spyder/releases/download/v6.0.8/Spyder-Windows-x86_64.exe'
+  checksum64     = '8cdbac3eb8b8f6dba3646cf385208ce9d7cab2a581eb898bcee0055b78529c4b'
   checksumType64 = 'sha256'
   fileType       = 'exe'
   silentArgs     = $silentArgs
