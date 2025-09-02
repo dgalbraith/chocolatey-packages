87dc7be63c7ef3127f901d28d33e2388157886eb
dgalbraith <dg@lbraith.io>
2025-09-02 04:21:06 +0000

    AU: 6 updated - dbgate dbgate.install dbgate.portable sqlean vscode-pull-request-github vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1f9c444927e79abe9ef8aac0bb565951d3939b62

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.13.2025082601.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.13.2025090101.vsix"
