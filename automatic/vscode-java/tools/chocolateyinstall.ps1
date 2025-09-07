613ee1771ead93e6498d62a0d4050605173bfc2d
dgalbraith <dg@lbraith.io>
2025-09-07 04:21:12 +0000

    AU: 1 updated - vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/75500b8a070e62230f940a780fc979696760443e

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.46.2025090508.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.46.2025090608.vsix"
