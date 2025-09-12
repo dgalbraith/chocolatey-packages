e6f008f8dff20a2aee469be0f84d3f1193548525
dgalbraith <dg@lbraith.io>
2025-09-12 04:21:51 +0000

    AU: 5 updated - kindle vscode-java vscode-pull-request-github vscode-pylance vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/11a42bba2adbbe6ffa1bfa3d49453ee81c4404ac

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.us-east-1.amazonaws.com/70978/KindleForPC-installer-2.7.70978.exe'
-  checksum      = '0a2dc0db4c32970d7d72a3c3e9e5e48aa34c2fad56dd11ad9e55a7ef11d71e84'
+  url           = 'https://kindleforpc.s3.us-east-1.amazonaws.com/70980/KindleForPC-installer-2.8.70980.exe'
+  checksum      = '2ed64ee0fb5ad94032d6d9824d4efbeeea435255c963e9393d949259b87ebfa4'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.13.2025090201.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.14.0.vsix"
