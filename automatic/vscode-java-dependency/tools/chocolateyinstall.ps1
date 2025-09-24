39e5e88abf71de28bd7d2b2474b8c8d3ba950df5
dgalbraith <dg@lbraith.io>
2025-09-24 04:21:53 +0000

    AU: 3 updated - vscode-java vscode-java-dependency vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5eda1025952bfadc594f7ed777623fa450549833

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.25.2025091901.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.25.2025092303.vsix"
