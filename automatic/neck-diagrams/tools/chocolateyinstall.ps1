75c1260db06784f4c5b1fc7860f8243bc83e3d68
dgalbraith <dg@lbraith.io>
2025-10-04 04:22:15 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable neck-diagrams [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ffa21b479000bbf03906c1e1d1b184038ec15c94

--- automatic/neck-diagrams/tools/chocolateyinstall.ps1
+++ automatic/neck-diagrams/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url32          = 'https://www.neckdiagrams.com/download-files/NeckDiagrams-2.4.1-Setup-32bit.exe'
-  url64          = 'https://www.neckdiagrams.com/download-files/NeckDiagrams-2.4.1-Setup-64bit.exe'
-  checksum32     = 'eb13a51fe100780ca9accde858141dc682e8985241a824521949e70c6f3560d3'
+  url32          = 'https://www.neckdiagrams.com/download-files/NeckDiagrams-2.4.2-Setup-32bit.exe'
+  url64          = 'https://www.neckdiagrams.com/download-files/NeckDiagrams-2.4.2-Setup-64bit.exe'
+  checksum32     = '637b3a60defc07844a1fff1c06b8fd5783436197448eff70276daba73dfc6ba3'
   checksumType32 = 'sha256'
-  checksum64     = 'b53a4b73585889337a087abc6ccdbe8439068daf4b53aad91cfe9509a87925e1'
+  checksum64     = 'c335e8bb8802f5796e6fd3a5be4d2caeabbbf06e5dec296de3d7a56f554ac3cb'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
