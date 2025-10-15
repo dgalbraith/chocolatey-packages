df3b2013d23d2d221b34971fb9c878cfa33bdee4
dgalbraith <dg@lbraith.io>
2025-10-15 04:21:47 +0000

    AU: 6 updated - authme authme.install authme.portable remote-desktop-client vscode-java vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a8735ea6d72b60040b7bf3c43e1f04c9c4b9d618

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.47.2025101108.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.47.2025101408.vsix"
