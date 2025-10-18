4c03a26b84aa69b48bb0f0d543419ccca6536b5a
dgalbraith <dg@lbraith.io>
2025-10-18 04:21:12 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-java-dependency vscode-python [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5e1c0c97d05adddade898dc8be4e3f741773022f

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.26.2025092801.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.26.2025101705.vsix"
