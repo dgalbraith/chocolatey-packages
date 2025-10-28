c834b1cb9609076f51dccbed2011797fa3cef92d
dgalbraith <dg@lbraith.io>
2025-10-28 04:21:14 +0000

    AU: 5 updated - dbgate dbgate.install dbgate.portable vscode-go vscode-java-dependency [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/0e08874005d1badd3194f17e4fc80876829a9afe

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.26.2025102208.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.26.2025102709.vsix"
