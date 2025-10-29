03fbb13a1bbbecb36dd990970bf8e34105439bc6
dgalbraith <dg@lbraith.io>
2025-10-29 04:21:53 +0000

    AU: 3 updated - dotcover-cli remote-desktop-client vscode-java-dependency [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/e73890ce57fbe7378f6f3917a8304d43952f4bd9

--- automatic/dotcover-cli/tools/chocolateyInstall.ps1
+++ automatic/dotcover-cli/tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $toolsDir  = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$archive = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.2025.2.3.tar.gz'
+$archive = Join-Path $toolsDir 'JetBrains.dotCover.CommandLineTools.2025.2.4.tar.gz'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = $archive
   Destination  = $toolsDir
-  Checksum     = '0EAB9BF9E63B42E09D67FCC105618BF5781317AE4892D3D5F9E3DD989906C56F'
+  Checksum     = '94298CA2B8507FED5345767AE7638A675B07DE73F74C9EA8A4FB2597C5E9CBF5'
   ChecksumType = 'sha256'
 }
 
