198d18e7b2783e66bcf8e4a4be756983632bb5dc
dgalbraith <dg@lbraith.io>
2025-11-05 04:20:09 +0000

    AU: 6 updated - sqlite.analyzer sqlite.shell vscode-java-debug vscode-maven vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1b7374ea5642b26050fd4a3c36b7d8f099718393

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.0.2025110100.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.0.2025110407.vsix"
