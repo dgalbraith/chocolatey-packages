3458b733fb8a2dff631d0de655fe71d64c339272
dgalbraith <dg@lbraith.io>
2025-11-06 04:20:33 +0000

    AU: 4 updated - vscode-maven vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/636fd6b54c1d924ac54ae99e3bba5a90fd4c79c6

--- automatic/vscode-maven/tools/chocolateyinstall.ps1
+++ automatic/vscode-maven/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\vscjava.vscode-maven-0.44.2025090403.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\C:\projects\chocolatey-packages\automatic\vscode-maven\tools\vscjava.vscode-maven-0.44.2025110601.vsix"
