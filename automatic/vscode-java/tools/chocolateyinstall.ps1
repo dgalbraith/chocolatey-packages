4cadfe402a190ec335e86c24fefc9ac717ffe1ac
dgalbraith <dg@lbraith.io>
2025-11-15 04:20:28 +0000

    AU: 2 updated - vscode-java vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/db5b3074e07d553da58dc3bd0d3da6a826bf40b6

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.48.2025110708.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.48.2025111408.vsix"
