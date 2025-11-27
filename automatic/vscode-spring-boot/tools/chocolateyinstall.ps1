179b0ca75bba4ca93deba4aa25a2c110b33a61b2
dgalbraith <dg@lbraith.io>
2025-11-27 04:21:05 +0000

    AU: 9 updated - dbgate dbgate.install dbgate.portable vscode-java vscode-java-debug vscode-kubernetes-tools vscode-pull-request-github vscode-pylance vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/89d2b8e2cb3eaca36db7c9e52ab6c600de7a7539

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.0.2025112500.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.0.2025112700.vsix"
