70487851ddbc0a3eb75b8ee164457e993fb87c51
dgalbraith <dg@lbraith.io>
2025-12-16 04:21:29 +0000

    AU: 3 updated - vscode-kubernetes-tools vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/67c9ac211d184671f298597a9dc318b43e77b833

--- automatic/vscode-python/tools/chocolateyinstall.ps1
+++ automatic/vscode-python/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.20.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\ms-python.python-2025.21.2025121501.vsix"
