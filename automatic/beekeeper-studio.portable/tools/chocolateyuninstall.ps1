47bfbd7c57e04254f62d17867cd844446b50f11d
dgalbraith <dg@lbraith.io>
2026-02-01 04:20:41 +0000

    AU: 4 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/ded7d50071c6a9848fbb0e755d29da00cc5f86dd

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.5.5-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.5.6-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
