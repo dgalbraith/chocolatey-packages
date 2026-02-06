47d71b67f406a925a14f216258b9ecc6aa302272
dgalbraith <dg@lbraith.io>
2026-02-06 04:21:15 +0000

    AU: 5 updated - dbgate dbgate.install dbgate.portable vscode-java-debug vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/f1d14b48639eadd9ac9cf2fe5307fd40a1334a60

--- automatic/dbgate.portable/tools/chocolateyInstall.ps1
+++ automatic/dbgate.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$archive = Join-Path $toolsDir 'dbgate-7.0.1-win_x64.zip'
+$archive = Join-Path $toolsDir 'dbgate-7.0.3-win_x64.zip'
 
 $unzipArgs = @{
   PackageName  = $env:ChocolateyPackageName
