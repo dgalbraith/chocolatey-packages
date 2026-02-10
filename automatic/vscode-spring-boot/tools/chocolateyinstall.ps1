cad64b523eb40225abe909effe5114c91d4af491
dgalbraith <dg@lbraith.io>
2026-02-10 04:22:18 +0000

    AU: 9 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable dbgate dbgate.install dbgate.portable vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b9182607cbc0605ed75c4c6b8c626cec8dfb933b

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.1.2026020700.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.1.2026021000.vsix"
