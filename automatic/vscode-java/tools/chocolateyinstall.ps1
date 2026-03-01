fa6bcfab03569ccad325e44977ce25ccca0751dc
dgalbraith <dg@lbraith.io>
2026-03-01 04:20:59 +0000

    AU: 4 updated - nushell nushell.install nushell.portable vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/08e2543a688661f5283fceee5308915e13e51238

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.53.2026022008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.54.2026022808.vsix"
