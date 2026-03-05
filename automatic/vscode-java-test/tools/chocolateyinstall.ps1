395af2dffa2c16f74096bdd2c05466e0242719be
dgalbraith <dg@lbraith.io>
2026-03-05 04:27:22 +0000

    AU: 5 updated - beekeeper-studio jruby vscode-java-test vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/1210eabda457d89fff2ee4ae80ff04460f864398

--- automatic/vscode-java-test/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-test/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.44.2026030208.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-test-0.44.2026030502.vsix"
