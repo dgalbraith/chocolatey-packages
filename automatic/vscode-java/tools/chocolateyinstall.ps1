3a5728ab4cc49a8567ad7a53942efad37e283eb5
dgalbraith <dg@lbraith.io>
2026-03-12 04:21:27 +0000

    AU: 5 updated - beekeeper-studio vscode-java vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/c7c3836c2d4c3bdc666e40111249cb4b96a01f66

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.54.2026031008.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.54.2026031108.vsix"
