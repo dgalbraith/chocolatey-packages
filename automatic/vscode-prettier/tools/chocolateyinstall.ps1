64c8df46ec95cf10f8a00a2dc458eb115a754a78
dgalbraith <dg@lbraith.io>
2026-03-17 04:20:51 +0000

    AU: 4 updated - mongodb-shell vscode-prettier vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/45e05df244287102dc73401abb112cb360ff51da

--- automatic/vscode-prettier/tools/chocolateyinstall.ps1
+++ automatic/vscode-prettier/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-12.3.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\esbenp.prettier-vscode-12.4.0.vsix"
