796bb9cb9cc3c5c58b3506841e0e6b2dad3afe00
dgalbraith <dg@lbraith.io>
2026-04-11 04:21:16 +0000

    AU: 3 updated - ant vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/c338f986f703fe02b51cb5216541027eb432e744

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.2.2026040900.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.2.2026041100.vsix"
