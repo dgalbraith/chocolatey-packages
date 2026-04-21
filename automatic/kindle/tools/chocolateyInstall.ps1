427b68abf6a4105749f4ee2613b94641bd6df6be
dgalbraith <dg@lbraith.io>
2026-04-21 04:22:21 +0000

    AU: 4 updated - kindle vscode-java-debug vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/b04602cc3de2131be3220966649e9d1b50d7a181

--- automatic/kindle/tools/chocolateyInstall.ps1
+++ automatic/kindle/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://kindleforpc.s3.us-east-1.amazonaws.com/70995/KindleForPC-installer-2.8.70995.exe'
-  checksum      = 'e5bda18ad4d57999de18372c94416c99bf85c7183d99ba52b2982df8fbe119e0'
+  url           = 'https://kindleforpc.s3.us-east-1.amazonaws.com/71006/KindleForPC-installer-2.9.71006.exe'
+  checksum      = '8e3bff714de9a8e3cad0f8072e3150df0d419e4a08b54f9ef91bcded7c2ef4ef'
   checksumType  = 'sha256'
   silentArgs    = '/S'
   validExitCodes= @(0)
