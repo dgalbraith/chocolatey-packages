f5b4e73e44c9de883f3a54d45e0aa1b61084ee1b
dgalbraith <dg@lbraith.io>
2026-04-23 04:21:16 +0000

    AU: 6 updated - dbgate dbgate.install dbgate.portable vscode-jupyter vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/bc21e6d8a82a67903a9ed0bd5b94fbc42a1deace

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.10.2026040301'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.10.2026042201'
