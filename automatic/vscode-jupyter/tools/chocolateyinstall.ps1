138be3f690bdc2cc2f8e5bc010f956dd80e18b78
dgalbraith <dg@lbraith.io>
2026-06-02 04:20:44 +0000

    AU: 3 updated - beekeeper-studio vscode-jupyter vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5a67ac603cd1c453548288b37b0ff6d580f7c653

--- automatic/vscode-jupyter/tools/chocolateyinstall.ps1
+++ automatic/vscode-jupyter/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2025.10.2026052901'
+Install-VsCodeExtension -extensionId 'ms-toolsai.jupyter@2026.6.2026060102'
