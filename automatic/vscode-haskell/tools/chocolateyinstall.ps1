e8b478c419c28ce7cf8032ac27410981b6df7eb6
dgalbraith <dg@lbraith.io>
2026-06-13 04:20:53 +0000

    AU: 3 updated - vscode-haskell vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/688a562ade09f253eb11aeacdc6c145c70958a9a

--- automatic/vscode-haskell/tools/chocolateyinstall.ps1
+++ automatic/vscode-haskell/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.8.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\haskell.haskell-2.8.1.vsix"
