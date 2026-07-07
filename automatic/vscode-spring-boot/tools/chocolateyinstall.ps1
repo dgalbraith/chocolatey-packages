cb5fd843f3579f311c263e4e3ef7483305d0216d
dgalbraith <dg@lbraith.io>
2026-07-07 04:21:00 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5a75d194893f108d974643930244a5ce2a84e8ee

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.3.2026070400.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.3.2026070701.vsix"
