1e8e4d0755f4a8c08e82222b9a80d8c12acb08b4
dgalbraith <dg@lbraith.io>
2026-07-16 04:20:56 +0000

    AU: 5 updated - rsync vscode-go vscode-jupyter vscode-pull-request-github vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/a7a1109dddbb072acce25b6e8f7b70dc0b847138

--- automatic/vscode-go/tools/chocolateyinstall.ps1
+++ automatic/vscode-go/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.55.0.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\golang.Go-0.56.0.vsix"
