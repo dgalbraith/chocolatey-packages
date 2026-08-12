416ebfa1c7bbbf720e069779d0f011fa9f590b3d
dgalbraith <dg@lbraith.io>
2026-08-12 04:21:13 +0000

    AU: 5 updated - beekeeper-studio beekeeper-studio.install beekeeper-studio.portable remote-desktop-client vscode-pull-request-github [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/9aaa531fc9741955da0443e171af40017c6124df

--- automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
+++ automatic/beekeeper-studio.portable/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$executable = Join-Path $toolsDir 'Beekeeper-Studio-5.9.3-portable.exe'
+$executable = Join-Path $toolsDir 'Beekeeper-Studio-6.0.0-portable.exe'
 
 Uninstall-BinFile -Name 'BeekeeperStudio' -Path "$executable"
