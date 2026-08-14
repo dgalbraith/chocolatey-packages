2becf459c9daff2070abba9e94d9ba75b0a176e8
dgalbraith <dg@lbraith.io>
2026-08-14 04:20:47 +0000

    AU: 5 updated - dbgate dbgate.install dbgate.portable mongodb-shell vscode-java [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/bec51043a44f958b279e2c52e89313b4ec69ccd8

--- automatic/vscode-java/tools/chocolateyinstall.ps1
+++ automatic/vscode-java/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.56.2026081208.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\redhat.java-1.56.2026081308.vsix"
