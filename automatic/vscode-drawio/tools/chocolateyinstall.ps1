226ffa1d76708b0767fda62331f10e57d28fdf51
dgalbraith <dg@lbraith.io>
2026-08-29 04:21:19 +0000

    AU: 5 updated - vscode-drawio vscode-java vscode-pull-request-github vscode-pylance vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/8213e7489df84537431fc00416921de3dac4f96b

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.9.260821026.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.9.260828027.vsix"
