3478e3c956aeb75caac1149390d7a28d64762339
dgalbraith <dg@lbraith.io>
2026-09-04 04:20:56 +0000

    AU: 3 updated - openvpn vscode-drawio vscode-mongodb [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/cc5e1f17906b5f412fdea47a9eb5cfe96e2ab395

--- automatic/vscode-drawio/tools/chocolateyinstall.ps1
+++ automatic/vscode-drawio/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.9.260828027.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\hediet.vscode-drawio-1.9.260903028.vsix"
