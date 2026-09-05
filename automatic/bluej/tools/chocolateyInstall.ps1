39c848f1716e3ca6de2ba8d93990ed07a333e4f6
dgalbraith <dg@lbraith.io>
2026-09-05 04:21:19 +0000

    AU: 5 updated - bluej vscode-java vscode-pull-request-github vscode-pylance vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/bc3e59ffcc54da837a66c6ef1f6cb7e2fb2031a3

--- automatic/bluej/tools/chocolateyInstall.ps1
+++ automatic/bluej/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq 'true') {
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'msi'
-  Url64Bit       = 'https://github.com/k-pet-group/BlueJ-Greenfoot/releases/download/BLUEJ-RELEASE-6.0.0/BlueJ-windows-6.0.0.msi'
-  Checksum64     = '4383dae085447d55672f9521320bd60f23fd3cf7d88e219c17a4731dad665528'
+  Url64Bit       = 'https://github.com/k-pet-group/BlueJ-Greenfoot/releases/download/BLUEJ-RELEASE-6.0.1/BlueJ-windows-6.0.1.msi'
+  Checksum64     = '7fe55fd4e5de9e57c417891c813fb4ee28c62b5bf112435f9ba43a5cf8b5e315'
   ChecksumType64 = 'sha256'
   SilentArgs     = '/quiet /qn /norestart'
   ValidExitCodes = @(0,3010)
