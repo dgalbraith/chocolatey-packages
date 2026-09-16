2c516481fd5731b696bebfaad3ad8beac3fb8e0b
dgalbraith <dg@lbraith.io>
2026-09-16 04:21:31 +0000

    AU: 7 updated - beekeeper-studio dbgate dbgate.install dbgate.portable mongodb-shell vscode-java-dependency vscode-pylance [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/5a64e8d7bba3c4ea2f6d18f6cff5b96f9bfd584a

--- automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
+++ automatic/vscode-java-dependency/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.27.2026082200.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vscjava.vscode-java-dependency-0.27.2026091500.vsix"
