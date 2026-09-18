61b32af4d9f0685d32da56d53d6279f695a040f1
dgalbraith <dg@lbraith.io>
2026-09-18 04:21:38 +0000

    AU: 8 updated - beekeeper-studio mongodb-cli mongodb-cli.install mongodb-cli.portable stunnel vscode-pull-request-github vscode-python vscode-spring-boot [skip ci] https://gist.github.com/dgalbraith/aef61e2663a09f3dfdfa40482177e040/24c0e204c08712e53776a220eeef688fb834dcda

--- automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
+++ automatic/vscode-spring-boot/tools/chocolateyinstall.ps1
@@ -2,4 +2,4 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.5.2026091700.vsix"
+Install-VsCodeExtension -extensionId "$toolsDir\vmware.vscode-spring-boot-2.5.2026091714.vsix"
